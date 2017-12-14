8f098baebaf1957bd99d8a2350e24529de1c0765
kvhnuke <kvhnuke@aol.com>
2017-12-13 18:17:28 -0800

    tx container

--- src/components/HomePage/TxContainer.vue
+++ src/components/HomePage/TxContainer.vue
@@ -1,29 +1,36 @@
 <template>
-<table class="latest-transection-table">
-  <thead>
-    <tr>
-      <td>Hash</td>
-      <td>Block Number</td>
-      <td>From</td>
-      <td>To</td>
-      <td>Value</td>
-      <td>Gas Used</td>
-    </tr>
-  </thead>
-  <tbody>
-    <transition-group name="list">
-      <tr v-for="tx in transactions"
-          v-bind:key="tx.getHash()">
-        <td>{{tx.getHash()}}</td>
-        <td>{{tx.getBlockNumber.toNumber().toString()}}</td>
-        <td>{{tx.getFrom()}}</td>
-        <td>{{tx.getTo()}}</td>
-        <td>{{tx.getValue.toEth()}}</td>
-        <td>{{tx.getGasUsed.toNumber().toString()}}</td>
-      </tr>
-    </transition-group>
-  </tbody>
-</table>
+<div>
+   <transition-group name="list">
+  <div class="data-block"
+       v-for="tx in transactions" v-bind:key="tx.getHash()">
+    <div class="data-top">
+      <div>Hash <span>{{tx.getHash()}}</span></div>
+      <div>Gas <span>{{tx.getGasUsed.toNumber().toString()}}</span></div>
+      <div>Gas Price <span>{{tx.getGasPrice.toEth()}}</span></div>
+      <div>Block <span>{{tx.getBlockNumber.toNumber().toString()}}</span></div>
+    </div>
+    <div class="data-mid">
+      <div>
+        <p>From</p>
+        <p>{{tx.getFrom()}}</p>
+      </div>
+      <div>
+        <p class="data-icon-container">
+          <icon name='long-arrow-right'
+                scale='1'></icon>
+        </p>
+        <p>{{tx.getValue.toEth()}}</p>
+      </div>
+      <div>
+        <p>To</p>
+        <p>{{tx.getTo()}}</p>
+      </div>
+    </div>
+    <!-- .data-mid -->
+  </div>
+</transition-group>
+  <!-- .data-block -->
+</div>
 
 </template>
 
@@ -32,7 +39,9 @@ import Vue from 'vue'
 import store from '@/states'
 export default Vue.extend({
   name: 'block-container',
-  props: ['maxItems'],
+  props: [
+    'maxItems'
+  ],
   data () {
     return {
       store: store
--- src/components/IndividualTransaction/IndividualTransaction.vue
+++ src/components/IndividualTransaction/IndividualTransaction.vue
@@ -1,23 +1,5 @@
 <template>
-  <div class="individualtransaction">
-    <Header></Header>
 
-    <div class="individualtransaction-container container">
-      <div class="row">
-        <div class="col-md-12">
-          
-          <div class="table-fields">
-            <p class="title">Block Height 123123</p>
-            
-            <div class="data-block">
-              
-              <div class="data-top">
-                <div>Hash <span>8723461243</span></div>
-                <div>Gas <span>872HEUD</span></div>
-                <div>Gas Price <span>231.99</span></div>
-                <div>Block <span>823199</span></div>
-              </div>
-              
               <div class="data-mid">
           
                 <div>
@@ -37,53 +19,6 @@
 
               </div>
               <!-- .data-mid -->
-
-            </div>
-            <!-- .data-block -->
-
-
-
-            <div class="data-block">
-              
-              <div class="data-top">
-                <div>Hash <span>8723461243</span></div>
-                <div>Gas <span>872HEUD</span></div>
-                <div>Gas Price <span>231.99</span></div>
-                <div>Block <span>823199</span></div>
-              </div>
-              
-              <div class="data-mid">
-          
-                <div>
-                  <p>From</p>
-                  <p>12371248</p>
-                </div>
-
-                <div>
-                  <p class="data-icon-container"><icon name='long-arrow-right' scale='1'></icon></p>
-                  <p>23412.23</p>
-                </div>
-
-                <div>
-                  <p>To</p>
-                  <p>82437123</p>
-                </div>
-
-              </div>
-              <!-- .data-mid -->
-                            
-            </div>
-            <!-- .data-block -->
-          </div>
-          <!-- .table-fields -->
-
-        </div>
-      </div>  
-    </div>
-
-    <Footer></Footer>
-
-  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
