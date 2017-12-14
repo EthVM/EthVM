1cede5c3255db09cb8e45cc61066838fad09c301
David Hong <hsurf22@gmail.com>
2017-12-14 15:58:39 -0800

    New table design

--- src/components/HomePage/TxContainer.vue
+++ src/components/HomePage/TxContainer.vue
@@ -1,37 +1,37 @@
 <template>
-<div>
-   <transition-group name="list">
-  <div class="data-block"
-       v-for="tx in transactions" v-bind:key="tx.getHash()">
-    <div class="data-top">
-      <div>Hash <span>{{tx.getHash()}}</span></div>
-      <div>Gas <span>{{tx.getGasUsed.toNumber().toString()}}</span></div>
-      <div>Gas Price <span>{{tx.getGasPrice.toEth()}}</span></div>
-      <div>Block <span>{{tx.getBlockNumber.toNumber().toString()}}</span></div>
-    </div>
-    <div class="data-mid">
-      <div>
-        <p>From</p>
-        <p>{{tx.getFrom()}}</p>
-      </div>
-      <div>
-        <p class="data-icon-container">
-          <icon name='long-arrow-right'
-                scale='1'></icon>
-        </p>
-        <p>{{tx.getValue.toEth()}}</p>
-      </div>
-      <div>
-        <p>To</p>
-        <p>{{tx.getTo()}}</p>
-      </div>
-    </div>
-    <!-- .data-mid -->
-  </div>
-</transition-group>
-  <!-- .data-block -->
-</div>
+  <div class="standard-table-1">
+    <p class= "table-title"><icon name='refresh' scale='1'></icon> Latest Transactions</p>
 
+    <transition-group name="list">
+      <div class="data-block-loop" v-for="tx in transactions" v-bind:key="tx.getHash()">
+
+        <div class="data-block-1">
+          <div>Hash <span>{{tx.getHash()}}</span></div>
+          <div>Gas <span>{{tx.getGasUsed.toNumber().toString()}}</span></div>
+          <div>Gas Price <span>{{tx.getGasPrice.toEth()}} ETH</span></div>
+          <div>Block <span>{{tx.getBlockNumber.toNumber().toString()}}</span></div>
+        </div><!-- .data-block-1 -->
+
+        <div class="data-block-2">
+          <div>
+            <h1>From</h1>
+            <p>{{tx.getFrom()}}</p>
+          </div>
+          <div>
+            <div class="data-icon-container">
+              <icon name='long-arrow-right' scale='1'></icon>
+            </div>
+            <p class="amount">{{tx.getValue.toEth()}} ETH</p>
+          </div>
+          <div>
+            <h1>To</h1>
+            <p>{{tx.getTo()}}</p>
+          </div>
+        </div><!-- .data-block-2 -->
+
+      </div><!-- .data-block-loop -->
+    </transition-group>
+  </div><!-- .standard-table-1 -->
 </template>
 
 <script lang="ts">
