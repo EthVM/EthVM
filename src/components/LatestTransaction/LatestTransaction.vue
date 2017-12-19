b77651afbb2a3e0bbfcc6e36b38d25888c49f39f
David Hong <hsurf22@gmail.com>
2017-12-19 10:31:25 -0800

    Table modification

--- src/components/LatestTransaction/LatestTransaction.vue
+++ src/components/LatestTransaction/LatestTransaction.vue
@@ -3,75 +3,111 @@
 
     <Header></Header>
 
-    <div class="standard-table-1">
-      <p class="table-title">
-        <icon name='refresh'
-              scale='1'></icon> Latest Transactions</p>
-      <transition-group name="list">
-        <div class="data-block-loop"
-             v-for="tx in txSplice"
-             v-bind:key="tx.getHash()">
-          
-          <!-- .data-block-1 -->
-          <div class="data-block-1">
-            <div>Hash <span>{{tx.getHash()}}</span></div>
-            <div>Gas <span>{{tx.getGasUsed.toNumber().toString()}}</span></div>
-            <div>Gas Price(Eth) <span>{{tx.getGasPrice.toEth()}}</span></div>
-            <div>Block <span>{{tx.getBlockNumber.toNumber().toString()}}</span></div>
-          </div>
-          <!-- .data-block-1 -->
-          
-          <!-- .data-block-2 -->
-          <div class="data-block-2">
-            <div>
-              <h1>From</h1>
-              <p>{{tx.getFrom()}}</p>
-            </div>
-            <div>
-              <div class="data-icon-container">
-                <icon name='long-arrow-right'
-                      scale='1'></icon> <span>(ETH)</span>
-              </div>
-              <p class="amount">{{tx.getValue.toEth()}}</p>
-            </div>
-            <div>
-              <h1>To</h1>
-              <p>{{tx.getTo()}}</p>
-            </div>
-          </div>
-          <!-- .data-block-2 -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
 
-          <!--sub txs -->
-          <div class="data-block-sub">
-            <div v-for="transfer in tx.getTrace().transfers">
-                <div class="sub-icon">
-                    <icon name='code-fork' scale='1'></icon>
-                </div>
+
+
+          <div class="table-1">
+            <p class="table-title">
+              <icon name='refresh' scale='1'></icon> Latest Transactions
+            </p>
+
+            <div class="data-block-loop">
+              
+
+
+
+
+
+              <!-- .data-block-1 -->
+              <div class="data-block-1">
+                <div>Hash <span>0X02EC1095BCC5A5E1EBEF04A445A628692F1C8ED7A252F57571B1174E2BD9E1B1</span></div>
+                <div>Gas <span>21000</span></div>
+                <div>Gas Price(Eth) <span>0.00000002226</span></div>
+                <div>Block <span>4757137</span></div>
+              </div>
+              <!-- .data-block-1 -->
+              
+
+
+
+
+
+
+              <!-- .data-block-2 -->
+              <div class="data-block-2">
                 <div>
                   <h1>From</h1>
-                  <p>{{transfer.from}}</p>
+                  <p>0xd24400ae8BfEBb18cA49Be86258a3C749cf46853</p>
+                </div>
+                <div>
+                  <div class="data-icon-container">
+                    <icon name='long-arrow-right'
+                          scale='1'></icon> <span>(ETH)</span>
+                  </div>
+                  <p class="amount">8.14501795</p>
                 </div>
                 <div>
                   <h1>To</h1>
-                  <p>{{transfer.to}}</p>
+                  <p>0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE</p>
                 </div>
-                <div>
-                  <h1>Value</h1>
-                  <p>{{transfer.value}}</p>
+              </div>
+              <!-- .data-block-2 -->
+
+
+
+
+
+
+              <!--sub txs -->
+              <div class="data-block-sub">
+                <div class="sub-loop-point">
+                    <div class="sub-icon">
+                        <icon name='code-fork' scale='1'></icon>
+                    </div>
+                    <div class="sub-data-container">
+                      <div class="sub-data-block">
+                        <h1>From</h1>
+                        <p>0x0b95993a39a363d99280ac950f5e4536ab5c5566</p>
+                      </div>
+                      <div class="sub-data-block">
+                        <h1>To</h1>
+                        <p>0x0ec110487c86b87cec37e64561f0ea45a6431992</p>
+                      </div>
+                      <div class="sub-data-block">
+                        <h1>Value</h1>
+                        <p>0x1550f7dca70000</p>
+                      </div>
+                      <div class="sub-data-block">
+                        <h1>Type</h1>
+                        <p>CALL</p>
+                      </div>
+                    </div>
+
                 </div>
-                <div>
-                  <h1>Type</h1>
-                  <p>{{transfer.op}}</p>
-                </div>
+              </div>
+              <!--sub txs -->
+
+
+
+
+
+
             </div>
+            <!-- .data-block-loop -->
+          
           </div>
-          <!--sub txs -->
+          <!-- .table-1 -->
+
+
+
 
         </div>
-        <!-- .data-block-loop -->
-      </transition-group>
+      </div>
     </div>
-    <!-- .standard-table-1 -->
+
 
 
     <Footer></Footer>
@@ -103,6 +139,6 @@ export default Vue.extend({
 })
 </script>
 <style scoped lang="less">
-@import '~lessPath/standardTables';
+@import '~lessPath/LatestTransaction/LatestTransaction';
 
 </style>
