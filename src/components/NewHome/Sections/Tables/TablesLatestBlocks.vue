eb5854e8547dd97908b3528556e053c1f78a81a9
kvhnuke <kvhnuke@aol.com>
2018-01-04 18:11:00 -0800

    latest blocks

--- src/components/NewHome/Sections/Tables/TablesLatestBlocks.vue
+++ src/components/NewHome/Sections/Tables/TablesLatestBlocks.vue
@@ -1,115 +1,138 @@
 <template>
-  <div id="TablesLatestBlocks" class="latest-blocks">
-
-    <p class="block-title">Latest Blocks</p>
-
-    <div class="table-container">
-      <table class="table-header">
-        <thead>
+<div id="TablesLatestBlocks"
+     class="latest-blocks">
+  <p class="block-title">Latest Blocks</p>
+  <div class="table-container">
+    <table class="table-header">
+      <thead>
+        <tr>
+          <td>Hash</td>
+          <td>Miner</td>
+        </tr>
+      </thead>
+    </table>
+    <!-- MAIN LOOP START ########################################## -->
+    <div class="table-content"
+         v-for="block in getBlocks"
+         v-if="!block.getIsUncle()">
+      <table class="top-table-content">
+        <tbody>
           <tr>
-            <td>Hash</td>
-            <td>Miner</td>
+            <td class="top-hash">
+              <p><a :href="'/block/'+block.getHash().toString()">{{block.getHash().toString()}}</a></p>
+            </td>
+            <td class="top-miner">
+              <p>{{block.getMiner().toString()}}</p>
+            </td>
           </tr>
-        </thead>
+        </tbody>
       </table>
-
-      <!-- MAIN LOOP START ########################################## -->
-      <div class="table-content" v-for="block in blocks" v-if="!block.getIsUncle()">
-        <table class="top-table-content">
-          <tbody>
-            <tr>
-              <td class="top-hash"><p>{{block.getHash().toString()}}</p></td>
-              <td class="top-miner"><p>{{block.getMiner().toString()}}</p></td>
-            </tr>
-          </tbody>
-        </table>
-        
-        <table class="mid-table-content">
-          <tbody>
-            <tr>
-              <td><p><icon v-show="uncleCount(block.getUncles()) != 0" name='plus-square-o' scale='1'></icon>&nbsp;{{uncleCount(block.getUncles())}}&nbsp;Uncles</p></td>
-              <td><p><icon name='cubes' scale='1'></icon>&nbsp;{{block.getNumber().toNumber()}}</p></td>
-              <td><p><icon name='exchange' scale='1'></icon>&nbsp;{{block.getTransactionCount()}}</p></td>
-              <td><p><icon name='credit-card-alt' scale='1'></icon>&nbsp;<span>{{block.getTotalBlockReward().toEth()}}</span>&nbsp;ETH</p></td>
-            </tr>
-          </tbody>
-        </table>
-
-        <!-- SUB LOOP START ############## -->
-        <div v-if="uncleCount(block.getUncles()) != 0">
-          <div class="sub-contents">
-            <table>
-              <tbody>
-                <tr>
-                  <td class="sub-height"><p>Height:&nbsp;<span>237534</span></p></td>
-                  <td class="sub-miner"><p>Miner:&nbsp;<span>2iu3498gg9ngn3eg934gun389g37534</span></p></td>
-                  <td class="sub-reward"><p>Reward:&nbsp;<span>237534</span></p></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="sub-contents">
-            <table>
-              <tbody>
-                <tr>
-                  <td class="sub-height"><p>Height:&nbsp;<span>237534</span></p></td>
-                  <td class="sub-miner"><p>Miner:&nbsp;<span>2iu3498gg9ngn3eg934gun389g37534</span></p></td>
-                  <td class="sub-reward"><p>Reward:&nbsp;<span>237534</span></p></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <div class="sub-contents">
-            <table>
-              <tbody>
-                <tr>
-                  <td class="sub-height"><p>Height:&nbsp;<span>237534</span></p></td>
-                  <td class="sub-miner"><p>Miner:&nbsp;<span>2iu3498gg9ngn3eg934gun389g37534</span></p></td>
-                  <td class="sub-reward"><p>Reward:&nbsp;<span>237534</span></p></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+      <table class="mid-table-content">
+        <tbody>
+          <tr>
+            <td>
+              <p>
+                <div v-show="block.getUncleHashes().length"
+                     @click="showUncles[block.getHash().toString()]=!showUncles[block.getHash().toString()]">
+                  <icon :name="!showUncles[block.getHash().toString()] ? 'plus-square-o' : 'minus-square-o'"
+                        scale='1'></icon>
+                </div>&nbsp;{{block.getUncleHashes().length}}&nbsp;Uncles</p>
+            </td>
+            <td>
+              <p>
+                <icon name='cubes'
+                      scale='1'></icon>&nbsp;{{block.getNumber().toNumber()}}</p>
+            </td>
+            <td>
+              <p>
+                <icon name='exchange'
+                      scale='1'></icon>&nbsp;{{block.getTransactionCount()}}</p>
+            </td>
+            <td>
+              <p>
+                <icon name='credit-card-alt'
+                      scale='1'></icon>&nbsp;<span>{{block.getTotalBlockReward().toEth()}}</span>&nbsp;ETH</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- SUB LOOP START ############## -->
+      <div v-if="showUncles[block.getHash().toString()]">
+        <div class="sub-contents" v-for="uncle in block.getUncles()">
+          <table>
+            <tbody>
+              <tr>
+                 <td class="sub-height">
+                  <p>Hash:&nbsp;<span><a :href="'/block/'+block.getHash().toString()">{{uncle.getHash().toString()}}</a></span></p>
+                </td>
+                <td class="sub-height">
+                  <p>Height:&nbsp;<span>{{uncle.getNumber().toNumber()}}</span></p>
+                </td>
+                <td class="sub-miner">
+                  <p>Miner:&nbsp;<span>{{uncle.getMiner().toString()}}</span></p>
+                </td>
+                <td class="sub-reward">
+                  <p>Reward:&nbsp;<span>{{uncle.getTotalBlockReward().toEth()}}</span></p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <!-- SUB LOOP END ############## -->
-
       </div>
-      <!-- MAIN LOOP END ########################################## -->
-
+      <!-- SUB LOOP END ############## -->
     </div>
+    <!-- MAIN LOOP END ########################################## -->
   </div>
+</div>
+
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import store from '@/states'
-import {common} from '@/libs'
-
+import { FIFO, Block, processBlocks } from '@/libs'
+import globConfigs from '@/configs/global.json'
+import sEvents from '@/configs/socketEvents.json'
 export default Vue.extend({
   name: 'TablesLatestBlocks',
+  props: {
+    maxItems: Number
+  },
   data () {
     return {
-      store: store,
-      common: common
+      blocks: new FIFO < Block >(globConfigs.maxTxsInMemory, processBlocks),
+      showUncles: {}
     }
   },
+  beforeMount () {
+    let parent = this
+    this.$socket.emit(sEvents.pastBlocks, '', (_blocks) => {
+      _blocks.forEach((_block) => {
+        let tempBlock = new Block(_block)
+        parent.$set(parent.showUncles, tempBlock.getHash().toString(), false)
+        parent.blocks.add(tempBlock)
+      })
+    })
+  },
+  created () {
+    let parent = this
+    parent.$eventHub.$on(sEvents.newBlock, (_block) => {
+      parent.$set(parent.showUncles, _block.getHash().toString(), false)
+      parent.blocks.add(_block)
+    })
+  },
+  beforeDestroy () {
+    this.$eventHub.$off(sEvents.newBlock)
+  },
   computed: {
-    blocks () {
-      return store.getters.getBlocks.slice(0, 50)
+    getBlocks () {
+      return this.blocks.items().slice(0, this.maxItems)
     }
   },
   methods: {
-    uncleCount (data) {
-      // Returns uncle count
-      if (data) {
-        return Object.keys(data).length
-      } else { return 0 }
-    }
   }
 })
 </script>
 
-<style scoped lang='less'>
-  @import "~lessPath/NewHome/Sections/Tables/TablesLatestBlocks.less";
+<style scoped="" lang="less">
+@import "~lessPath/NewHome/Sections/Tables/TablesLatestBlocks.less";
 </style>
