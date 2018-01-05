2b794ca2080e29042f74ac9ac9b52d52c8e87d5a
kvhnuke <kvhnuke@aol.com>
2018-01-04 18:55:31 -0800

    added last block number and time since last block

--- src/components/NewHome/Sections/ShortData/ShortDataLastBlock.vue
+++ src/components/NewHome/Sections/ShortData/ShortDataLastBlock.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="ShortDataLastBlock" class="last-block">
-
-    <p class="block-title">Last Block</p>
-    
-    <div class="bottom-block">
-      <div class="icon-block"><icon name='cube' scale='1'></icon></div>
-      <p class="block-value">16280901</p>
+<div id="ShortDataLastBlock"
+     class="last-block">
+  <p class="block-title">Last Block</p>
+  <div class="bottom-block">
+    <div class="icon-block">
+      <icon name='cube'
+            scale='1'></icon>
     </div>
-    
+    <p class="block-value">{{latestBlockNumber}}</p>
   </div>
+</div>
+
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  export default Vue.extend({
-    name: 'ShortDataLastBlock',
-    data () {
-      return {
-
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'ShortDataLastBlock',
+  data () {
+    return {}
+  },
+  computed: {
+    latestBlockNumber () {
+      if (!this.$store.getters.getLatestBlock) {
+        return 'loading'
       }
+      return this.$store.getters.getLatestBlock.getIntNumber()
     }
-  })
+  }
+})
 </script>
 
-<style scoped lang="less">
-  @import "~lessPath/NewHome/Sections/ShortData/ShortDataLastBlock.less";
+<style scoped="" lang="less">
+@import "~lessPath/NewHome/Sections/ShortData/ShortDataLastBlock.less";
 </style>
--- src/components/NewHome/Sections/ShortData/ShortDataTimeSinceLastBlock.vue
+++ src/components/NewHome/Sections/ShortData/ShortDataTimeSinceLastBlock.vue
@@ -5,7 +5,7 @@
 
     <div class="bottom-block">
       <div class="icon-block"><icon name='clock-o' scale='1'></icon></div>
-      <p class="block-value">20 Sec</p>
+      <p class="block-value">{{seconds}} Sec</p>
     </div>
     
   </div>
@@ -17,9 +17,20 @@
     name: 'ShortDataLastBlock',
     data () {
       return {
-
+        seconds: 0
       }
-    }
+    },
+    mounted () {
+      let parent = this
+      setInterval(() => {
+        if (!this.$store.getters.getLatestBlock) {
+          parent.seconds = 'loading'
+          return
+        }
+        parent.seconds = new Date().getTime() - this.$store.getters.getLatestBlock.getTimestamp().toDate()
+      }, 1000)
+    },
+    computed: {}
   })
 </script>
 
