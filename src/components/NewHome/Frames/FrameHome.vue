5676098ef82858e8417fb530f85ef9f90f782f42
kvhnuke <kvhnuke@aol.com>
2018-01-24 16:41:42 -0800

    fixed homepage transactions

--- src/components/NewHome/Frames/FrameHome.vue
+++ src/components/NewHome/Frames/FrameHome.vue
@@ -31,19 +31,26 @@
                     <tables-latest-blocks-new :max-items="20"></tables-latest-blocks-new>
             </div>
             <div class="col-md-6 table-data">          
-                   <latest-transactions frompage="from-home" :max-items="20"></latest-transactions>
+                   <table-transactions-new :transactions="txs"></table-transactions-new>
             </div>
         </div>
     </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+const MAX_ITEMS = 20
 export default Vue.extend({
   name: 'FramesHome',
   data () {
     return {}
   },
-  created (){}
+  created (){},
+  computed:{
+    txs(){
+        if(this.$store.getters.getTxs.length) return this.$store.getters.getTxs.slice(0, MAX_ITEMS)
+        else return []
+    }
+  }
 })
 </script>
 <style scoped lang="less">
