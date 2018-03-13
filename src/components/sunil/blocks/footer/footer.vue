98ee2fb02138c4efb2779ef5a4a530b81107fac5
David Hong <hsurf22@gmail.com>
2018-03-12 17:49:48 -0700

    Mailchimp added on footer

--- src/components/sunil/blocks/footer/footer.vue
+++ src/components/sunil/blocks/footer/footer.vue
@@ -31,11 +31,38 @@
           <p>E: <a href="mailto:support@ethvm.com">support@ethvm.com</a></p>
         </div>
         <div class="col-md-6">
-          <div class="input-block">
+          <div class="input-block hidden">
             <h5>Connect With Us</h5>
             <input type="text" name="email" placeholder="Your email address">
             <button v-on:click="signupButton">Sign Up</button>
           </div>
+
+          <div class="input-block">
+            
+            <h5>Connect With Us</h5>
+
+            <!-- Begin MailChimp Signup Form -->
+            <div id="mc_embed_signup">
+              <form action="https://ethvm.us12.list-manage.com/subscribe/post?u=3f151389cc45ad83e9bbb41ed&amp;id=bd3f611f67" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                  <div class="mc-field-group">
+                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                  </div>
+                  <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response" style="display:none"></div>
+                    <div class="response" id="mce-success-response" style="display:none"></div>
+                  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3f151389cc45ad83e9bbb41ed_bd3f611f67" tabindex="-1" value=""></div>
+                  <div class="clear signup-submit"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                </div>
+              </form>
+            </div>
+
+            <!--End mc_embed_signup-->
+
+            
+          </div>
+
           <div class="input-block">
             <h5>Donate <span>Etherium Wallet</span></h5>
             <input type="text" name="email" placeholder="0x829bd824b016326a401d083b33d092293333a830">
