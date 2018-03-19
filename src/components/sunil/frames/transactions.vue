93369df78608dfd8062d6577abf05b4af5f94d58
Olga <olga.kupchevskaya.812@my.csun.edu>
2018-03-19 14:18:40 -0700

    added small blocks to tx page

--- src/components/sunil/frames/transactions.vue
+++ src/components/sunil/frames/transactions.vue
@@ -24,6 +24,12 @@
         </div>
       </div>
 
+      <div class="row small-blocks">
+        <div class="col-md-3"><block-last-block></block-last-block></div>
+        <div class="col-md-3"><successful-tx-small-block></successful-tx-small-block></div>
+        <div class="col-md-3"><failed-tx-small-block></failed-tx-small-block></div>
+        <div class="col-md-3"><pending-tx-small-block></pending-tx-small-block>></div>
+      </div>
         
       <div class="last-transactions-header">
         <li>TXn#</li>
