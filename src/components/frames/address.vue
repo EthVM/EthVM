03bc2bd50c26690c6d829e77e0027383ef73ef7e
Olga <olga.kupchevskaya.812@my.csun.edu>
2018-06-05 17:01:07 -0700

    Tokens UI updates
    Implemented tokens table + header in token tracker
    Omplemnted Loading + Error views

--- src/components/frames/address.vue
+++ src/components/frames/address.vue
@@ -1,52 +1,50 @@
 <template>
-  
   <div class="">
-    
     <div class="container">
-
-
       <div class="page-title-container">
         <div class="page-title">
           <h3>Address Overview</h3>
           <h6 class="text-muted">Ethereum wallet address overview / Transactions information</h6>
         </div>
-
         <div class="search-block">
           <block-search></block-search>
         </div>
       </div>
-
-
       <address-detail :account="account"></address-detail>
-
-
-
+      <!-- Tab Menu -->
       <div class="tab-menu-container">
         <ul class="tabs">
           <li v-on:click="nav1on" v-bind:class="{ active: nav1 }">Transactions</li>
           <li v-on:click="nav2on" v-bind:class="{ active: nav2 }">Tokens</li>
           <li v-on:click="nav3on" v-bind:class="{ active: nav3 }">Network History</li>
           <li v-on:click="nav4on" v-bind:class="{ active: nav4 }">Mining History</li>
- 
-        </ul>      
-
+        </ul>
         <div class="tab-content">
-          <div v-if="nav1 === true" class="">            
-            <block-last-transactions2 :transactions="account.txs" :showheader="true"></block-last-transactions2>
-          </div>
-            <div v-if="nav2 === true" class="" :account="account">
-            <button class="top-right-button-common">More</button>
-            <div class="sub-tab tx-history-container">
-              
-                <ul v-for="token in account.tokens">
-                  <template v-if="token.balance!=0">
-                   <li>{{token.name}} : {{token.symbol}}</li>
-                  <li>Balance: {{token.balance}}</li>
-                  </template>
-
-                   <!-- <li>{{token.email}}</li> -->
-                  </ul> 
+          <!-- Transactions -->
+          <div v-if="nav1 === true" class="">
+            <div class="content" v-if="account.totalTxs === 0">
+              <p> There are no transactions </p>
             </div>
+            <div v-else>
+              <block-last-transactions2 :transactions="account.txs" :showheader="true"></block-last-transactions2>
+            </div>
+            <!-- End Transactions -->
+          </div>
+          <!-- Tokens -->
+          <div v-if="nav2 === true" class="" :account="account">
+            <div v-if="!tokenError">
+              <div v-if="tokensLoaded">
+                <block-token-tracker :tokens="account.tokens"></block-token-tracker>
+              </div>
+              <div v-else class="loading-tokens">
+                <i class="fa fa-spinner fa-pulse fa-4x fa-fw"></i>
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+            <div v-else class="info">
+              <p> Oops Something Went Wrong :( </p>
+            </div>
+            <!-- End Tokens -->
           </div>
           <div v-if="nav3 === true" class="">
             <button class="top-right-button-common">More</button>
@@ -78,24 +76,22 @@
               </ul>
             </div>
           </div>
-        
         </div>
+        <!-- End Tab Menu -->
       </div>
-
-    </div><!-- container -->
-
-
-
+    </div>
+    <!-- container -->
   </div>
-
 </template>
-
 <script lang="ts">
 import Vue from 'vue';
 import bn from 'bignumber.js';
-import { common ,Tx } from '@/libs';
+import {
+  common,
+  Tx
+} from '@/libs';
 import ethUnits from 'ethereumjs-units'
- var utils =  require("../../libs/utils.js")
+var utils = require("../../libs/utils.js")
 
 
 let Account = require('ethereumjs-account')
@@ -118,62 +114,71 @@ export default Vue.extend({
         ethusd: 0,
         totalTxs: this.totalTxs,
         tokens: this.tokens,
-        txs:this.txs
+        txs: this.txs
       },
 
-      nav1 : true,
-      nav2 : false,
-      nav3 : false,
-      nav4 : false,     
-      nav5 : false
+      nav1: true,
+      nav2: false,
+      nav3: false,
+      nav4: false,
+      nav5: false,
+
+      tokensLoaded: false,
+      tokenError: false,
 
     }
   },
-  created(){
+  created() {
     var _this = this;
-     this.$socket.emit('getBalance', this.address, (err, result) => {
+    this.$socket.emit('getBalance', this.address, (err, result) => {
       console.log(err, result)
       if (!err && result) {
-          let balance = common.EthValue(common.HexToBuffer(result.result)).toEth()
-         _this.account.balance = balance
-         console.log("account balance in eth: " + _this.account.balance)
-  
-      ethtousd().then(function(resp){
-        console.log("price: " +resp[0].price_usd)
-        _this.account.ethusd = resp[0].price_usd
-      })
+        let balance = common.EthValue(common.HexToBuffer(result.result)).toEth()
+        _this.account.balance = balance
+        console.log("account balance in eth: " + _this.account.balance)
 
-  
- 
-       }
+        ethtousd().then(function(resp) {
+          console.log("price: " + resp[0].price_usd)
+          _this.account.ethusd = resp[0].price_usd
+        })
+
+
+
+      }
     })
 
     this.$socket.emit('getTokenBalance', this.address, (err, result) => {
       console.log(err, result)
-       _this.account.tokens = utils.decode(result.result)
+      //_this.account.tokens = utils.decode(result.result)
+      console.log(_this.account.tokens)
+      if (result.result !="0x") {
+        _this.account.tokens = utils.decode(result.result)
+        _this.tokensLoaded = true
+      }
+      else {
+        _this.tokenError = true
+      }
     })
 
     this.$socket.emit('getTotalTxs', this.address, (err, result) => {
       console.log(err, result)
       _this.account.totalTxs = result;
 
-    
-       
     })
 
     this.$socket.emit('getTxs', this.address, (err, result) => {
       var txs = [];
-       result.forEach(element => {
-          txs.push(new Tx(element))
-       });
+      result.forEach(element => {
+        txs.push(new Tx(element))
+      });
       _this.account.txs = txs;
     })
 
-    
+
 
   },
   methods: {
-    alloff(){
+    alloff() {
       this.nav1 = false
       this.nav2 = false
       this.nav3 = false
@@ -181,32 +186,32 @@ export default Vue.extend({
       this.nav5 = false
     },
 
-    nav1on(){
+    nav1on() {
       this.alloff()
       this.nav1 = true
     },
 
-    nav2on(){
+    nav2on() {
       this.alloff()
       this.nav2 = true
     },
 
-    nav3on(){
+    nav3on() {
       this.alloff()
       this.nav3 = true
     },
 
-    nav4on(){
+    nav4on() {
       this.alloff()
       this.nav4 = true
     },
 
-    nav5on(){
+    nav5on() {
       this.alloff()
       this.nav5 = true
     }
   },
-  computed:{
+  computed: {
     // txs(){
     //     if(this.$store.getters.getTxs.length) return this.$store.getters.getTxs.slice(0, MAX_ITEMS)
     //     else return []
@@ -215,23 +220,17 @@ export default Vue.extend({
 
 })
 
-async function ethtousd(){
- const response =  await fetch("https://api.coinmarketcap.com/v1/ticker/ethereum/", {
- 	// mode: 'cors', 
- 	// headers: new Headers({
-	// 	'Content-Type': 'text/plain'
-  // })
-  }) 
+async function ethtousd() {
+  const response = await fetch("https://api.coinmarketcap.com/v1/ticker/ethereum/", {
+    // mode: 'cors', 
+    // headers: new Headers({
+    //  'Content-Type': 'text/plain'
+    // })
+  })
   const parsedResponse = await response.json()
-  return  parsedResponse
+  return parsedResponse
 }
-
- 
-
-
-
 </script>
-
 <style scoped="" lang="less">
-  @import "~lessPath/sunil/frames/address.less";
+@import "~lessPath/sunil/frames/address.less";
 </style>
