8593aa1a97eed6e8c93d9bd721b51c12ae251f68
olgakup <olga.kupchevskaya.812@my.csun.edu>
2018-08-14 16:17:26 -0700

    search strings added and implemented

--- src/components/blocks/largeBlocks/addressTx.vue
+++ src/components/blocks/largeBlocks/addressTx.vue
@@ -31,7 +31,7 @@
     props: ['address', 'transactions', 'isPending'],
     data() {
       return {
-        placeholder: 'Search Address/Tx Hash',
+        placeholder: 'addressTxSearch',
         options: [{
             text: 'All',
             value: 'all'
