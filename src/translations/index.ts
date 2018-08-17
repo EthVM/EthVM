c0882a525241ca6c489f7af44957e4344fa1bc9c
Aldo Borrero González <aldo@aldoborrero.com>
2018-08-18 00:32:05 +0200

    Removed unused string and deleted ja_JP file

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import * as en_US from './en_US.json'
-import * as ja_JP from './ja_JP.json'
 
 const defaultLocale: string = 'en_US'
 const messages: any = {
   en_US,
-  ja_JP
 }
 
 Vue.use(VueI18n)
