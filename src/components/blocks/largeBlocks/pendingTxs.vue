e19d06651ad435bf38432011b3529d5bacd5d1b9
olgakup <olga.kupchevskaya.812@my.csun.edu>
2018-08-17 13:02:01 -0700

    edited pendt tx table header

--- src/components/blocks/largeBlocks/pendingTxs.vue
+++ src/components/blocks/largeBlocks/pendingTxs.vue
@@ -3,10 +3,10 @@
     <div class="block-body">
       <!-- Table Header -->
       <div class="tx-table-header" v-if="showHeader === true">
-        <li>TXn#</li>
-        <li class="eth">ETH</li>
-        <li class="limit">Gas Limit</li>
-        <li class="gas">GWEI</li>
+        <li>{{ $t('tableHeader.txN') }} </li>
+        <li class="eth">{{ $t('common.eth') }}</li>
+        <li class="limit">{{ $t( 'gas.limit' ) }}</li>
+        <li class="gas">{{ $t( 'common.gwei' ) }}</li>
         <li></li>
         <!-- End Table Header -->
       </div>
