139191002039e5a7d1672b2abe779f272199aad0
Kosala Hemachandra <kvhnuke@aol.com>
2018-08-21 15:56:40 -0700

    es-lint fix

--- apps/ethvm/src/states/actions.ts
+++ apps/ethvm/src/states/actions.ts
@@ -3,17 +3,17 @@ import sEvents from '@app/configs/socketEvents.json'
 import { Block, Tx } from '@app/libs'
 import { BlockLayout, TxLayout } from '@app/typeLayouts'
 
-const socket_socketNewblock = function ({ commit }, block: BlockLayout | BlockLayout[])  {
+const socket_socketNewblock = function({ commit }, block: BlockLayout | BlockLayout[]) {
   commit(sEvents.newBlock, block)
   this._vm.$eventHub.$emit(sEvents.newBlock, Array.isArray(block) ? new Block(block[0]) : new Block(block))
 }
 
-const socket_socketNewtx = function ({ commit }, tx: TxLayout | TxLayout[]) {
+const socket_socketNewtx = function({ commit }, tx: TxLayout | TxLayout[]) {
   commit(sEvents.newTx, tx)
   this._vm.$eventHub.$emit(sEvents.newTx, Array.isArray(tx) ? new Tx(tx[0]) : new Tx(tx))
 }
-
-const socket_socketConnect = function ({}, tx: TxLayout) {
+// eslint-disable-next-line
+const socket_socketConnect = function({}, tx: TxLayout) {
   this._vm.$socket.emit(sEvents.join, { rooms: defaultRooms })
 }
 
