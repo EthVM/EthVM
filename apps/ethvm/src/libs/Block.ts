be1d567bb40e24b45ac9c2d0ad150b45d9d09d69
Aldo Borrero González <aldo@aldoborrero.com>
2018-08-22 20:46:07 +0200

    Formatted libs classes

--- apps/ethvm/src/libs/Block.ts
+++ apps/ethvm/src/libs/Block.ts
@@ -6,17 +6,21 @@ class Block {
   public readonly id: string
   private readonly block: BlockLayout
   private cache: any
+
   constructor(block: BlockLayout) {
     this.cache = {}
     this.block = block
     this.id = common.Hash(this.block.hash).toString()
   }
+
   public getId(): string {
     return this.id
   }
+
   public setTransactions(txs: Tx[]): void {
     this.block.transactions = txs
   }
+
   public setIsUncle(isUncle: boolean): void {
     if (isUncle) {
       this.setTransactions([])
@@ -25,50 +29,61 @@ class Block {
     }
     this.block.isUncle = isUncle
   }
+
   public setUncles(uncles: Block[]): void {
     this.block.uncles = uncles
   }
+
   public addUncle(uncle: Block): void {
     if (!this.block.uncles) {
       this.block.uncles = []
     }
     this.block.uncles.push(uncle)
   }
+
   public getIsUncle(): boolean {
     if (!this.cache.isUncle) {
       this.cache.isUncle = this.block.isUncle
     }
     return this.cache.isUncle
   }
+
   public getUncles(): Block[] {
     return this.block.uncles
   }
+
   public getUncleHashes(): Hash[] {
     return this.block.uncleHashes.map(_uncle => {
       return common.Hash(_uncle)
     })
   }
+
   public setUncleHashes(hashes: Hash[]): void {
     this.block.uncleHashes = hashes
   }
+
   public getHash(): Hash {
     if (!this.cache.hash) {
       this.cache.hash = common.Hash(this.block.hash)
     }
     return this.cache.hash
   }
+
   public getIntNumber(): number {
     return this.block.intNumber
   }
+
   public getNumber(): HexNumber {
     if (!this.cache.number) {
       this.cache.number = common.HexNumber(this.block.number)
     }
     return this.cache.number
   }
+
   public getTransactionCount(): number {
     return typeof this.block.transactionCount !== 'undefined' ? this.block.transactionCount : this.block.transactionHashes.length
   }
+
   public getTotalBlockReward(): EthValue {
     if (!this.cache.totalBlockReward) {
       this.cache.totalBlockReward = this.block.totalBlockReward
@@ -86,30 +101,35 @@ class Block {
 
     return this.cache.totalBlockReward
   }
+
   public getParentHash(): Hash {
     if (!this.cache.parentHash) {
       this.cache.parentHash = common.Hash(this.block.parentHash)
     }
     return this.cache.parentHash
   }
+
   public getNonce(): Hex {
     if (!this.cache.nonce) {
       this.cache.nonce = common.Hex(this.block.nonce)
     }
     return this.cache.nonce
   }
+
   public getMixHash(): Hash {
     if (!this.cache.mixHash) {
       this.cache.mixHash = common.Hash(this.block.mixHash)
     }
     return this.cache.mixHash
   }
+
   public getSha3Uncles(): Hash {
     if (!this.cache.sha3Uncles) {
       this.cache.sha3Uncles = common.Hash(this.block.sha3Uncles)
     }
     return this.cache.sha3Uncles
   }
+
   public getLogsBloom(): Hex {
     if (!this.cache.logsBloom) {
       this.cache.logsBloom = common.Hex(this.block.logsBloom)
@@ -123,18 +143,21 @@ class Block {
     }
     return this.cache.stateRoot
   }
+
   public getMiner(): Address {
     if (!this.cache.miner) {
       this.cache.miner = common.Address(this.block.miner)
     }
     return this.cache.miner
   }
+
   public getMinerBalance(): EthValue {
     if (!this.cache.minerBalance) {
       this.cache.minerBalance = common.EthValue(this.block.minerBalance)
     }
     return this.cache.minerBalance
   }
+
   public getDifficulty(): HexNumber {
     if (!this.cache.difficulty) {
       this.cache.difficulty = common.HexNumber(this.block.difficulty)
@@ -148,51 +171,60 @@ class Block {
     }
     return this.cache.totalDifficulty
   }
+
   public getExtraData(): Hex {
     if (!this.cache.extraData) {
       this.cache.extraData = common.Hex(this.block.extraData)
     }
     return this.cache.extraData
   }
+
   public getSize(): HexNumber {
     if (!this.cache.size) {
       this.cache.size = common.HexNumber(this.block.size)
     }
     return this.cache.size
   }
+
   public getGasLimit(): HexNumber {
     if (!this.cache.gasLimit) {
       this.cache.garLimit = common.HexNumber(this.block.gasLimit)
     }
     return this.cache.garLimit
   }
+
   public getGasUsed(): HexNumber {
     if (!this.cache.gasUsed) {
       this.cache.gasUsed = common.HexNumber(this.block.gasUsed)
     }
     return this.cache.gasUsed
   }
+
   public getTimestamp(): HexTime {
     if (!this.cache.timestamp) {
       this.cache.timestamp = common.HexTime(this.block.timestamp)
     }
     return this.cache.timestamp
   }
+
   public getTransactionsRoot(): Hash {
     if (!this.cache.transactionsRoot) {
       this.cache.transactionsRoot = common.Hash(this.block.transactionsRoot)
     }
     return this.cache.transactionsRoot
   }
+
   public getReceiptsRoot(): Hash {
     if (!this.cache.receiptsRoot) {
       this.cache.receiptsRoot = common.Hash(this.block.receiptsRoot)
     }
     return this.cache.receiptsRoot
   }
+
   public getTransactions(): Tx[] {
     return this.block.transactions
   }
+
   public geTransactionHashes(): Hash[] {
     if (!this.cache.transactionHashes) {
       this.cache.transactionHashes = this.block.transactionHashes.map((_hash, idx) => {
@@ -201,24 +233,28 @@ class Block {
     }
     return this.cache.transactionHashes
   }
+
   public getTxFees(): EthValue {
     if (!this.cache.txFees) {
       this.cache.txFees = common.EthValue(this.block.txFees)
     }
     return this.cache.txFees
   }
+
   public getBlockReward(): EthValue {
     if (!this.cache.blockReward) {
       this.cache.blockReward = common.EthValue(this.block.blockReward)
     }
     return this.cache.blockReward
   }
+
   public getUncleReward(): EthValue {
     if (!this.cache.uncleReward) {
       this.cache.uncleReward = common.EthValue(this.block.uncleReward)
     }
     return this.cache.uncleReward
   }
+
   public getStats(): BlockLayout['blockStats'] {
     return this.block.blockStats
   }
--- apps/ethvm/src/libs/Tx.ts
+++ apps/ethvm/src/libs/Tx.ts
@@ -11,57 +11,67 @@ class Tx {
     this.tx = tx
     this.id = common.Hash(this.tx.hash).toString()
   }
+
   public getId(): string {
     return this.id
   }
+
   public getHash(): Hash {
     if (!this.cache.hash) {
       this.cache.hash = common.Hash(this.tx.hash)
     }
     return this.cache.hash
   }
+
   public getTo(): Address {
     if (!this.cache.to) {
       this.cache.to = common.Address(this.tx.to)
     }
     return this.cache.to
   }
+
   public getFrom(): Address {
     if (!this.cache.from) {
       this.cache.from = common.Address(this.tx.from)
     }
     return this.cache.from
   }
+
   public getGasUsed(): HexNumber {
     if (!this.cache.gasUsed) {
       this.cache.gasUsed = common.HexNumber(this.tx.gasUsed)
     }
     return this.cache.gasUsed
   }
+
   public getBlockHash(): Hash {
     if (!this.cache.blcokHash) {
       this.cache.blcokHash = common.Hash(this.tx.blockHash)
     }
     return this.cache.blcokHash
   }
+
   public getBlockNumber(): HexNumber {
     if (!this.cache.blockNumber) {
       this.cache.blockNumber = common.HexNumber(this.tx.blockNumber)
     }
     return this.cache.blockNumber
   }
+
   public geTransactionIndex(): HexNumber {
     if (!this.cache.transactionIndex) {
       this.cache.transactionIndex = common.HexNumber(this.tx.transactionIndex)
     }
     return this.cache.transactionIndex
   }
+
   public getFromBalance(): EthValue {
     if (!this.cache.fromBalance) {
       this.cache.fromBalance = common.EthValue(this.tx.fromBalance)
     }
     return this.cache.fromBalance
   }
+
   public getToBalance(): EthValue {
     if (!this.cache.ethValue) {
       this.cache.ethValue = common.EthValue(this.tx.toBalance)
@@ -75,24 +85,28 @@ class Tx {
     }
     return this.cache.cumulativeGasUsed
   }
+
   public getContractAddress(): Address {
     if (!this.cache.contractAddress) {
       this.cache.contractAddress = common.Address(this.tx.contractAddress)
     }
     return this.cache.contractAddress
   }
+
   public getLogsBloom(): Hex {
     if (!this.cache.logsBloom) {
       this.cache.logsBloom = common.Hex(this.tx.logsBloom)
     }
     return this.cache.logsBloom
   }
+
   public getGas(): HexNumber {
     if (!this.cache.gas) {
       this.cache.gas = common.HexNumber(this.tx.gas)
     }
     return this.cache.gas
   }
+
   public getGasPrice(): EthValue {
     if (!this.cache.gasPrice) {
       this.cache.gasPrice = common.EthValue(this.tx.gasPrice)
@@ -106,42 +120,50 @@ class Tx {
     }
     return this.cache.input
   }
+
   public getNonce(): HexNumber {
     if (!this.cache.hexNumber) {
       this.cache.hexNumber = common.HexNumber(this.tx.nonce)
     }
     return this.cache.hexNumber
   }
+
   public getValue(): EthValue {
     if (!this.cache.ethValue) {
       this.cache.ethValue = common.EthValue(this.tx.value)
     }
     return this.cache.ethValue
   }
+
   public getV(): Hex {
     if (!this.cache.v) {
       this.cache.v = common.Hex(this.tx.v)
     }
     return this.cache.v
   }
+
   public getR(): Hex {
     if (!this.cache.r) {
       this.cache.r = common.Hex(this.tx.r)
     }
     return this.cache.r
   }
+
   public getS(): Hex {
     if (!this.cache.s) {
       this.cache.s = common.Hex(this.tx.s)
     }
     return this.cache.s
   }
+
   public getStatus(): boolean {
     return this.tx.status
   }
+
   public isPending(): boolean {
     return this.tx.pending
   }
+
   public getTimestamp(): HexTime {
     if (!this.cache.timestamp) {
       this.cache.timestamp = common.HexTime(this.tx.timestamp)
