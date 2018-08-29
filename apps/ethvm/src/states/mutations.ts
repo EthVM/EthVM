86a2a4f4cf7a4bb5bf9371352970e3ef3d12e3aa
Aldo Borrero González <aldo@aldoborrero.com>
2018-08-29 13:08:46 +0200

    Formatted code and simplified mutations on ethvm

--- apps/ethvm/src/states/mutations.ts
+++ apps/ethvm/src/states/mutations.ts
@@ -1,24 +1,24 @@
 import { Block, Tx } from '@app/models'
 import { BlockLayout, StateLayout, TxLayout } from '@app/models/server'
 
-const NEW_BLOCK = (state: StateLayout, block: BlockLayout | BlockLayout[]) => {
-  if (Array.isArray(block)) {
-    block.forEach(_block => {
-      state.blocks.add(new Block(_block))
-    })
-  } else {
-    state.blocks.add(new Block(block))
+const NEW_BLOCK = (state: StateLayout, blocks: BlockLayout | BlockLayout[]) => {
+  if (!Array.isArray(blocks)) {
+    blocks = [blocks]
   }
+
+  blocks.forEach(block => {
+    state.blocks.add(new Block(block))
+  })
 }
 
-const NEW_TX = (state: StateLayout, tx: TxLayout | TxLayout[]) => {
-  if (Array.isArray(tx)) {
-    tx.forEach(_tx => {
-      state.txs.add(new Tx(_tx))
-    })
-  } else {
-    state.txs.add(new Tx(tx))
+const NEW_TX = (state: StateLayout, txs: TxLayout | TxLayout[]) => {
+  if (!Array.isArray(txs)) {
+    txs = [txs]
   }
+
+  txs.forEach(tx => {
+    state.txs.add(new Tx(tx))
+  })
 }
 
 export default {
