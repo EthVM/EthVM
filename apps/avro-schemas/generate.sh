b433d4a3e8c4e255d4323d601f09a82c1e81af52
Aldo Borrero González <aldo@aldoborrero.com>
2018-09-06 18:29:25 +0200

    Updated avro schemas

---
+++ apps/avro-schemas/generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gogen-avro --package=ethvm go block.schema.v1.asvc pendingtx.schema.v1.asvc
+
+avro-tools compile schema block.schema.v1.asvc java
+avro-tools compile schema pendingtx.schema.v1.asvc java
