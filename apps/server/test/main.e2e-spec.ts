2f5e06d28f45cb4d27e50fce0405a998e4ec0e6e
niktrix <nicky.gurbani@gmail.com>
2018-09-06 14:33:49 +0530

    block, pasttx tests

--- apps/server/test/main.e2e-spec.ts
+++ apps/server/test/main.e2e-spec.ts
@@ -5,7 +5,7 @@ import { Block, BlocksService,BlocksServiceImpl, RethinkBlockRepository } from '
 import { ChartService, ChartsServiceImpl } from '@app/server/modules/charts'
 import { ExchangeService, MockExchangeServiceImpl } from '@app/server/modules/exchanges'
 import { RethinkTxsRepository, Tx, TxsRepository, TxsService, TxsServiceImpl } from '@app/server/modules/txs'
-import { VmService } from '@app/server/modules/vm'
+import { VmService,VmServiceImpl } from '@app/server/modules/vm'
 import { RedisCacheRepository } from '@app/server/repositories'
 import { expect } from 'chai'
 import * as r from 'rethinkdb'
@@ -14,7 +14,7 @@ import { mock, when } from 'ts-mockito'
 
 
 // Increase Jest timeout for safety
-jest.setTimeout(30000)
+jest.setTimeout(50000)
 
 class VmServiceImpl implements VmService {
   public setStateRoot(hash: Buffer): Promise<boolean> {
@@ -36,8 +36,6 @@ class VmServiceImpl implements VmService {
     return Promise.resolve(Buffer.from(''))
   }
 }
-
-
 
 function callEvent(ev, payload, client): Promise<any> {
   return new Promise((resolve, reject) => {
@@ -79,7 +77,7 @@ describe('ethvm-server-events', () => {
     const blockService = new BlocksServiceImpl(blocksRepository, ds)
 
     const txsRepository = new RethinkTxsRepository(rConn, rethinkOpts)
-    const txsService = new TxsServiceImpl(txsRepository, ds)
+    const txsService:TxsService = new TxsServiceImpl(txsRepository, ds)
 
     const chartsService: ChartService = mock(ChartsServiceImpl)
     const exchangeService: ExchangeService = new MockExchangeServiceImpl()
@@ -103,15 +101,15 @@ describe('ethvm-server-events', () => {
     it('should return Promise<Tx[]>', async () => {
       const inputs = [
         {
-          address: '0xd9ea042ad059033ba3c3be79f4081244f183bf03',
-          limit: 0,
+          address: '0x8b2a6d0b4183b5db91bb901eefdd0d0ba06ef125',
+          limit: 10,
           page: 0
         }
       ]
 
       for (const input of inputs) {
         const data = await callEvent('getTxs', input, client)
-        expect(data).to.have.lengthOf(0)
+        expect(data).to.have.lengthOf(1)
       }
     })
 
@@ -233,11 +231,12 @@ describe('ethvm-server-events', () => {
   // })
 
 
-  describe('getTx Event', () => {
+  // need trace and log table
+  describe.skip('getTx Event', () => {
     it('should return Promise<Tx>', async () => {
       const inputs = [
         {
-          hash: '0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238'
+          hash: '0xff7ac9e368c483f73d34595780cdee65e8d44c40c26ff8bd3ce53c48035a863e'
         }
       ]
 
@@ -282,13 +281,13 @@ describe('ethvm-server-events', () => {
     it('should return Promise<number>', async () => {
       const inputs = [
         {
-          address: '0xd9ea042ad059033ba3c3be79f4081244f183bf03'
+          address: '0x8b2a6d0b4183b5db91bb901eefdd0d0ba06ef125'
         }
       ]
 
       for (const input of inputs) {
         const data = await callEvent('getTotalTxs', input, client)
-        expect(data).to.equal(9)
+        expect(data).to.equal(1)
       }
     })
 
@@ -370,13 +369,14 @@ describe('ethvm-server-events', () => {
     it('should return Promise<Tx[]>', async () => {
       const inputs = [
         {
-          limit: 1,
-          page: 1
+          limit: 10,
+          page: 0
         }
       ]
 
       for (const input of inputs) {
         const data = await callEvent('pastTxs', input, client)
+        //timeout happens here
         expect(data).to.have.lengthOf(2)
       }
     })
@@ -410,16 +410,61 @@ describe('ethvm-server-events', () => {
     })
   })
 
+  describe('pastBlocks', () => {
+    it('should return Promise<Block[]>', async () => {
+      const inputs = [
+        {
+          limit: 10,
+          page: 0
+        }
+      ]
+
+      for (const input of inputs) {
+        const data = await callEvent('pastBlocks', input, client)
+        expect(data).to.have.lengthOf(2)
+      }
+    })
+
+    it('should return err ', async () => {
+      const inputs = [
+        '',
+        '0x',
+        '0x0',
+        {},
+        {
+          address: '0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238'
+        },
+        {
+          address: '0xd9ea042ad059033ba3c3be79f4081244f183bf03',
+          limit: '1',
+          page: 1
+        },
+        {
+          number: 1
+        }
+      ]
+
+      for (const  input of  inputs) {
+        try {
+          const data = await callEvent('pastBlocks', input, client)
+        } catch (e) {
+          expect(e).to.not.be.undefined
+        }
+      }
+    })
+  })
+
   describe('getBlock', () => {
     it('should return Promise<Block>', async () => {
       const inputs = [
         {
-          hash: '0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238'
+          hash: '0x0041061b4de06bb3243312dc0795f8b2ee6a40611d86f401a9679fb0c0bee1bf'
         }
       ]
 
       for (const input of inputs) {
         const data = await callEvent('getBlock', input, client)
+
         expect(data).to.be.not.undefined
       }
     })
