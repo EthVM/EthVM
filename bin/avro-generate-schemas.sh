00695b5a4b5aaf4a802aa27320bda99350891812
Aldo Borrero González <aldo@aldoborrero.com>
2018-09-10 22:48:47 +0200

    Updated generation path for avro schemas

--- bin/avro-generate-schemas.sh
+++ bin/avro-generate-schemas.sh
@@ -5,7 +5,7 @@ AVRO_TOOLS="docker run --rm -v $(pwd):/share enkryptio/avro-tools"
 GO_GEN="${AVRO_TOOLS} go"
 JAVA_GEN="${AVRO_TOOLS} java"
 
-${GO_GEN} --package=models /share/apps/avro-schemas/go /share/apps/avro-schemas/block.schema.v1.asvc /share/apps/avro-schemas/pendingtx.schema.v1.asvc
+${GO_GEN} --package=models /share/apps/eth/ethvm/models /share/apps/avro-schemas/block.schema.v1.asvc /share/apps/avro-schemas/pendingtx.schema.v1.asvc
 
-${JAVA_GEN} compile schema /share/apps/avro-schemas/block.schema.v1.asvc /share/apps/avro-schemas/java
-${JAVA_GEN} compile schema /share/apps/avro-schemas/pendingtx.schema.v1.asvc /share/apps/avro-schemas/java
+${JAVA_GEN} compile schema /share/apps/avro-schemas/block.schema.v1.asvc /share/apps/bolt/src/main/kotlin/io/enkrypt/bolt/models
+${JAVA_GEN} compile schema /share/apps/avro-schemas/pendingtx.schema.v1.asvc /share/apps/bolt/src/main/kotlin/io/enkrypt/bolt/models
