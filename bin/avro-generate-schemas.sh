e37f08cf55bcf2e69e762229d74d89daa841c875
Brian McGee <brian@bluerosedev.com>
2018-09-11 09:42:18 +0200

    Fix kotlin avro generation path

--- bin/avro-generate-schemas.sh
+++ bin/avro-generate-schemas.sh
@@ -7,5 +7,5 @@ JAVA_GEN="${AVRO_TOOLS} java"
 
 ${GO_GEN} --package=models /share/apps/eth/ethvm/models /share/apps/avro-schemas/block.schema.v1.asvc /share/apps/avro-schemas/pendingtx.schema.v1.asvc
 
-${JAVA_GEN} compile schema /share/apps/avro-schemas/block.schema.v1.asvc /share/apps/bolt/src/main/kotlin/io/enkrypt/bolt/models
-${JAVA_GEN} compile schema /share/apps/avro-schemas/pendingtx.schema.v1.asvc /share/apps/bolt/src/main/kotlin/io/enkrypt/bolt/models
+${JAVA_GEN} compile schema /share/apps/avro-schemas/block.schema.v1.asvc /share/apps/bolt/src/main/kotlin
+${JAVA_GEN} compile schema /share/apps/avro-schemas/pendingtx.schema.v1.asvc /share/apps/bolt/src/main/kotlin
