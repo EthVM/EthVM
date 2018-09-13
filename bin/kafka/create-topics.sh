bab16c284bcfcdebe652a5fa439ea8e1aeb28ab6
Aldo Borrero González <aldo@aldoborrero.com>
2018-09-13 14:22:37 +0200

    Updated kafka scripts

--- bin/kafka/create-topics.sh
+++ bin/kafka/create-topics.sh
@@ -14,3 +14,13 @@ docker-compose exec kafka sh -c "$CMD"
 CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 100 --topic raw-pending-txs"
 echo "COMMAND: $CMD"
 docker-compose exec kafka sh -c "$CMD"
+
+# processed blocks
+CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic blocks"
+echo "COMMAND: $CMD"
+docker-compose exec kafka sh -c "$CMD"
+
+# raw-blocks retention
+CMD="kafka-configs --alter --zookeeper zookeeper:2181 --entity-type topics --entity-name blocks --add-config retention.ms=300000"
+echo "COMMAND: $CMD"
+docker-compose exec kafka sh -c "$CMD"
--- bin/kafka/register-avro-schemas.sh
+++ bin/kafka/register-avro-schemas.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$(cd ${SCRIPT_DIR}/../..; pwd)
 
-
 export SCHEMA=$(jq tostring ${ROOT_DIR}/apps/avro-schemas/block.schema.v1.asvc)
 curl -H "Content-Type: application/vnd.schemaregistry.v1+json" -X POST -d"{\"schema\":$SCHEMA}" http://kafka-schema-registry.ethvm.lan/subjects/raw-blocks-value/versions
+curl -H "Content-Type: application/vnd.schemaregistry.v1+json" -X POST -d"{\"schema\":$SCHEMA}" http://kafka-schema-registry.ethvm.lan/subjects/blocks-value/versions
 
 export SCHEMA=$(jq tostring ${ROOT_DIR}/apps/avro-schemas/pendingtx.schema.v1.asvc)
 curl -H "Content-Type: application/vnd.schemaregistry.v1+json" -X POST -d"{\"schema\":$SCHEMA}" http://kafka-schema-registry.ethvm.lan/subjects/raw-pending-txs-value/versions
