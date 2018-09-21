84c49cd0e20c7760b3d25be7d07a0d7a81ed022a
Brian McGee <brian@bluerosedev.com>
2018-09-21 18:04:45 +0200

    Changes to kafka setup

--- bin/kafka/create-topics.sh
+++ bin/kafka/create-topics.sh
@@ -1,23 +1,26 @@
 #!/bin/bash -e
 
-# raw-blocks
-CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic raw-blocks --config retention.ms=-1"
+# blocks
+CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic blocks --config retention.ms=-1 --config cleanup.policy=compact"
 echo "COMMAND: $CMD"
 docker-compose exec kafka sh -c "$CMD"
 
-CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic latest-blocks --config retention.ms=10800000"
+# block info
+CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic blocks-info --config retention.ms=-1 --config cleanup.policy=compact"
 echo "COMMAND: $CMD"
 docker-compose exec kafka sh -c "$CMD"
 
-CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 100 --topic contracts --config cleanup.policy=compact"
+# state
+CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic state --config retention.ms=-1 --config cleanup.policy=compact"
 echo "COMMAND: $CMD"
 docker-compose exec kafka sh -c "$CMD"
 
-CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 100 --topic balances --config cleanup.policy=compact"
+# transactions
+CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic transactions --config retention.ms=-1 --config cleanup.policy=compact"
 echo "COMMAND: $CMD"
 docker-compose exec kafka sh -c "$CMD"
 
-# pending transactions
-CMD="kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 100 --topic raw-pending-txs"
-echo "COMMAND: $CMD"
-docker-compose exec kafka sh -c "$CMD"
+
+
+
+
