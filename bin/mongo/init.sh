3b863625659009a82afbfe66829d28e7232f3eb5
Aldo Borrero González <aldo@aldoborrero.com>
2018-09-24 16:34:56 +0200

    Added forgotten script detector for zsh

--- bin/mongo/init.sh
+++ bin/mongo/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
+if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+SCRIPT_DIR="$( cd "$( dirname "${(%):-%x}" )" && pwd )"
+elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
 
 MONGO="docker-compose exec -T mongodb mongo"
 SCRIPT=$(cat ${SCRIPT_DIR}/init.js)
