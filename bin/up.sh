2d73e2adbe0eb8035bb6be297acbfc6c8ad4f83c
Aldo Borrero González <aldo@aldoborrero.com>
2018-09-25 15:15:17 +0200

    Updated up.sh script to start bolt service

--- bin/up.sh
+++ bin/up.sh
@@ -3,7 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$(cd ${SCRIPT_DIR}/..; pwd)
 
-# create containers
 echo "Building containers..."
 CMD="docker-compose build"
 echo "Executing: ${CMD}"
@@ -14,18 +13,28 @@ CMD="docker-compose up -d traefik kafka-schema-registry mongodb"
 echo "Executing: ${CMD}"
 ${CMD}
 
-echo "Waiting 15 seconds to allow docker services initialisation..."
+echo "Waiting 15 seconds to allow previous docker containers initialisation..."
 sleep 15
 
+echo "Creating kafka topics..."
 CMD="${SCRIPT_DIR}/kafka/init.sh"
 echo "Executing: ${CMD}"
 ${CMD}
 
+echo "Initialisation of mongo"
 CMD="${SCRIPT_DIR}/mongo/init.sh"
 echo "Executing: ${CMD}"
 ${CMD}
 
-echo "Starting ethereumj container..."
+echo "Starting up container: ethereumj"
 CMD="docker-compose up -d ethereumj"
+echo "Executing: ${CMD}"
+${CMD}
+
+echo "Waiting 15 secs to allow ethereumj to compile (it will take a lot more than that...)"
+sleep 15
+
+echo "Starting up container: bolt"
+CMD="docker-compose up -d bolt"
 echo "Executing: ${CMD}"
 ${CMD}
