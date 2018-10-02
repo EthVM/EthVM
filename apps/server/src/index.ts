ed05a87e2930d39a0a386fc75a0f02d279087557
Aldo Borrero González <aldo@aldoborrero.com>
2018-10-02 20:57:45 +0200

    Disabled for now kafka-streamer and added null-streamer

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,6 @@
 import config from '@app/config'
 import { logger } from '@app/logger'
-import { KafkaStreamer, KafkaStreamerOpts } from '@app/server/core/streams'
+import { NullStreamer } from '@app/server/core/streams'
 import { EthVMServer } from '@app/server/ethvm-server'
 import { AddressServiceImpl, MongoAddressRepository } from '@app/server/modules/address'
 import { BlocksServiceImpl, MongoBlockRepository } from '@app/server/modules/blocks'
@@ -106,18 +106,19 @@ async function bootstrapServer() {
   // Create streamer
   // ---------------
   logger.debug('bootstrapper -> Initializing streamer')
-  const kafkaStreamerOpts: KafkaStreamerOpts = {
-    groupId: config.get('streamer.kafka.group_id'),
-    brokers: config.get('streamer.kafka.brokers'),
-    blocksTopic: config.get('streamer.kafka.topics.blocks'),
-    pendingTxsTopic: config.get('streamer.kafka.topics.pending_txs')
-  }
-  const streamer = new KafkaStreamer(kafkaStreamerOpts, emitter)
-  await streamer.initialize()
+  // const kafkaStreamerOpts: KafkaStreamerOpts = {
+  //   groupId: config.get('streamer.kafka.group_id'),
+  //   brokers: config.get('streamer.kafka.brokers'),
+  //   blocksTopic: config.get('streamer.kafka.topics.blocks'),
+  //   pendingTxsTopic: config.get('streamer.kafka.topics.pending_txs')
+  // }
+  // const streamer = new KafkaStreamer(kafkaStreamerOpts, emitter)
+  // await streamer.initialize()
+  const streamer = new NullStreamer()
 
   // Create server
   logger.debug('bootstrapper -> Initializing server')
-  const server = new EthVMServer(blockService, addressService, txsService, chartsService, pendingTxService, exchangeService, vmService, streamer, ds)
+  const server = new EthVMServer(blockService, addressService, txsService, chartsService, pendingTxService, exchangeService, vmService, streamer)
   await server.start()
 }
 
