57d2c04d3f3f4b9ddb4c6401184cd04de2b68885
Aldo Borrero <aldo@aldoborrero.com>
2018-10-15 08:42:01 -0700

    Removoded unneded spec tests for events

--- apps/server/src/server/events/chart-account-growth.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai'
-
-xdescribe('chart-account-growth event', () => {
-  describe('start() method', () => {
-    it('should return true', () => {
-      expect(true).to.be.true
-    })
-  })
-})
--- apps/server/src/server/events/past-blocks.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai'
-
-xdescribe('past-blocks event', () => {
-  describe('start() method', () => {
-    it('should return true', () => {
-      expect(true).to.be.true
-    })
-  })
-})
