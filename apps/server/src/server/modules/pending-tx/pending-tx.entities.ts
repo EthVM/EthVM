f4b9076e21d819271591781fdb11af0bbc054f50
Brian McGee <brian@bluerosedev.com>
2018-10-17 12:14:32 +0200

    Re-work processing to handle duplicate events, republishing and chain re-orgs

---
+++ apps/server/src/server/modules/pending-tx/pending-tx.entities.ts
@@ -0,0 +1,13 @@
+export interface PendingTx {
+  nonce: Buffer
+  from: string
+  to: string
+  contractAddress: string
+  value: Buffer
+  data: Buffer
+  gasPrice: Buffer
+  gasLimit: Buffer
+  v: number
+  r: Buffer
+  s: Buffer
+}
