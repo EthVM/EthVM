75b98c25f9f1e948498ddbf2e9a79eaac0f67796
niktrix <nicky.gurbani@gmail.com>
2018-11-09 13:08:43 +0530

    mock i18n  for test

--- apps/ethvm/tests/unit/latestBlocks.spec.ts
+++ apps/ethvm/tests/unit/latestBlocks.spec.ts
@@ -2,11 +2,25 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import LatestBlocks from '@/components/blocks/largeBlocks/latestBlocks.vue'
 import store from '../../src/states'
 import Vuex from 'vuex'
+import en_US from '../../src/translations/en_US.json'
+ import VueI18n from 'vue-i18n'
 
+const defaultLocale: string = 'en_US'
+const messages: any = {
+  en_US
+}
 
 const localVue = createLocalVue()
 localVue.prototype.$eventHub = new localVue()
 localVue.use(Vuex)
+localVue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: defaultLocale,
+  messages,
+  fallbackLocale: 'en_US'
+})
+
 
 describe('latestBlocks.vue', () => {
   let getters
@@ -29,6 +43,7 @@ describe('latestBlocks.vue', () => {
         $t: () => 'footer'
       },
       localVue,
+      i18n,
       store
     })
     //expect(wrapper.text()).toMatch('msg')
