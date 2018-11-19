8285d5d233dfc95f193639357385d334a439982e
Aldo Borrero González <aldo@aldoborrero.com>
2018-11-19 12:04:15 +0100

    Updated dependencies for bolt

--- apps/bolt/build.gradle.kts
+++ apps/bolt/build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val kotlinVersion = "1.3.10"
+
 plugins {
   application
-  kotlin("jvm") version "1.2.70"
+  kotlin("jvm") version "1.3.10"
 }
 
 tasks.withType<KotlinCompile> {
@@ -32,10 +34,11 @@ project.java.sourceSets["main"].java {
 
 dependencies {
   // Kotlin
-  implementation(kotlin("stdlib"))
+  implementation(kotlin("stdlib", kotlinVersion))
+  compile(kotlin("reflect", kotlinVersion))
 
   // Ethereumj
-  implementation(group = "org.ethereum", name ="ethereumj-core", version = "1.10.0@ethvm.2-SNAPSHOT")
+  implementation(group = "org.ethereum", name = "ethereumj-core", version = "1.10.0@ethvm.2-SNAPSHOT")
 
   // Kafka
   implementation("org.apache.kafka:kafka-streams:2.0.0")
@@ -43,14 +46,14 @@ dependencies {
   implementation("io.enkrypt:avro:0.0.1-SNAPSHOT")
 
   // Utils
-  implementation("com.github.ajalt:clikt:1.4.0")
-  implementation("ch.qos.logback.logback:logback-classic:1.2.3")
-  implementation("io.github.microutils:kotlin-logging:1.5.9")
+  implementation("com.github.ajalt:clikt:1.5.0")
+  implementation("ch.qos.logback:logback-classic:1.2.3")
+  implementation("io.github.microutils:kotlin-logging:1.6.10")
   implementation("joda-time:joda-time:2.10")
-  implementation("org.koin:koin-core:1.0.0-RC-2")
-  implementation("io.arrow-kt:arrow-core:0.7.3")
+  implementation("org.koin:koin-core:1.0.0")
+  implementation("io.arrow-kt:arrow-core:0.8.1")
 
   // Testing
-  testImplementation("org.koin:koin-test:1.0.0-RC-2")
-  testImplementation("io.kotlintest:kotlintest-runner-junit5:3.1.9")
+  testImplementation("org.koin:koin-test:1.0.0")
+  testImplementation("io.kotlintest:kotlintest-runner-junit5:3.1.10")
 }
