d4d837e875ec3e2d30b84639c9ff3cd72c99d9fc
Aldo Borrero <aldo@aldoborrero.com>
2018-11-21 17:09:13 +0100

    Improved script for creating kafka topics with docker image

--- docker/images/kafka-ethvm-init/create-topics.sh
+++ docker/images/kafka-ethvm-init/create-topics.sh
@@ -1,23 +1,22 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -o nounset \
+    -o errexit \
+    -o verbose \
+    -o xtrace
 
 ensure_zookeeper() {
   echo "===> Check if Zookeeper is ready ..."
-  if ! [ "$(cub zk-ready $ZOOKEEPER ${KAFKA_CUB_TIMEOUT:-300})" ]; then
-    echo "===> Zookeeper seems to be down ..."
-    exit 1
-  fi
+  cub zk-ready $ZOOKEEPER ${KAFKA_CUB_TIMEOUT:-300}
 }
 
 ensure_kafka() {
-  echo "===> Check if Zookeeper is ready ..."
-  if ! [ "$(cub kafka-ready $KAFKA_BROKERS ${KAFKA_CUB_TIMEOUT:-300} --zookeeper_connect $ZOOKEEPER)" ]; then
-    echo "===> Kafka seems to be down ..."
-    exit 1
-  fi
+  echo "===> Check if Kafka is ready ..."
+  cub kafka-ready $KAFKA_BROKERS ${KAFKA_CUB_TIMEOUT:-300} --zookeeper_connect $ZOOKEEPER
 }
 
 create() {
-  echo "===> Create Kafka topics ..."
+  echo "===> Create Kafka topics (if necessary) ..."
   kafka-topics --create --if-not-exists --zookeeper $ZOOKEEPER --replication-factor 1 --partitions 12 --topic blocks --config retention.ms=-1 --config cleanup.policy=compact
   kafka-topics --create --if-not-exists --zookeeper $ZOOKEEPER --replication-factor 1 --partitions 12 --topic account-state --config retention.ms=-1 --config cleanup.policy=compact
   kafka-topics --create --if-not-exists --zookeeper $ZOOKEEPER --replication-factor 1 --partitions 12 --topic pending-transactions --config retention.ms=-1 --config cleanup.policy=compact
