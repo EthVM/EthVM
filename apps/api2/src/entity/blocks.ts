f1d9ee57c9ab2a19149f08687a3eb8c9e2d500fd
niktrix <nicky.gurbani@gmail.com>
2018-12-18 03:29:35 +0530

    update orm config, add mongodb dep

---
+++ apps/api2/src/entity/blocks.ts
@@ -0,0 +1,58 @@
+import {Entity,ObjectIdColumn,Column,ObjectID,PrimaryColumn,PrimaryGeneratedColumn} from "typeorm";
+
+
+
+@Entity()
+export class Blocks {
+  @PrimaryColumn()
+  number: number
+
+  @Column()
+  hash: string
+
+  @Column()
+  header: Header
+
+  @Column()
+  stats: BlockStats
+
+  @Column()
+  transactions?: string[]
+
+  @Column()
+  uncles?: string[]
+}
+export interface Header {
+  parentHash: string
+  sha3Uncles?: string
+  timestamp?: number
+  nonce?: string
+  miner: string
+  rewards: any
+  difficulty?: number
+  totalDifficulty?: number
+  stateRoot: Buffer
+  transactionsRoot?: Buffer
+  receiptsRoot: Buffer
+  logsBloom?: Buffer
+  gasLimit?: number
+  gasUsed?: number
+  mixHash?: Buffer
+  extraData?: Buffer
+}
+
+export interface BlockStats {
+  successfulTxs?: number
+  failedTxs?: number
+  pendingTxs?: number
+  processingTimeMs?: number
+  txs?: number
+  internalTxs?: number
+  avgGasPrice?: number
+  avgTxsFees?: number
+  totalGasPrice?: number
+  totalTxsFees?: number
+}
+
+
+
