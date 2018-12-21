2d18a2a2dfca96fde1fd2e1049e363ad89cd5ad6
niktrix <nicky.gurbani@gmail.com>
2018-12-21 19:03:35 +0530

    update Block type for graphql

--- apps/api2/src/modules/graphql/schema.ts
+++ apps/api2/src/modules/graphql/schema.ts
@@ -1,28 +1,53 @@
 export class Block {
-  number?: number
-  hash?: string
-  header?: Header
-  transactions?: string[]
-  uncles?: string[]
+    number?: number;
+    hash?: string;
+    header?: Header;
+    stats?: Stats;
+    transactions?: string[];
+    uncles?: string[];
 }
 
 export class Header {
-  parentHash?: string
-  sha3Uncles?: string
-  hash?: string
-  number?: number
-  timestamp?: number
-  nonce?: string
-  miner?: string
+    parentHash?: string;
+    sha3Uncles?: string;
+    hash?: string;
+    number?: number;
+    timestamp?: number;
+    nonce?: string;
+    miner?: string;
+    mixHash?: JSON;
+    rewards?: JSON;
+    difficulty?: JSON;
+    totalDifficulty?: JSON;
+    stateRoot?: JSON;
+    transactionsRoot?: JSON;
+    receiptsRoot?: JSON;
+    logsBloom?: JSON;
+    gasLimit?: JSON;
+    gasUsed?: JSON;
+    extraData?: JSON;
 }
 
 export abstract class IQuery {
-  abstract blocks(limit?: number, page?: number): Block[] | Promise<Block[]>
+    abstract blocks(limit?: number, page?: number): Block[] | Promise<Block[]>;
 
-  abstract block(hash?: string): Block | Promise<Block>
+    abstract block(hash?: string): Block | Promise<Block>;
 
-  abstract temp__(): boolean | Promise<boolean>
+    abstract temp__(): boolean | Promise<boolean>;
 }
 
-export type Date = any
-export type JSON = any
+export class Stats {
+    successfulTxs?: number;
+    failedTxs?: number;
+    pendingTxs?: number;
+    processingTimeMs?: number;
+    txs?: number;
+    internalTxs?: number;
+    totalGasPrice?: number;
+    avgGasPrice?: number;
+    totalTxsFees?: number;
+    avgTxsFees?: number;
+}
+
+export type Date = any;
+export type JSON = any;
