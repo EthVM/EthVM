3133cbd1c5e71ad777962e1a6c868039e5b517fb
niktrix <nicky.gurbani@gmail.com>
2019-01-09 16:27:36 +0530

    test Date scalar

--- apps/api2/src/modules/graphql/schema.ts
+++ apps/api2/src/modules/graphql/schema.ts
@@ -1,55 +1,55 @@
 export class Block {
-    number?: number;
-    hash?: string;
-    header?: Header;
-    stats?: Stats;
-    transactions?: string[];
-    uncles?: string[];
+  number?: number
+  hash?: string
+  header?: Header
+  stats?: Stats
+  transactions?: string[]
+  uncles?: string[]
 }
 
 export class Header {
-    parentHash?: string;
-    sha3Uncles?: string;
-    hash?: string;
-    number?: number;
-    timestamp?: number;
-    nonce?: string;
-    miner?: string;
-    mixHash?: JSON;
-    rewards?: JSON;
-    difficulty?: Decimal;
-    totalDifficulty?: Decimal;
-    stateRoot?: Buffer;
-    transactionsRoot?: JSON;
-    receiptsRoot?: JSON;
-    logsBloom?: JSON;
-    gasLimit?: JSON;
-    gasUsed?: JSON;
-    extraData?: JSON;
+  parentHash?: string
+  sha3Uncles?: string
+  hash?: string
+  number?: number
+  timestamp?: number
+  nonce?: string
+  miner?: string
+  mixHash?: JSON
+  rewards?: JSON
+  difficulty?: Decimal
+  totalDifficulty?: Decimal
+  stateRoot?: Buffer
+  transactionsRoot?: JSON
+  receiptsRoot?: JSON
+  logsBloom?: JSON
+  gasLimit?: JSON
+  gasUsed?: JSON
+  extraData?: JSON
 }
 
 export abstract class IQuery {
-    abstract blocks(limit?: number, page?: number): Block[] | Promise<Block[]>;
+  abstract blocks(limit?: number, page?: number): Block[] | Promise<Block[]>
 
-    abstract block(hash?: string): Block | Promise<Block>;
+  abstract block(hash?: string): Block | Promise<Block>
 
-    abstract temp__(): boolean | Promise<boolean>;
+  abstract temp__(): boolean | Promise<boolean>
 }
 
 export class Stats {
-    successfulTxs?: number;
-    failedTxs?: number;
-    pendingTxs?: number;
-    processingTimeMs?: number;
-    txs?: number;
-    internalTxs?: number;
-    totalGasPrice?: number;
-    avgGasPrice?: number;
-    totalTxsFees?: number;
-    avgTxsFees?: number;
+  successfulTxs?: number
+  failedTxs?: number
+  pendingTxs?: number
+  processingTimeMs?: number
+  txs?: number
+  internalTxs?: number
+  totalGasPrice?: number
+  avgGasPrice?: number
+  totalTxsFees?: number
+  avgTxsFees?: number
 }
 
-export type Buffer = any;
-export type Date = any;
-export type Decimal = any;
-export type JSON = any;
+export type Buffer = any
+export type Date = any
+export type Decimal = any
+export type JSON = any
