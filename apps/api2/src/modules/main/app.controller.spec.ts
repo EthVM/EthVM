cc924999996d9a42ecc0f6f2f5d1e61bf1c49153
niktrix <nicky.gurbani@gmail.com>
2019-01-09 14:05:09 +0530

    init e2e block test

--- apps/api2/src/modules/main/app.controller.spec.ts
+++ apps/api2/src/modules/main/app.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ConfigModule } from './../config'
+import { ConfigModule } from '../config/config.module'
 
 describe('AppController', () => {
   let app: TestingModule
---
+++ apps/api2/test/block.e2e-spec.ts
@@ -0,0 +1,57 @@
+import supertest from 'supertest'
+import { Test } from '@nestjs/testing'
+import { AppModule } from '../src/modules/main/app.module'
+import { INestApplication } from '@nestjs/common'
+
+describe('e2e', () => {
+  let app: INestApplication
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+    app = await moduleFixture.createNestApplication().init()
+  })
+
+  it('get block', done => {
+    return supertest(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `{
+        block(
+          hash: "88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6"
+        ) {
+          hash
+        }
+      }
+      `
+      })
+      .expect(200)
+      .then(response => {
+        expect(response.body.data.block.hash).toEqual('88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6')
+        done()
+      })
+  })
+
+  it('get blocks', done => {
+    return supertest(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `{
+        blocks(limit:10,page:9){
+          number
+        }
+      }
+      `
+      })
+      .expect(200)
+      .then(response => {
+        expect(response.body.data.blocks).toHaveLength(10)
+        done()
+      })
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+})
