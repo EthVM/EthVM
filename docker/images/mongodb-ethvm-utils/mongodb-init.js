d7f406a25878294b9d48267d658406a7164a50f7
Brian <brian@bluerosedev.com>
2019-01-10 16:01:37 +0100

    Refine docker compose resource limits and fix polling bug in Eth list source
    - Added cpu_shares to all processes to apply relative weights to their
      cpu usages
    
    - Fixed a bug in the Eth lists source for kafka-connect which resulted
      in the thread for the source spinning at 100%

--- docker/images/mongodb-ethvm-utils/mongodb-init.js
+++ docker/images/mongodb-ethvm-utils/mongodb-init.js
@@ -13,3 +13,51 @@ const collections = [
 ];
 
 collections.forEach(name => db.createCollection(name));
+
+const indexes = {
+
+  blocks: [
+    {key: {'header.hash': 1}, options: {unique: true}},
+    {key: {'header.author': 1}},
+    {key: {'header.timestamp': 1}},
+    {key: { totalDifficulty: 1 }}
+  ],
+
+  balances: [
+    {key: { address: 1 }},
+    {key: { balanceType: 1 }}
+  ],
+
+  block_statistics: [
+    {key: {name: 1}},
+    {key: {date: 1}},
+    {key: {date: 1, name: 1}, options: {unique: true}}
+  ],
+
+  contracts: [
+    {key: {type: 1}},
+    {key: {address: 1}},
+    {key: {type: 1, address: 1}, options: {unique: true}},
+  ],
+
+  token_transfers: [
+    {key: {transferType: 1}},
+    {key: {from: 1}},
+    {key: {to: 1}}
+  ],
+
+  transactions: [
+    {key: {blockHash: 1}},
+    {key: {blockNumber: 1}}
+  ]
+
+};
+
+function createIndexes(collectionId) { indexes[collectionId].forEach(params => db[collectionId].createIndex(params.key, params.options)) };
+
+createIndexes('blocks');
+createIndexes('balances');
+createIndexes('block_statistics');
+createIndexes('contracts');
+createIndexes('token_transfers');
+createIndexes('transactions');
