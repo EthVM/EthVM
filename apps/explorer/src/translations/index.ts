5f0f63f6ecd3511eecf48d9125c9b7ada955e562
Aldo Borrero <aldo@aldoborrero.com>
2019-01-14 07:02:34 -0800

    Solved imports in translations

--- apps/explorer/src/translations/index.ts
+++ apps/explorer/src/translations/index.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-import en_US from '@app/core/translations/en_US.json'
-import es_ES from '@app/core/translations/es_ES.json'
+import en_US from '@app/translations/en_US.json'
+import es_ES from '@app/translations/es_ES.json'
 
 const messages = {
   en_US,
