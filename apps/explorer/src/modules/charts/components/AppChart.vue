752f23b1cc0f76b165fa784b85673c1614d72c25
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-01-14 16:56:50 -0800

    WIP AppChart adding toggle buttons, to select datasets

--- apps/explorer/src/modules/charts/components/AppChart.vue
+++ apps/explorer/src/modules/charts/components/AppChart.vue
@@ -1,14 +1,33 @@
 <template>
   <v-card color="white" flat class="ma-2 mb-3">
-    <v-card-title class="title font-weight-bold pt-4 pl-4">
+    <v-layout align-center row>
+      <v-flex xs8>
+    <v-card-title class="title font-weight-bold pl-4">
       {{ chartTitle }}
       <v-tooltip bottom>
         <v-icon slot="activator" dark small class="pl-2">fas fa-exclamation-circle info--text</v-icon>
         <span>{{ chartDescription }}</span>
       </v-tooltip>
     </v-card-title>
+      </v-flex>
+      <v-flex xs4>
+        <!--
+        <v-layout align-center justify-end fill-height>
+          <v-btn flat color="secondary" class="text-capitalize" to="/blocks">{{ $t('bttn.more') }} <v-icon right>fas fa-angle-right</v-icon></v-btn>
+        </v-layout> -->
+      <v-layout row align-center justify-end fill-height >
+        <v-btn-toggle depressed v-model="toggleData" primary>
+          <v-btn flat :value="0" small>All</v-btn>
+          <v-btn flat :value="1" small>1D</v-btn>
+          <v-btn flat :value="2" small>1W</v-btn>
+          <v-btn flat :value="3" small>1M</v-btn>
+          </v-btn-toggle>
+      </v-layout>
+      </v-flex>
+
+    </v-layout>
     <v-divider></v-divider>
-    <v-layout align-center justify-end row fill-height>
+    <v-layout align-center justify-end row fill-height v-if="footnoteArr.length > 0">
      <app-footnotes :footnotes="footnoteArr"/>
     </v-layout>
     <canvas ref="chart" :width="width" :height="height"></canvas>
@@ -73,6 +92,8 @@ export default class AppChart extends Vue {
   @Prop({type: String}) chartDescription!: string
   @Prop({type:Array}) footnoteArr!: Footnote[]
 
+
+  toggleData = 1
   /*LifeCycle: */
   mounted() {
     this.createChart()
