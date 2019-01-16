d1c3c208e74c95a7908003307676a01685486066
Aldo Borrero <aldo@aldoborrero.com>
2019-01-16 01:56:53 -0800

    mend

--- apps/api/src/server/modules/uncles/uncle.mapper.ts
+++ apps/api/src/server/modules/uncles/uncle.mapper.ts
@@ -1,49 +1,11 @@
 import { Uncle } from 'ethvm-common'
 
 const toUncle = (uncle: any): Uncle => {
+  uncle.number = uncle.number.bytes
+  uncle.difficulty = uncle.difficulty.bytes
+  uncle.gasLimit = uncle.gasLimit.bytes
+  uncle.gasUsed = uncle.gasUsed.bytes
   return uncle
-
-  // const u: any = {}
-  // u.sha3Uncles = uncle.sha3Uncles
-  // u.uncleHeight = uncle.uncleHeight
-  // u.blockHeight = uncle.blockHeight
-  // u.position = uncle.position
-  // u.hash = uncle.hash
-  // u.parentHash = uncle.parentHash
-  // u.timestamp = uncle.timestamp
-  // u.nonce = uncle.nonce
-  // u.miner = uncle.miner
-  // if (uncle.rewards) {
-  //   for (const address in uncle.rewards) {
-  //     if (uncle.rewards[address]) {
-  //       uncle.rewards[address] = parseFloat(uncle.rewards[address])
-  //     }
-  //   }
-  //   u.rewards = uncle.rewards
-  // }
-  // u.difficulty = parseFloat(uncle.difficulty)
-  // u.totalDifficulty = parseFloat(uncle.totalDifficulty)
-  // if (uncle.stateRoot) {
-  //   u.stateRoot = uncle.stateRoot.buffer
-  // }
-  // if (uncle.transactionsRoot) {
-  //   u.transactionsRoot = uncle.transactionsRoot.buffer
-  // }
-  // if (uncle.receiptsRoot) {
-  //   u.receiptsRoot = uncle.receiptsRoot.buffer
-  // }
-  // if (uncle.logsBloom) {
-  //   u.logsBloom = uncle.logsBloom.buffer
-  // }
-  // u.gasLimit = parseFloat(uncle.gasLimit)
-  // u.gasUsed = parseFloat(uncle.gasUsed)
-  // if (uncle.mixHash) {
-  //   u.mixHash = uncle.mixHash.buffer
-  // }
-  // if (uncle.extraData) {
-  //   u.extraData = uncle.extraData.buffer
-  // }
-  // return u
 }
 
 export { toUncle }
