dad21e3881363748b8dc410c381db15cc83af8c4
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-01-15 16:18:14 -0800

    Charts: hash rate/ mining rewards

---
+++ apps/explorer/src/modules/charts/components/history/ChartHashRate.vue
@@ -0,0 +1,97 @@
+<template>
+  <app-chart type="line" :chart-id="id" :chart-title="title" :chart-description="description" :data="chartData" :options="chartOptions" :redraw="redraw" unfilled="true">
+  </app-chart>
+</template>
+
+<script lang="ts">
+  import AppChart from '@app/modules/charts/components/AppChart.vue'
+  import {
+    Vue,
+    Component
+  } from 'vue-property-decorator'
+  import ethUnits from 'ethereumjs-units'
+
+   /* Time Variables: */
+  const STATES = {
+    ALL: 'ALL',
+    YEAR: 'YEAR',
+    MONTH: 'MONTH',
+    DAY: 'DAY'
+  }
+
+  const DES = {
+    BEGIN: 'Average block size history in Ethereum blockchain since the begining.',
+    OTHER: 'Average block size history in Ethereum blockchain in last '
+  }
+
+  @Component({
+    components: {
+      AppChart
+    }
+  })
+
+  export default class ChartHashRate extends Vue {
+    id = "hash_rate_history"
+    title = "Average Hash Rate"
+    redraw = false
+    timeFrame = STATES.DAY
+    chartOptions = {
+      title: {
+        text: 'Average Hash Rate',
+        lineHeight: 1
+      },
+      responsive: true,
+      scales: {
+        yAxes: [{
+          position: 'left',
+          id: 'y-axis-1',
+          ticks: {
+            beginAtZero: true
+          },
+          gridLines: {
+            color: 'rgba(0, 0, 0, 0)'
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Average Hash Rate'
+          }
+        }],
+        xAxes: [{
+          type: 'time',
+          distribution: 'series',
+          ticks: {
+            source: 'labels'
+          }
+        }]
+      }
+    }
+    /*Computed: */
+    get chartData() {
+      const newLabels = []
+      const newPoints = []
+
+      this.$socket.emit('getAverageHashRate', this.timeFrame, (err, result) => {
+        if (!err && result) {
+          result.forEach(function(block) {
+            newPoints.push(block.reduction)
+            newLabels.push(block.group)
+          })
+        }
+      })
+      return {
+          labels: newLabels,
+          datasets: [{
+            label: 'Average Hash Rate',
+            borderColor: '#20c0c7',
+            backgroundColor: '#20c0c7',
+            data: newPoints,
+            yAxisID: 'y-axis-1',
+            fill: false
+          }]
+        }
+    }
+    get description(): string {
+      return (this.timeFrame === STATES.ALL) ?  DES.BEGIN : DES.OTHER + this.timeFrame
+    }
+}
+</script>
---
+++ apps/explorer/src/modules/charts/components/history/ChartMiningReward.vue
@@ -0,0 +1,97 @@
+<template>
+  <app-chart type="line" :chart-id="id" :chart-title="title" :chart-description="description" :data="chartData" :options="chartOptions" :redraw="redraw" unfilled="true">
+  </app-chart>
+</template>
+
+<script lang="ts">
+  import AppChart from '@app/modules/charts/components/AppChart.vue'
+  import {
+    Vue,
+    Component
+  } from 'vue-property-decorator'
+  import ethUnits from 'ethereumjs-units'
+
+   /* Time Variables: */
+  const STATES = {
+    ALL: 'ALL',
+    YEAR: 'YEAR',
+    MONTH: 'MONTH',
+    DAY: 'DAY'
+  }
+
+  const DES = {
+    BEGIN: 'Average miner rewards history in Ethereum blockchain since the begining.',
+    OTHER: 'Average miner rewards history  in Ethereum blockchain in last '
+  }
+
+  @Component({
+    components: {
+      AppChart
+    }
+  })
+
+  export default class ChartBlockSize extends Vue {
+    id = "miner_rewards_history"
+    title = "Average Miner Rewards"
+    redraw = false
+    timeFrame = STATES.DAY
+    chartOptions = {
+      title: {
+        text: 'Average Miner Rewards',
+        lineHeight: 1
+      },
+      responsive: true,
+      scales: {
+        yAxes: [{
+          position: 'left',
+          id: 'y-axis-1',
+          ticks: {
+            beginAtZero: true
+          },
+          gridLines: {
+            color: 'rgba(0, 0, 0, 0)'
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Average Miner Rewards (ETH)'
+          }
+        }],
+        xAxes: [{
+          type: 'time',
+          distribution: 'series',
+          ticks: {
+            source: 'labels'
+          }
+        }]
+      }
+    }
+    /*Computed: */
+    get chartData() {
+      const newLabels = []
+      const newPoints = []
+
+      this.$socket.emit('getAverageMinerRewards', this.timeFrame, (err, result) => {
+        if (!err && result) {
+          result.forEach(function(block) {
+            newPoints.push(block.reduction)
+            newLabels.push(block.group)
+          })
+        }
+      })
+      return {
+          labels: newLabels,
+          datasets: [{
+            label: 'Average Miner Rewards',
+            borderColor: '#20c0c7',
+            backgroundColor: '#20c0c7',
+            data: newPoints,
+            yAxisID: 'y-axis-1',
+            fill: false
+          }]
+        }
+    }
+    get description(): string {
+      return (this.timeFrame === STATES.ALL) ?  DES.BEGIN : DES.OTHER + this.timeFrame
+    }
+}
+</script>
