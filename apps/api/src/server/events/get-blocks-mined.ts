156e0961e274f0e4a1f108157807278b38858852
Aldo Borrero <aldo@aldoborrero.com>
2019-01-23 12:13:33 -0800

    Updated get-blocks-mined event

--- apps/api/src/server/events/get-blocks-mined.ts
+++ apps/api/src/server/events/get-blocks-mined.ts
@@ -1,6 +1,6 @@
 import { blockMinedPayloadValidator } from '@app/server/core/validation'
 import { EthVMServer, SocketEvent, SocketEventValidationResult } from '@app/server/ethvm-server'
-import { Events, SmallBlock } from 'ethvm-common'
+import { Events, Block } from 'ethvm-common'
 
 const getBlocksMinedEvent: SocketEvent = {
   id: Events.getBlocksMined,
@@ -11,7 +11,7 @@ const getBlocksMinedEvent: SocketEvent = {
       errors: [] // TODO: Map properly the error
     }
   },
-  onEvent: (server: EthVMServer, socket: SocketIO.Socket, payload: any): Promise<SmallBlock[]> =>
+  onEvent: (server: EthVMServer, socket: SocketIO.Socket, payload: any): Promise<Block[]> =>
     server.blockService.getBlocksMined(payload.address, payload.limit, payload.page)
 }
 export default getBlocksMinedEvent
