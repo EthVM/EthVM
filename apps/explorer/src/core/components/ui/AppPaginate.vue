74198eb1114e5a83b7000a74d6d9799cfb810d14
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-02-13 14:02:15 -0800

    added hasInput, hasLast props to the pagination component

--- apps/explorer/src/core/components/ui/AppPaginate.vue
+++ apps/explorer/src/core/components/ui/AppPaginate.vue
@@ -6,14 +6,15 @@
         <v-btn flat class="bttnGrey info--text text-capitalize bttn" @click="setPageOnClick('prev')" small
           ><v-icon class="secondary--text" small>fas fa-angle-left</v-icon>
         </v-btn>
-        <div class="page-input">
+        <div v-if="hasInput" class="page-input">
           <v-text-field v-model="pageInput" :mask="inputMask" :placeholder="newPH" :error="!valid(pageInput)" :class="validClass"></v-text-field>
         </div>
+        <p v-else class="info--text pr-1">{{pageInput}}</p>
         <p class="info--text">out of {{ total }}</p>
         <v-btn flat class="bttnGrey info--text text-capitalize bttn" @click="setPageOnClick('next')" small
           ><v-icon class="secondary--text" small>fas fa-angle-right</v-icon>
         </v-btn>
-        <v-btn flat class="bttnGrey info--text text-capitalize bttn" @click="setPageOnClick('last')" small>{{ $t('bttn.last') }}</v-btn>
+        <v-btn v-if="hasLast" flat class="bttnGrey info--text text-capitalize bttn" @click="setPageOnClick('last')" small>{{ $t('bttn.last') }}</v-btn>
       </v-layout>
     </v-container>
   </v-card>
@@ -27,6 +28,9 @@ import _debounce from 'lodash.debounce'
 export default class AppPaginate extends Vue {
   @Prop(Number) total: number
   @Prop(Number) newPage: number
+  @Prop({type: Boolean, default:true}) hasLast: boolean
+  @Prop({type: Boolean, default:true}) hasInput: boolean
+
 
   page = 1
   pageInput = this.page
--- apps/explorer/src/modules/txs/components/TableTxs.vue
+++ apps/explorer/src/modules/txs/components/TableTxs.vue
@@ -17,7 +17,7 @@
       </v-flex>
       <v-flex v-else xs12 sm7 md6>
         <v-layout v-if="pages > 1 && !hasError" justify-end row class="pb-1 pr-2 pl-2">
-          <app-paginate :total="pages" @newPage="setPage" :new-page="page" />
+          <app-paginate :total="pages" @newPage="setPage" :new-page="page" :hasInput="false" :hasLast="false"/>
         </v-layout>
       </v-flex>
     </v-layout>
@@ -66,7 +66,7 @@
             <v-divider class="mb-2 mt-2" />
           </v-card>
           <v-layout v-if="pages > 1" justify-end row class="pb-1 pr-2 pl-2">
-            <app-paginate :total="pages" @newPage="setPage" :new-page="page" />
+            <app-paginate :total="pages" @newPage="setPage" :new-page="page" :hasInput="false" :hasLast="false"/>
           </v-layout>
         </v-flex>
         <v-flex xs12 v-if="loading">
