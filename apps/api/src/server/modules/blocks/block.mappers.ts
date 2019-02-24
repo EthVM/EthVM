da7ac1c71ca9cbefd9e379a592374c796f76e3aa
Aldo Borrero <aldo@aldoborrero.com>
2019-02-24 05:11:13 -0800

    Updated entities to match required changes on API

--- apps/api/src/server/modules/blocks/block.mappers.ts
+++ apps/api/src/server/modules/blocks/block.mappers.ts
@@ -1,57 +1,24 @@
-import { toSimpleTx, toTx } from '@app/server/modules/txs'
-import { toUncle } from '@app/server/modules/uncles'
+import { toSimpleTx } from '@app/server/modules/txs'
 import { Block } from 'ethvm-common'
 
 const toBlock = (block: any, format: string = 'full'): Block => {
   switch (format) {
-    case 'full':
-      return toFullBlock(block)
     case 'simple':
       return toSimpleBlock(block)
     default:
-      throw new Error('Illegal format passed to mapper!')
+      return block
   }
-}
-
-const toReward = (r: any): any => {
-  r.reward = r.reward.toString()
-  return r
-}
-
-const toFullBlock = (block: any): Block => {
-  block.header.number = block.header.number.toString()
-  block.header.difficulty = block.header.difficulty.toString()
-  block.header.gasLimit = block.header.gasLimit.toString()
-  block.header.gasUsed = block.header.gasUsed.toString()
-  block.totalDifficulty = block.totalDifficulty.toString()
-
-  if (block.rewards) {
-    block.rewards.map(r => toReward(r))
-  }
-
-  if (block.transactions) {
-    block.transactions = block.transactions.map(tx => toTx(tx))
-  }
-
-  if (block.uncles) {
-    block.uncles = block.uncles.map(u => toUncle(u))
-  }
-
-  return block
 }
 
 const toSimpleBlock = (block: any): Block => {
-  block.header.number = block.header.number.toString()
-
-  if (block.rewards) {
-    block.rewards.map(r => toReward(r))
-  }
-
-  if (block.transactions) {
-    block.transactions = block.transactions.map(tx => toSimpleTx(tx))
-  }
-
-  return block
+  const b: any = {}
+  b.header = {}
+  b.header.number = block.header.number
+  b.header.hash = block.header.hash
+  b.header.author = block.header.author
+  b.rewards = block.rewards || []
+  b.transactions = block.transactions ? block.transactions.map(tx => toSimpleTx(tx)) : []
+  return b
 }
 
-export { toBlock }
+export { toBlock, toSimpleBlock }
--- apps/api/src/server/modules/uncles/uncle.mapper.ts
+++ apps/api/src/server/modules/uncles/uncle.mapper.ts
@@ -1,20 +1,5 @@
 import { Uncle } from 'ethvm-common'
 
-const toUncle = (uncle: any): Uncle => {
-  uncle.number = uncle.number.toString()
-  uncle.difficulty = uncle.difficulty.toString()
-  uncle.gasLimit = uncle.gasLimit.toString()
-  uncle.gasUsed = uncle.gasUsed.toString()
-
-  if (uncle.blockNumber) {
-    uncle.blockNumber = uncle.blockNumber.toString()
-  }
-
-  if (uncle.uncleReward) {
-    uncle.uncleReward = uncle.uncleReward.toString()
-  }
-
-  return uncle
-}
+const toUncle = (uncle: any): Uncle => uncle
 
 export { toUncle }
