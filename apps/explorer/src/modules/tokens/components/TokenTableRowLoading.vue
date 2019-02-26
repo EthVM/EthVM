120b471b432d5e60582515239f80711ff9940fe4
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-02-26 14:53:38 -0800

    Mobile token row ui loading edited

--- apps/explorer/src/modules/tokens/components/TokenTableRowLoading.vue
+++ apps/explorer/src/modules/tokens/components/TokenTableRowLoading.vue
@@ -1,5 +1,25 @@
 <template>
   <v-card flat white>
+    <v-layout>
+      <v-flex xs12 hidden-sm-and-up>
+        <div class="token-mobile">
+          <v-layout grid-list-xs row wrap align-center justify-start fill-height>
+            <v-flex xs2 >
+              <div class="loading"></div>
+            </v-flex>
+            <v-flex xs8 >
+                <div class="loading"></div>
+                <div class="loading"></div>
+                <div class="loading"></div>
+                <div class="loading"></div>
+            </v-flex>
+             <v-flex xs2 >
+               <div class="loading"></div>
+             </v-flex>
+          </v-layout>
+        </div>
+      </v-flex>
+      <v-flex hidden-xs-only sm12>
     <v-layout grid-list-xs row wrap align-center justify-start fill-height class="pl-2 pr-2 pt-2">
       <v-flex xs1>
         <div class="loading"></div>
@@ -21,6 +41,8 @@
       </v-flex>
     </v-layout>
     <v-divider class="mb-2 mt-2" />
+    </v-flex>
+    </v-layout>
   </v-card>
 </template>
 
@@ -36,5 +58,11 @@ export default class TokenTableRowLoading extends Vue {}
   background: #e6e6e6;
   height: 12px;
   border-radius: 2px;
+  margin:1px;
+}
+.token-mobile {
+  border: 1px solid #b4bfd2;
+  padding: 10px 0px 10px 0px;
+  height: 68px;
 }
 </style>
