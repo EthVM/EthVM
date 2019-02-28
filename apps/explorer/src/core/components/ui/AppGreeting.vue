54827693b4c5d1ad7230b6ce489f9279dddd876a
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-02-27 17:54:17 -0800

    Lint Code

--- apps/explorer/src/core/components/ui/AppGreeting.vue
+++ apps/explorer/src/core/components/ui/AppGreeting.vue
@@ -1,8 +1,7 @@
 <template>
   <v-dialog v-if="greet" v-model="dialog" width="500">
     <v-card>
-      <canvas id="animate">
-        </canvas>
+      <canvas id="animate"> </canvas>
       <v-card-title class="headline" primary-title>
         {{ $t('message.welcome.title') }}
       </v-card-title>
@@ -17,70 +16,58 @@
           </v-btn>
         </v-layout>
       </v-card-actions>
-
     </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts">
-  import {
-    Vue,
-    Component,
-    Prop
-  } from 'vue-property-decorator'
-  import storePack from 'store'
-  import confetti from 'canvas-confetti'
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import storePack from 'store'
+import confetti from 'canvas-confetti'
 
+@Component
+export default class AppGreeting extends Vue {
+  @Prop(Boolean) greet!: boolean
+  dialog = true
+  supportEmail = 'support@ethvm.com'
 
-  @Component
-  export default class AppGreeting extends Vue {
-    @Prop(Boolean) greet!: boolean
-    dialog = true
-    supportEmail = 'support@ethvm.com'
+  mounted() {
+    setTimeout(() => {
+      this.createAnimation()
+    }, 200)
+  }
 
-    mounted() {
-      setTimeout(() =>{
-           this.createAnimation()
-      }, 200)
+  //Methods:
+  removeDialog(): void {
+    this.dialog = false
+    //storePack.set('notFirstTimeVisit', true)
+  }
 
-
-
-    }
-
-    //Methods:
-    removeDialog(): void {
-      this.dialog = false
-      //storePack.set('notFirstTimeVisit', true)
-    }
-
-    createAnimation(): void {
-
-
-
-         const myCanvas : any = document.getElementById('animate');
-      var colors = ['#6270fc', '#92cce1', '#fed9a1', '#fe8778', '#00b173', '#fe1377', '#eea66b']
-      myCanvas.confetti = myCanvas.confetti|| confetti.create(myCanvas, {
+  createAnimation(): void {
+    const myCanvas: any = document.getElementById('animate')
+    const colors = ['#6270fc', '#92cce1', '#fed9a1', '#fe8778', '#00b173', '#fe1377', '#eea66b']
+    myCanvas.confetti =
+      myCanvas.confetti ||
+      confetti.create(myCanvas, {
         resize: true
       })
 
-         myCanvas.confetti({
-           particleCount: 100,
-    spread: 70,
-    origin: {
+    myCanvas.confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: {
         y: 1
-    },
-          colors: colors
-        });
-
-
-    }
+      },
+      colors: colors
+    })
   }
+}
 </script>
 
 <style scoped lang="css">
-  canvas {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-  }
+canvas {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
 </style>
