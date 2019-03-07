f7bea5cd39c79b9ce2ec542c0071caf02cc0729a
Caroline <caroline@bluerosedev.com>
2019-03-07 18:41:06 +0100

    Create entities directory within orm folder and move blocks entity file

--- apps/api-nestjs/src/modules/blocks/block.service.ts
+++ apps/api-nestjs/src/modules/blocks/block.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import { BlockEntity } from '@app/modules/blocks/block.entity'
+import { BlockEntity } from '@app/orm/entities/block.entity'
 
 @Injectable()
 export class BlockService {
