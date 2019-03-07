23b68149677cefb51e051dea9c7c1e6bec6ae254
Caroline <caroline@bluerosedev.com>
2019-03-07 16:15:56 +0100

    Remove swagger set up from main.hmr.ts

--- apps/api-nestjs/src/main.hmr.ts
+++ apps/api-nestjs/src/main.hmr.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
-import { setupSwagger } from './swagger'
 
 declare const module: any
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  setupSwagger(app)
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
