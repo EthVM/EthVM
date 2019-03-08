fe20585856c39d97839f7516eb6fd4780d8693f4
Caroline <caroline@bluerosedev.com>
2019-03-08 15:24:20 +0100

    Add entity file for block_metric

---
+++ apps/api-nestjs/src/orm/entities/block-metric.entity.ts
@@ -0,0 +1,47 @@
+import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm'
+import { assignClean } from '@app/shared/utils'
+
+@Entity('block_metrics')
+export class BlockMetricEntity {
+
+  constructor(data: any) {
+    assignClean(this, data)
+  }
+
+  @ObjectIdColumn({name: '_id', type: 'decimal', readonly: true})
+  id: ObjectID
+
+  @Column({type: 'string'})
+  avgGasLimit: string
+
+  @Column({type: 'string'})
+  avgGasPrice: string
+
+  @Column({type: 'string'})
+  avgTxFees: string
+
+  @Column({type: 'string'})
+  blockTime: string
+
+  @Column({type: 'string'})
+  difficulty: string
+
+  @Column({type: 'string'})
+  hash: string
+
+  @Column({type: 'long'})
+  number: number
+
+  @Column({type: 'int'})
+  numFailedTxs: number
+
+  @Column({type: 'int'})
+  numPendingTxs: number
+
+  @Column({type: 'int'})
+  numSuccessfulTxs: number
+
+  @Column({type: 'long'})
+  timestamp: number
+
+}
