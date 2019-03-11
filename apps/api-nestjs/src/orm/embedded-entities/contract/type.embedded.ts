d89aa36e03cbf62a6c75d487af63e3f48cd61407
Caroline <caroline@bluerosedev.com>
2019-03-11 16:14:47 +0100

    Add contract-type enum

--- apps/api-nestjs/src/orm/embedded-entities/contract/type.embedded.ts
+++ apps/api-nestjs/src/orm/embedded-entities/contract/type.embedded.ts
@@ -1,13 +1,17 @@
 import { Column } from 'typeorm'
 import { assignClean } from '@app/shared/utils'
 
+export enum ContractType {
+  GENERIC, ERC20, ERC721
+}
+
 export class TypeEmbedded {
 
   constructor(data: any) {
     assignClean(this, data)
   }
 
-  @Column({type: 'string'})
-  string: string
+  @Column({type: 'enum', enum: ContractType})
+  string: ContractType
 
 }
