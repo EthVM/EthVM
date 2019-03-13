2b2ff04a4f6e0c9e0e6501483ee53cdaceae8f74
Aldo Borrero González <aldo@aldoborrero.com>
2019-03-13 19:16:20 +0100

    Removed es_ES translation (for now). Updated en_US.json translation to start working on porting strings to the new format.

--- apps/explorer/src/translations/index.ts
+++ apps/explorer/src/translations/index.ts
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 import en_US from '@app/translations/en_US.json'
-import es_ES from '@app/translations/es_ES.json'
 
 const messages = {
   en_US,
-  es_ES
 }
 
 Vue.use(VueI18n)
