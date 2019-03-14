f126cc21b8b4f7a8bd318307f265dda43a0e5ab0
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-03-14 15:26:41 -0700

    Reorganized strings

--- apps/explorer/src/translations/index.ts
+++ apps/explorer/src/translations/index.ts
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 import en_US from '@app/translations/en_US.json'
+import ru_RU from '@app/translations/ru_RU.json'
 
 const messages = {
   en_US,
+  ru_RU
 }
 
 Vue.use(VueI18n)
