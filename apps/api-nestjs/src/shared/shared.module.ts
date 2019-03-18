843fb1593238c66debebc6ef34cecba85857b3ac
Aldo Borrero González <aldo@aldoborrero.com>
2019-03-18 17:11:55 +0100

    Reordered imports in shared.module.ts

--- apps/api-nestjs/src/shared/shared.module.ts
+++ apps/api-nestjs/src/shared/shared.module.ts
@@ -1,11 +1,11 @@
-import { Global, Module } from '@nestjs/common'
 import { ConfigService } from '@app/shared/config.service'
 import { DurationService } from '@app/shared/duration.service'
 import { EthService } from '@app/shared/eth.service'
-import { ParseHashPipe } from '@app/shared/validation/parse-hash.pipe'
 import { ParseAddressPipe } from '@app/shared/validation/parse-address.pipe'
+import { ParseHashPipe } from '@app/shared/validation/parse-hash.pipe'
 import { ParseLimitPipe } from '@app/shared/validation/parse-limit.pipe'
 import { ParsePagePipe } from '@app/shared/validation/parse-page.pipe'
+import { Global, Module } from '@nestjs/common'
 
 @Global()
 @Module({
