e19d5e6ff7e953088b9c06205b2badae4c4a7d46
Caroline <caroline@bluerosedev.com>
2019-03-19 17:36:15 +0100

    Bugfix block.header.number default to 0 if undefined

--- apps/api-nestjs/src/modules/blocks/block.dto.ts
+++ apps/api-nestjs/src/modules/blocks/block.dto.ts
@@ -4,6 +4,9 @@ import { assignClean } from '@app/shared/utils'
 export class BlockDto extends Block {
   constructor(data: any) {
     super()
+    if (data.header && !data.header.number) {
+      data.header.number = 0 // Ensure "undefined" returned from mongo is treated as 0
+    }
     assignClean(this, data)
   }
 }
