17fbde315b4f9c2faf50a94d1c2c4836811367b8
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-03-29 15:36:13 -0700

    Address Tx Table UI - mobile added

--- apps/explorer/src/modules/addresses/components/TableAddressTxRow.vue
+++ apps/explorer/src/modules/addresses/components/TableAddressTxRow.vue
@@ -12,6 +12,63 @@
             <v-card-text class="text-xs-center secondary--text">{{ text }}</v-card-text>
           </v-card>
           <v-card v-else v-for="tx in transactions" class="transparent pb-1" flat :key="tx.getHash()">
+
+
+              <v-container pa-0 ma-0>
+    <v-layout d-block>
+    <!--
+      =====================================================================================
+        Mobile (XS)
+      =====================================================================================
+      -->
+      <v-flex xs12 hidden-sm-and-up>
+        <div :class="getStatusClass(tx)">
+          <v-layout grid-list-xs row wrap align-center justify-start fill-height class="pt-3 pb-3 pr-4 pl-4">
+            <v-flex xs6 pa-1>
+              <p :class="getTxTypeClass(tx)">{{getTypeString(tx)}} </p>
+            </v-flex>
+            <v-flex xs6 pr-44>
+              <v-layout row justify-end>
+                <p :class="getTxTypeClass(tx)">{{ getSign(tx) }} {{ getRoundNumber(tx.getValue().toEth()) }} {{$t('common.eth')}}</p>
+              </v-layout>
+            </v-flex>
+            <v-flex xs2 pa-1>
+              <p class="info--text psmall">{{ $tc('tx.hash', 1) }}:</p>
+            </v-flex>
+            <v-flex xs10 pa-1>
+              <app-hash-concat :hash="tx.getHash()" :link="'/tx/' + tx.getHash()" />
+            </v-flex>
+            <v-flex xs3 pa-1>
+              <p class="info--text psmall ">{{ $tc('address.name', 1) }}:</p>
+            </v-flex>
+            <v-flex shrink pa-1>
+              <div class="tx-icon-type"><p>{{ getTxTypeIcon(tx)}}</p></div>
+            </v-flex>
+            <v-flex xs6 pa-1>
+                <app-hash-concat v-if="getType(tx) === 'self'" :hash="tx.getFrom().toString()" :italic="true" />
+                <div v-else>
+                  <div v-if="tx.getContractAddress().isEmpty()" >
+                    <app-hash-concat v-if="getType(tx) === 'in'" :hash="tx.getTo().toString()" :italic="true" :link="'/address/' + tx.getTo().toString()"/>
+                    <app-hash-concat v-else :hash="tx.getFrom().toString()" :italic="true" :link="'/address/' + tx.getFrom().toString()"/>
+                  </div>
+                  <div v-else>
+                    <app-hash-concat  :hash="tx.getContractAddress().toString()" :italic="true" :link="'/address/' + tx.getContractAddress().toString()"/>
+                  </div>
+                </div>
+            </v-flex>
+          </v-layout>
+        </div>
+      </v-flex>
+
+      <v-flex xs12 hidden-xs-only >
+      <!--
+      =====================================================================================
+        Tablet/ Desktop (SM - XL)
+      =====================================================================================
+      -->
+
+
+
             <v-layout grid-list-xs row wrap align-center justify-start fill-height pl-3 pr-2 pt-2 pb-1>
               <!--
               =====================================================================================
@@ -158,6 +215,9 @@
               </v-flex>
             </v-layout>
             <v-divider />
+      </v-flex>
+    </v-layout>
+              </v-container>
           </v-card>
         </v-flex>
       </v-layout>
@@ -169,10 +229,13 @@
 import { Vue, Component, Prop, Mixins } from 'vue-property-decorator'
 import { StringConcatMixin } from '@app/core/components/mixins'
 import AppTimeAgo from '@app/core/components/ui/AppTimeAgo.vue'
+import AppHashConcat from '@app/core/components/ui/AppHashConcat.vue'
+import { TranslateResult } from 'vue-i18n';
 
 @Component({
   components: {
-    AppTimeAgo
+    AppTimeAgo,
+    AppHashConcat
   }
 })
 export default class TableAddressTxRow extends Mixins(StringConcatMixin) {
@@ -193,17 +256,70 @@ export default class TableAddressTxRow extends Mixins(StringConcatMixin) {
   ===================================================================================
   */
 
-  getType(tx): boolean {
+ getTypeString(tx): TranslateResult{
+   const type =  this.getType(tx)
+   switch (type) {
+     case 'in':
+       return this.$i18n.t('filter.in')
+    case 'out':
+      return this.$i18n.t('filter.out')
+     default:
+       return this.$i18n.t('tx.self')
+   }
+ }
+
+ getTxTypeClass(tx): string {
+   const type =  this.getType(tx)
+   switch (type) {
+     case 'in':
+       return 'tx-in'
+    case 'out':
+      return 'tx-out'
+     default:
+       return 'tx-self'
+   }
+ }
+
+ getTxTypeIcon(tx): TranslateResult{
+   const type =  this.getType(tx)
+   switch (type) {
+     case 'in':
+       return this.$i18n.t('tx.from')
+    case 'out':
+      return this.$i18n.t('tx.to')
+     default:
+       return  `${this.$i18n.t('tx.from')} | ${this.$i18n.t('tx.to')}`
+   }
+ }
+
+  getType(tx): string {
+    if (tx.getTo().toString().toUpperCase() === tx.getFrom().toString().toUpperCase()) {
+     return 'self'
+   }
+   else {
     return (
       tx
         .getFrom()
         .toString()
         .toUpperCase() === this.account.toUpperCase()
-    )
+    ) ? 'out' : 'in'
+  }
   }
 
   getSign(tx): string {
-    return this.getType(tx) ? '-' : '+'
+      const type =  this.getTypeString(tx)
+   switch (type) {
+     case this.$i18n.t('tx.self'):
+       return ''
+    case this.$i18n.t('filter.in'):
+      return '+'
+     default:
+       return '-'
+   }
+  }
+
+  getStatusClass(tx): string {
+    return tx.getStatus() ? 'tx-status-sucess table-row-mobile' :'tx-status-fail table-row-mobile'
   }
 
   log(tx) {}
@@ -233,3 +349,52 @@ export default class TableAddressTxRow extends Mixins(StringConcatMixin) {
   }
 }
 </script>
+
+
+<style scoped lang="css">
+.table-row-mobile {
+  border: 1px solid #b4bfd2;
+}
+
+p {
+  margin-bottom: 0px;
+  padding-bottom: 0px;
+}
+
+.tx-status-fail {
+  border-left: 2px solid #fe1377;
+}
+
+.tx-status-sucess {
+  border-left: 2px solid #40ce9c;
+}
+
+.tx-self {
+  color: #8391a8;
+}
+
+.tx-in {
+  color: black;
+}
+
+.tx-out {
+  color: #fe8778;
+}
+
+.tx-icon-type {
+  padding: 0px 4px;
+  background-color: #98a8c2;
+  border-radius: 2px;
+  color: white;
+  text-transform: uppercase;
+  font-size: 10px;
+  height: 16px;
+  margin: 0px;
+}
+
+.address-row{
+  display:inline-block;
+}
+
+</style>
+
