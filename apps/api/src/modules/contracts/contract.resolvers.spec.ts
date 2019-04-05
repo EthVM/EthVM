3d823169720e062af97fd2a33f79677f28218f4b
Caroline <caroline@bluerosedev.com>
2019-04-05 17:13:48 +0200

    Add unit tests for ContractResolvers and TokenExchangeRateResolvers

---
+++ apps/api/src/modules/contracts/contract.resolvers.spec.ts
@@ -0,0 +1,64 @@
+import { Test } from '@nestjs/testing'
+import { ContractService } from './contract.service'
+import { ContractResolvers } from './contract.resolvers'
+import { ContractEntity } from '../../orm/entities/contract.entity'
+import { ContractDto } from './contract.dto'
+import { ParseAddressPipe } from '../../shared/validation/parse-address.pipe'
+import { EthService } from '../../shared/eth.service'
+
+const mockService = {
+  async findContractByHash(hash) {},
+  async findContractsCreatedBy(creator, limit, page) {}
+}
+
+describe('ContractResolvers', () => {
+
+  let contractService: ContractService
+  let contractResolvers: ContractResolvers
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        ContractResolvers,
+        ParseAddressPipe,
+        EthService,
+        {
+          provide: ContractService,
+          useValue: mockService
+        }
+      ],
+    }).compile()
+    contractService = module.get<ContractService>(ContractService)
+    contractResolvers = module.get<ContractResolvers>(ContractResolvers)
+  })
+
+  const hash = '0000000000000000000000000000000000000000'
+  const creator = '0000000000000000000000000000000000000001'
+  const result = {
+    id: hash,
+    address: hash,
+    creator
+  }
+
+  describe('contractByHash', () => {
+    it('should return an instance of ContractDto matching the address hash provided', async () => {
+
+      jest.spyOn(contractService, 'findContractByHash')
+        .mockImplementation(() => new Promise(resolve => {
+          resolve(new ContractEntity(result))
+        }))
+
+      expect(await contractResolvers.contractByHash(hash)).toEqual(new ContractDto(result))
+    })
+  })
+
+  describe('contractsCreatedBy', () => {
+    it('should return an array of ContractDto instances matching the creator hash provided', async () => {
+      jest.spyOn(contractService, 'findContractsCreatedBy')
+        .mockImplementation(() => new Promise(resolve => {
+          resolve([new ContractEntity(result)])
+        }))
+      expect(await contractResolvers.contractsCreatedBy(creator, 1, 0)).toEqual([new ContractDto(result)])
+    })
+  })
+})
---
+++ apps/api/src/modules/exchanges/token-exchange-rate.resolvers.spec.ts
@@ -0,0 +1,117 @@
+import { Test } from '@nestjs/testing'
+import { ParseAddressPipe } from '../../shared/validation/parse-address.pipe'
+import { EthService } from '../../shared/eth.service'
+import { ExchangeService } from './exchange.service'
+import { TokenExchangeRateResolvers } from './token-exchange-rate.resolvers'
+import { TokenTransferService } from '../token-transfers/token-transfer.service'
+import { QuoteDto } from './quote.dto'
+import { TokenExchangeRateEntity } from '../../orm/entities/token-exchange-rate.entity'
+import { TokenExchangeRateDto } from './token-exchange-rate.dto'
+import { EthplorerTokenInfoDto } from '../token-transfers/dto/ethplorer-token-info.dto'
+
+const mockExchangeService = {
+  async findQuote(symbol, to) {},
+  async findTokenExchangeRates(filter, limit, page) {},
+  async countTokenExchangeRates() {},
+  async findTokenExchangeRateBySymbol(symbol) {},
+  async findTokenExchangeRateByAddress(address) {}
+}
+const mockTokenTransferService = {
+  async fetchTokenInfo(address) {}
+}
+
+describe('TokenExchangeRateResolvers', () => {
+
+  let exchangeService: ExchangeService
+  let tokenTransferService: TokenTransferService
+  let tokenExchangeRateResolvers: TokenExchangeRateResolvers
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TokenExchangeRateResolvers,
+        ParseAddressPipe,
+        EthService,
+        {
+          provide: ExchangeService,
+          useValue: mockExchangeService
+        },
+        {
+          provide: TokenTransferService,
+          useValue: mockTokenTransferService
+        }
+      ],
+    }).compile()
+    exchangeService = module.get<ExchangeService>(ExchangeService)
+    tokenTransferService = module.get<TokenTransferService>(TokenTransferService)
+    tokenExchangeRateResolvers = module.get<TokenExchangeRateResolvers>(TokenExchangeRateResolvers)
+  })
+
+  const to = 'USD'
+  const symbol = 'ETH'
+  const address = '0000000000000000000000000000000000000000'
+  const quoteResult = {
+    to
+  }
+  const tokenExchangeRateResult = {
+    id: 'TEST'
+  }
+  const ethplorerTokenInfoResult = {
+    balance: 0,
+    totalIn: 0,
+    totalOut: 0
+  }
+
+  describe('quote', () => {
+    it('should return an instance of QuoteDto matching the symbol and to provided', async () => {
+
+      jest.spyOn(exchangeService, 'findQuote')
+        .mockImplementation(() => new Promise(resolve => {
+          resolve(new QuoteDto(quoteResult))
+        }))
+
+      expect(await tokenExchangeRateResolvers.quote(symbol, to)).toEqual(new QuoteDto(quoteResult))
+    })
+  })
+
+  describe('tokenExchangeRates', () => {
+    it('should return an array of TokenExchangeRateDto entities', async () => {
+      jest.spyOn(exchangeService, 'findTokenExchangeRates')
+        .mockImplementation(() => new Promise(resolve => {
+          resolve([new TokenExchangeRateEntity(tokenExchangeRateResult)])
+        }))
+      expect(await tokenExchangeRateResolvers.tokenExchangeRates('price_high', 1, 0))
+        .toEqual([new TokenExchangeRateDto(tokenExchangeRateResult)])
+    })
+  })
+
+  describe('totalNumTokenExchangeRates', () => {
+    it('should return the total number of TokenExchangeRate entities in the database', async () => {
+      const total = 100
+      jest.spyOn(exchangeService, 'countTokenExchangeRates')
+        .mockImplementation(() => new Promise(resolve => resolve(total)))
+      expect(await tokenExchangeRateResolvers.totalNumTokenExchangeRates()).toBe(total)
+    })
+  })
+
+  describe('tokenExchangeRateBySymbol', () => {
+    it('should return an instance of TokenExchangeRateDto with id matching the symbol provided', async () => {
+      jest.spyOn(exchangeService, 'findTokenExchangeRateBySymbol')
+        .mockImplementation(() => new Promise(resolve => resolve(new TokenExchangeRateEntity(tokenExchangeRateResult))))
+      expect(await tokenExchangeRateResolvers.tokenExchangeRateBySymbol(symbol)).toEqual(new TokenExchangeRateDto(tokenExchangeRateResult))
+    })
+  })
+
+  describe('tokenExchangeRateByAddress', () => {
+    it('should return an instance of TokenExchangeRateDto matching the address hash provided', async () => {
+      jest.spyOn(exchangeService, 'findTokenExchangeRateByAddress')
+        .mockImplementation(() => new Promise(resolve => resolve(new TokenExchangeRateEntity(tokenExchangeRateResult))))
+      jest.spyOn(tokenTransferService, 'fetchTokenInfo')
+        .mockImplementation(() => new Promise(resolve => resolve(new EthplorerTokenInfoDto(ethplorerTokenInfoResult))))
+
+      expect(await tokenExchangeRateResolvers.tokenExchangeRateByAddress(address))
+        .toEqual(new TokenExchangeRateDto({...tokenExchangeRateResult, ...ethplorerTokenInfoResult}))
+    })
+  })
+
+})
