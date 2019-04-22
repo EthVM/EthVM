53a8666702cd8d24471c228a0d3affa8c5a94fab
Brian McGee <brian@bluerosedev.com>
2019-04-22 13:01:31 +0200

    feat(bin): add s3 upload to postgres script

--- bin/postgres.sh
+++ bin/postgres.sh
@@ -9,11 +9,24 @@ source ${SCRIPT_DIR}/env.sh
 ensure
 
 run() {
+
   local command="${1}"
   local action="${2}"
 
+  local filepath="${ROOT_DIR}/datasets/ethvm_dev.sql.gz"
+
   case "${command}" in
-    dump) docker-compose exec -u postgres timescale pg_dump ethvm_dev | gzip > datasets/ethvm_dev.sql.gz ;;
+
+    dump)
+      docker-compose exec -u postgres timescale pg_dump ethvm_dev | gzip > ${filepath}
+      md5sum ${filepath} > ${filepath}.md5
+      ;;
+
+    upload-dump)
+      aws s3 cp ${filepath}.md5 s3://ethvm/datasets/ --profile ethvm --acl public-read
+      aws s3 cp ${filepath} s3://ethvm/datasets/ --profile ethvm --acl public-read
+      ;;
+
   esac
 }
 run "$@"
