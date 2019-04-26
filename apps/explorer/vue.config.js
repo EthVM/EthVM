300d88c0c88f05d7383ec46a043b3ad863b8ea9d
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-04-25 19:14:36 -0700

    Adde common, dev and productions webpack configurations

--- apps/explorer/vue.config.js
+++ apps/explorer/vue.config.js
@@ -1,7 +1,45 @@
 const path = require('path')
 const webpack = require('webpack')
+const merge = require('webpack-merge')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+const webpackCommon = {
+  plugins: [ new VuetifyLoaderPlugin(), new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)],
+  resolve: {
+    extensions: ['.ts', '.vue', '.json'],
+    alias: {
+      '@app': path.join(__dirname, '/src/'),
+      cssPath: path.join(__dirname, '/src/css'),
+      vue$: 'vue/dist/vue.esm.js',
+      vuex$: 'vuex/dist/vuex.esm.js'
+    }
+  }
+}
+
+const webpackDevelopment = {
+}
+
+const webpackProduction = {
+  plugins: [new BundleAnalyzerPlugin()],
+  optimization: {
+    namedModules: true,
+    moduleIds: 'size',
+    mangleWasmImports: true,
+    splitChunks: {
+      maxSize: 244000,
+      minChunks: 1,
+      cacheGroups: {
+        vendors: {
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
+  // stats: {
+  //   warningsFilter: /mini-css-extract-plugin[^]*Conflicting order between:/
+  // }
+}
 
 module.exports = {
   chainWebpack: config => {
@@ -9,6 +47,13 @@ module.exports = {
       args[0].hash = true
       return args
     })
+    // Vue Loader
+    config.module
+      .rule('vue')
+      .test(/\.vue$/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
 
     // GraphQL Loader
     config.module
@@ -18,33 +63,7 @@ module.exports = {
       .loader('graphql-tag/loader')
       .end()
   },
-  configureWebpack: {
-    plugins: [new VuetifyLoaderPlugin(), new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/), new BundleAnalyzerPlugin()],
-    resolve: {
-      extensions: ['.ts', '.vue', '.json'],
-      alias: {
-        '@app': path.join(__dirname, '/src/'),
-        cssPath: path.join(__dirname, '/src/css'),
-        vue$: 'vue/dist/vue.esm.js',
-        vuex$: 'vuex/dist/vuex.esm.js'
-      }
-    },
-    optimization: {
-      namedModules: true,
-      moduleIds: 'size',
-      mangleWasmImports: true,
-      splitChunks: {
-        maxSize: 244000,
-        minChunks: 1,
-        maxInitialRequests: 3,
-        cacheGroups: {
-          vendors: {
-            reuseExistingChunk: true
-          }
-        }
-      }
-    }
-  },
+  configureWebpack: process.env.NODE_ENV === 'production' ? merge( webpackCommon, webpackProduction) : webpackCommon,
   productionSourceMap: false,
   devServer: {
     disableHostCheck: true,
