2eb4fabe15526cd1bd3ba0d21ce2c6016984dc33
Caroline <caroline@bluerosedev.com>
2019-04-29 17:44:58 +0200

    feat: Add utils method to check if a Buffer is a GZIP file, decompress TxDto input if GZIP

--- apps/api/src/modules/txs/dto/tx.dto.ts
+++ apps/api/src/modules/txs/dto/tx.dto.ts
@@ -1,8 +1,9 @@
 import { Transaction } from '@app/graphql/schema'
-import { assignClean } from '@app/shared/utils'
+import { assignClean, isGzip } from '@app/shared/utils'
 import { TransactionEntity } from '@app/orm/entities/transaction.entity'
 import { TxReceiptDto } from '@app/modules/txs/dto/tx-receipt.dto'
 import { TxTraceDto } from '@app/modules/txs/dto/tx-trace.dto'
+import zlib from 'zlib'
 
 export class TxDto extends Transaction {
   constructor(data: TransactionEntity) {
@@ -21,5 +22,13 @@ export class TxDto extends Transaction {
     delete data.traces
 
     assignClean(this, data)
+
+    // Decompress input field if necessary
+
+    const { input } = data
+    if (input && isGzip(input)) {
+      this.input = zlib.gunzipSync(input)
+    }
+
   }
 }
