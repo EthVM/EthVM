85f6238ef311cdc7dbd45d93d3facba486cf385d
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-04-30 13:14:13 -0700

    Contracts Row Mobile added

--- apps/explorer/src/modules/addresses/components/TableAddressContractsRow.vue
+++ apps/explorer/src/modules/addresses/components/TableAddressContractsRow.vue
@@ -1,6 +1,44 @@
 <template>
-  <div>
-    <v-layout grid-list-lg row wrap align-center justify-start fill-height pa-3>
+  <v-container pa-0 ma-0>
+    <v-layout d-block>
+      <!--
+      =====================================================================================
+        Mobile (XS)
+      =====================================================================================
+      -->
+      <v-flex xs12 hidden-sm-and-up>
+        <div class="table-row-mobile">
+        <v-layout grid-list-xs row wrap align-center justify-start fill-height class="pt-3 pb-3 pr-4 pl-4">
+          <v-flex xs12 pb-1>
+             <app-time-ago :timestamp="contract.tx.getTimestamp()" />
+          </v-flex>
+          <v-flex xs12>
+            <v-layout grid-list-xs row align-center justify-start fill-height class="pl-2 pr-2 pt-1 pb-1">
+              <p class="info--text tx-hash "> {{$tc('contract.name', 1)}}: </p>
+              <app-hash-concat :hash="contract.address" :link="`/address/${contract.address}`" :italic="true"/>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12>
+            <v-layout grid-list-xs row align-center justify-start fill-height class="pl-2 pr-2 pt-1 pb-1">
+              <p class="info--text tx-hash"> {{ $t('contract.created') }} {{ $tc('tx.hash', 1)}}: </p>
+              <app-hash-concat :hash="contract.tx.getHash()" :link="`/tx/${contract.tx.getHash()}`" :italic="true"/>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 pt-1>
+            <p class="info--text tx-hash"> {{ $t('tx.cost') }}:
+              <span class="black--text">{{ getTxFee(contract.tx) }}</span>
+            </p>
+          </v-flex>
+        </v-layout>
+        </div>
+      </v-flex>
+      <!--
+      =====================================================================================
+        Desktop (SM-LG)
+      =====================================================================================
+      -->
+      <v-flex hidden-xs-only>
+      <v-layout grid-list-lg row wrap align-center justify-start fill-height pa-3>
         <v-flex sm4 class="pr-5">
           <app-hash-concat :hash="contract.address" :link="`/address/${contract.address}`" :italic="true"/>
         </v-flex>
@@ -18,7 +56,9 @@
         </v-flex>
     </v-layout>
     <v-divider />
-  </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -63,5 +103,48 @@ export default class TableAddressContractsRow extends Mixins(StringConcatMixin) 
     min-width: fit-content;
     padding-right: 5px;
   }
-</style>
 
+.table-row-mobile {
+  border: 1px solid #b4bfd2;
+}
+
+p {
+  margin-bottom: 0px;
+  padding-bottom: 0px;
+}
+
+.tx-status-fail {
+  border-left: 2px solid #fe1377;
+}
+
+.tx-status-sucess {
+  border-left: 2px solid #40ce9c;
+}
+
+.tx-self {
+  color: #8391a8;
+}
+
+.tx-in {
+  color: black;
+}
+
+.tx-out {
+  color: #fe8778;
+}
+
+.tx-icon-type {
+  padding: 0px 4px;
+  background-color: #98a8c2;
+  border-radius: 2px;
+  color: white;
+  text-transform: uppercase;
+  font-size: 10px;
+  height: 16px;
+  margin: 0px;
+}
+
+.p-row{
+  display:inline;
+}
+</style>
