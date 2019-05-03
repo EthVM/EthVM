830cab7c1b2b4b6bdb5405bb1772b06366beac41
Brian McGee <brian@bluerosedev.com>
2019-05-03 10:08:26 +0200

    fix: import errors

--- apps/processing/kafka-streams/src/main/kotlin/com/ethvm/kafka/streams/Serdes.kt
+++ apps/processing/kafka-streams/src/main/kotlin/com/ethvm/kafka/streams/Serdes.kt
@@ -15,6 +15,8 @@ import com.ethvm.avro.capture.TransactionRecord
 import com.ethvm.avro.capture.UncleListRecord
 import com.ethvm.avro.capture.UncleRecord
 import com.ethvm.avro.processing.BlockAuthorRecord
+import com.ethvm.avro.processing.BlockKeyRecord
+import com.ethvm.avro.processing.BlockMetricKeyRecord
 import com.ethvm.avro.processing.BlockMetricsHeaderRecord
 import com.ethvm.avro.processing.BlockMetricsTransactionFeeRecord
 import com.ethvm.avro.processing.BlockMetricsTransactionRecord
