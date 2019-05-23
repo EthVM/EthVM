e7f16fdec61602e40db8e801bcd35f7363721a06
Caroline <caroline@bluerosedev.com>
2019-05-23 10:00:24 +0200

    feat: Refactor AppSearch to use vue-apollo smart query

---
+++ apps/explorer/src/core/api/apollo/types/SearchResult.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { SearchType } from "./globalTypes";
+
+// ====================================================
+// GraphQL fragment: SearchResult
+// ====================================================
+
+export interface SearchResult {
+  __typename: "Search";
+  type: SearchType;
+}
