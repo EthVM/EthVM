6cb806bc2c76174a12ae6099725a8ae435262259
Caroline <caroline@bluerosedev.com>
2019-06-13 16:02:48 +0100

    fix: Remove unnecessary global ValidationPipe from api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,5 @@
 import { AppModule } from '@app/app.module'
 import { ConfigService } from '@app/shared/config.service'
-import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import ExpressRateLimit from 'express-rate-limit'
 import helmet from 'helmet'
@@ -9,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors()
-  app.useGlobalPipes(new ValidationPipe())
 
   const config = app.get(ConfigService)
 
