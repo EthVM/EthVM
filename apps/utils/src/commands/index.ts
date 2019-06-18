decf5a6716155c6aec004cec6836484e94ea4145
Brian McGee <brian@bluerosedev.com>
2019-06-18 17:05:42 +0100

    feat: improvements to verification tools

--- apps/utils/src/commands/index.ts
+++ apps/utils/src/commands/index.ts
@@ -1,3 +1,4 @@
 export * from '@app/commands/coingecko/eth-tokens-list-to-coingecko-ids'
 export * from '@app/commands/verifier/erc20-balances'
 export * from '@app/commands/verifier/ether-balances'
+export * from '@app/commands/verifier/canonical-topics-sequence-checker'
