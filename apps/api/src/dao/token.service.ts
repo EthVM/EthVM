4c6cc90e378faca4c211754a6582c812f04c3413
Caroline <caroline@bluerosedev.com>
2019-06-20 16:04:07 +0100

    feat: Update and add to TokenResolvers unit tests

--- apps/api/src/dao/token.service.ts
+++ apps/api/src/dao/token.service.ts
@@ -96,7 +96,7 @@ export class TokenService {
   }
 
   async findTokenExchangeRates(
-    sort: string,
+    sort: string = 'market_cap_rank',
     limit: number = 10,
     offset: number = 0,
     symbols: string[] = [],
