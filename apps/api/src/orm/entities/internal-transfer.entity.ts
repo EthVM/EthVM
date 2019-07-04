43e7f601bfde2324ef4c23cb7ce4dd3911fff934
Caroline <caroline@bluerosedev.com>
2019-07-04 16:45:52 +0100

    feat: Add InternalTransferEntity to orm entities

---
+++ apps/api/src/orm/entities/internal-transfer.entity.ts
@@ -0,0 +1,55 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm'
+import { assignClean } from '@app/shared/utils'
+import { BigNumberTransformer } from '@app/orm/transformers/big-number.transformer'
+import BigNumber from 'bignumber.js'
+
+@Entity('canonical_internal_transfer')
+export class InternalTransferEntity {
+
+  constructor(data: any) {
+    assignClean(this, data);
+  }
+
+  @PrimaryColumn({type: 'bigint', readonly: true})
+  id!: string
+
+  @Column({type: 'character', length: 42, readonly: true})
+  to!: string
+
+  @Column({type: 'character varying', length: 32, readonly: true})
+  deltaType!: string
+
+  @Column({type: 'character', length: 42, readonly: true})
+  from?: string
+
+  @Column({type: 'character', length: 42, readonly: true})
+  contractAddress?: string
+
+  @Column({type: 'character varying', length: 32, readonly: true})
+  tokenType!: string
+
+  @Column({type: 'numeric', readonly: true, transformer: new BigNumberTransformer()})
+  amount!: BigNumber
+
+  @Column({type: 'character', length: 66, readonly: true})
+  traceLocationBlockHash!: string
+
+  @Column({type: 'numeric', readonly: true, transformer: new BigNumberTransformer()})
+  traceLocationBlockNumber!: BigNumber
+
+  @Column({type: 'character', length: 66, readonly: true})
+  traceLocationTransactionHash?: string
+
+  @Column({type: 'integer', readonly: true})
+  traceLocationTransactionIndex?: number
+
+  @Column({type: 'integer', readonly: true})
+  traceLocationLogIndex?: number
+
+  @Column({type: 'text', length: 64, readonly: true})
+  traceLocationTraceAddress!: string
+
+  @Column({type: 'timestamp', readonly: true})
+  timestamp!: Date
+
+}
