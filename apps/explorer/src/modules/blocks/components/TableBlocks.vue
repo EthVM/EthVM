da614e03688ba5951546189fbb2c1a292cb1d403
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-07-10 15:20:13 -0700

    chore: Lint Code

--- apps/explorer/src/modules/blocks/components/TableBlocks.vue
+++ apps/explorer/src/modules/blocks/components/TableBlocks.vue
@@ -7,17 +7,7 @@
       TITLE
     =====================================================================================
     -->
-    <v-layout
-      v-if="pageType != 'home'"
-      align-end
-      justify-space-between
-      row
-      wrap
-      fill-height
-      pb-1
-      pr-2
-      pl-2
-    >
+    <v-layout v-if="pageType != 'home'" align-end justify-space-between row wrap fill-height pb-1 pr-2 pl-2>
       <v-flex xs12 sm5 md4 class="title-live" pb-0>
         <v-layout align-end justify-start row fill-height>
           <v-card-title class="title font-weight-bold pl-2">{{ getTitle }}</v-card-title>
@@ -44,12 +34,7 @@
       <v-spacer />
       <v-flex xs4 md1>
         <v-layout justify-end>
-          <v-btn
-            outline
-            color="secondary"
-            class="text-capitalize"
-            to="/blocks"
-          >{{ $t('btn.view-all') }}</v-btn>
+          <v-btn outline color="secondary" class="text-capitalize" to="/blocks">{{ $t('btn.view-all') }}</v-btn>
         </v-layout>
       </v-flex>
     </v-layout>
@@ -67,13 +52,7 @@
     -->
     <v-layout pl-2 pr-2>
       <v-flex hidden-xs-only sm12>
-        <v-card
-          v-if="!hasError"
-          color="info"
-          flat
-          class="white--text pl-3 pr-1 table-blocks-header-card"
-          height="40px"
-        >
+        <v-card v-if="!hasError" color="info" flat class="white--text pl-3 pr-1 table-blocks-header-card" height="40px">
           <v-layout align-center justify-start row fill-height pr-3>
             <v-flex sm2>
               <h5>{{ $t('block.number') }}</h5>
@@ -104,16 +83,8 @@
 
         <div xs12 v-if="loading">
           <div v-for="i in maxItems" :key="i">
-            <div :class="[$vuetify.breakpoint.name === 'xs'? 'table-row-mobile ma-2' : '']">
-              <v-layout
-                grid-list-xs
-                row
-                wrap
-                align-center
-                justify-start
-                fill-height
-                class="pl-4 pr-4 pt-2"
-              >
+            <div :class="[$vuetify.breakpoint.name === 'xs' ? 'table-row-mobile ma-2' : '']">
+              <v-layout grid-list-xs row wrap align-center justify-start fill-height class="pl-4 pr-4 pt-2">
                 <v-flex xs4 sm2>
                   <v-flex xs12 class="table-row-loading"></v-flex>
                 </v-flex>
