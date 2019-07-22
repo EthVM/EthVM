47084fbaf209df53ee4217847010e0a9b60ade0a
Caroline <caroline@bluerosedev.com>
2019-07-22 16:48:38 +0100

    Backup

--- apps/api/src/orm/orm.module.ts
+++ apps/api/src/orm/orm.module.ts
@@ -14,7 +14,7 @@ import { DbConnection, SnakeCaseNamingStrategy } from '@app/orm/config'
         synchronize: false,
         namingStrategy: new SnakeCaseNamingStrategy(),
         entities: ['src/**/**.entity{.ts,.js}'],
-        logging: ['error'],
+        logging: ['error', 'query'],
         maxQueryExecutionTime: 1000,
         cache: {
           type: 'redis',
