860c27ae248721cabb3f074f4800de99e4322c12
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-07-24 17:09:40 -0700

    feat(breadcrumb refactor): simplified crumb prop, changed component logic

--- apps/explorer/src/core/components/ui/AppBreadCrumbs.vue
+++ apps/explorer/src/core/components/ui/AppBreadCrumbs.vue
@@ -1,20 +1,21 @@
 <template>
   <v-card flat color="transparent" class="mt-2 mb-4">
     <v-layout row wrap align-center justify-start pa-2>
+      <v-card-title class="crumb-container">
+        <v-icon small :class="[ isHome ? 'black--text  pr-2' : 'info--text  pr-2']">fa fa-home</v-icon>
+          <router-link :class="[ isHome ? 'black--text' : 'info--text']" to="/">{{ $t('home') }}</router-link>
+      </v-card-title>
+      <div v-if="!isHome" >
       <v-card-title v-for="(item, i) in crumbs" :key="i" class="crumb-container">
-        <v-icon small v-if="item.icon" :class="[item.disabled ? 'black--text  pr-2' : 'info--text  pr-2']">{{ item.icon }}</v-icon>
         <p v-if="item.link">
-          <router-link :class="[item.disabled ? 'black--text' : 'info--text']" :to="item.link">{{ getText(item) }}</router-link>
+          <router-link :class="['info--text']" :to="item.link">{{ getText(item) }}</router-link>
         </p>
         <div v-else>
           <p class="black--text">{{ getText(item) }}</p>
         </div>
-        <div v-if="item.label">
-          <p v-if="!item.label.hash">: {{ item.label.name }}</p>
-          <p v-else>: {{ getHash(item.label.name) }}</p>
-        </div>
         <p v-if="i < crumbs.length - 1" class="pl-1 pr-0 caption">/</p>
       </v-card-title>
+      </div>
     </v-layout>
   </v-card>
 </template>
@@ -35,60 +36,12 @@ export default class AppBreadCrumbs extends Vue {
 
   /*
   ===================================================================================
-    Initial Data
-  ===================================================================================
-  */
-
-  crumbs: Crumb[] = []
-
-  /*
-  ===================================================================================
-    Lifecycle
-  ===================================================================================
-  */
-
-  mounted() {
-    this.prepareBreadcrumbs()
-  }
-
-  /*
-  ===================================================================================
-    Watch
-  ===================================================================================
-  */
-
-  @Watch('newItems')
-  onNewItemsChange() {
-    this.crumbs = []
-    this.prepareBreadcrumbs()
-  }
-
-  /*
-  ===================================================================================
     Methods
   ===================================================================================
   */
 
-  prepareBreadcrumbs(): void {
-    this.setHome()
-    this.addNewItems()
-  }
-
-  setHome(): void {
-    this.crumbs.push(this.home)
-  }
-
-  addNewItems(): void {
-    if (this.newItems) {
-      this.crumbs[0].disabled = false
-      for (const i in this.newItems) {
-        this.crumbs.push(this.newItems[i])
-      }
-    }
-  }
-
-  getText(item: Crumb) {
-    return item.plural ? this.$tc(item.text, item.plural) : this.$t(item.text)
+  getText(item: Crumb):string  {
+    return item.hash? `${item.text}: ${this.getHash(item.hash)}`: `${item.text}`
   }
 
   getHash(hash: string): string {
@@ -96,19 +49,15 @@ export default class AppBreadCrumbs extends Vue {
     return `${hash.slice(0, 4)}...${hash.slice(n - 4, n)}`
   }
 
+
   /*
   ===================================================================================
     Computed Values
   ===================================================================================
   */
 
-  get home(): Crumb {
-    return {
-      text: 'home',
-      disabled: true,
-      icon: 'fa fa-home',
-      link: '/'
-    }
+  get isHome(): boolean {
+    return !this.newItems
   }
 }
 </script>
@@ -123,8 +72,4 @@ export default class AppBreadCrumbs extends Vue {
   margin-right: 0.5em;
 }
 
-p {
-  margin-bottom: 0px;
-  padding-bottom: 0px;
-}
 </style>
