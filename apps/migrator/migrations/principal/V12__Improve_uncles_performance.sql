00ccb5021b00eb677e693d9555bf1690395a3bad
Caroline <caroline@bluerosedev.com>
2019-07-29 16:29:22 +0100

    fix: Fixes for db-principal migrations

--- apps/migrator/migrations/principal/V12__Improve_uncles_performance.sql
+++ apps/migrator/migrations/principal/V12__Improve_uncles_performance.sql
@@ -2,8 +2,7 @@ DROP VIEW canonical_uncle;
 
 CREATE VIEW canonical_uncle AS
 SELECT
-  u.*,
-  fbd.amount as reward_amount
+  u.*
 FROM
   uncle AS u
     RIGHT JOIN canonical_block_header AS cb ON u.nephew_hash = cb.hash
--- apps/migrator/migrations/principal/V2__Internal_transfer_view.sql
+++ apps/migrator/migrations/principal/V2__Internal_transfer_view.sql
@@ -1,18 +1,21 @@
-DROP VIEW canonical_internal_transfer;
-
 CREATE VIEW canonical_internal_transfer AS
 SELECT fbd.id,
-       fbd.counterpart_address,
-       fbd.address,
+       fbd.counterpart_address      AS "from",
+       fbd.address                  AS "to",
        fbd.contract_address,
        fbd.delta_type,
+       fbd.token_type,
        fbd.amount,
        fbd.trace_location_block_hash,
        fbd.trace_location_block_number,
        fbd.trace_location_transaction_hash,
        fbd.trace_location_transaction_index,
+       fbd.trace_location_log_index,
+       fbd.trace_location_trace_address,
        fbd.trace_location_timestamp AS "timestamp"
 FROM fungible_balance_delta AS fbd
-  RIGHT JOIN canonical_block_header AS cb ON fbd.trace_location_block_hash = cb.hash
+         RIGHT JOIN canonical_block_header AS cb ON fbd.trace_location_block_hash = cb.hash
 WHERE cb.number IS NOT NULL
+  AND fbd.address IS NOT NULL
+  AND fbd.amount > 0
   AND fbd.delta_type IN ('INTERNAL_TX', 'CONTRACT_CREATION', 'CONTRACT_DESTRUCTION');
