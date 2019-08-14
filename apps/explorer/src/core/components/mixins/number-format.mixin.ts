f0978b96714d403c44813547e4ac52e6c4e2d892
Caroline <caroline@bluerosedev.com>
2019-08-14 12:44:38 +0100

    feat: Refactor NumberFormatMixin

--- apps/explorer/src/core/components/mixins/number-format.mixin.ts
+++ apps/explorer/src/core/components/mixins/number-format.mixin.ts
@@ -2,11 +2,28 @@ import { Component, Vue } from 'vue-property-decorator'
 import BigNumber from 'bignumber.js'
 import { EthValue } from '@app/core/models'
 
-export enum EthUnit {
+export enum FormattedNumberUnit {
   ETH = 'Eth',
   GWEI = 'Gwei',
-  WEI = 'Wei'
+  WEI = 'Wei',
+  PERCENT = '%',
+  USD = '$'
 }
+
+export interface FormattedNumber {
+  value: string
+  unit?: FormattedNumberUnit
+  tooltipText?: string
+}
+
+const SmallUsdBreakpoint = 0.04
+const SmallNumberBreakpoint = 0.0000001
+
+const OneHundredThousand = 100000
+const OneMillion = 1000000
+const OneBillion = 1000000000
+const OneTrillion = 1000000000000
+const TenTrillion = 10000000000000
 
 @Component
 export class NumberFormatMixin extends Vue {
@@ -20,123 +37,92 @@ export class NumberFormatMixin extends Vue {
   /* General numeric values */
 
   /**
-   * Converts a general numeric value to a formatted string
+   * Converts a general numeric value to a FormattedNumber object
    * @param value BigNumber
-   * @returns formatted string
+   * @returns Object FormattedNumber with value as formatted string, unit and tooltipText
    */
-  formatNumericValue(value: BigNumber): string {
+  formatNumericValue(value: BigNumber): FormattedNumber {
 
-    if (value.isGreaterThan(999999)) { // TODO review loss of accuracy
-      return value.toFormat(0)
+    if (value.isGreaterThanOrEqualTo(OneMillion)) { // TODO review loss of accuracy
+      return { value: value.toFormat(0) }
     }
-    if (value.isGreaterThan(1)) {
-      return value.toFormat(2) // TODO review loss of accuracy
+    if (value.isGreaterThan(1)) { // TODO review loss of accuracy
+      return { value: value.toFormat(2) }
     }
-    if (value.isLessThan(0.0000001)) {
-      return '< 0.0000001' // TODO review rules for this - string.length > 9 ?
+    if (value.isLessThan(SmallNumberBreakpoint)) { // TODO review rules for this - string.length > 9 ?
+      return { value: '< 0.0000001', tooltipText: value.toFormat() }
     }
-    return value.toFormat(7) // TODO review loss of accuracy
+    return { value: value.toFormat(7) } // TODO review loss of accuracy
   }
-
-  /**
-   * Generates tooltip text for a general numeric value where applicable
-   * @param value BigNumber
-   * @returns formatted string or undefined
-   */
-  generateTooltipNumericValue(value: BigNumber): string | undefined {
-    return value.isLessThan(0.0000001) ? value.toFormat() : undefined
-  }
-
 
   /* Eth values */
 
   /**
    * Converts a value from wei to a formatted string in an appropriate unit
    * @param value BigNumber - must be original wei value (not already converted to Eth)
-   * @returns Object { formatted: formatted string, unit: Eth, Gwei or Wei }
+   * @returns  Object FormattedNumber with value as formatted string, unit and tooltipText
    */
-  formatEthValue(value: BigNumber): {formatted: string, unit: EthUnit} {
+  formatEthValue(value: BigNumber): FormattedNumber {
 
-    if (value.isGreaterThan(9999999999999)) {
-      return { formatted: new EthValue(value).toEthFormatted().toString(), unit: EthUnit.ETH }
+    if (value.isGreaterThanOrEqualTo(TenTrillion)) {
+      return { value: new EthValue(value).toEthFormatted().toString(), unit: FormattedNumberUnit.ETH }
     }
-    if (value.isGreaterThan(100000)) {
-      return { formatted: new EthValue(value).toGWei().toString(), unit: EthUnit.GWEI }
+    if (value.isGreaterThanOrEqualTo(OneHundredThousand)) {
+      return {
+        value: new EthValue(value).toGWei().toString(),
+        unit: FormattedNumberUnit.GWEI,
+        tooltipText: `${new EthValue(value).toEthFormatted().toString()} ${FormattedNumberUnit.ETH }`
+      }
     }
-    return { formatted: value.toFormat(), unit: EthUnit.WEI }
-
+    return {
+      value: value.toFormat(),
+      unit: FormattedNumberUnit.WEI,
+      tooltipText: `${new EthValue(value).toEthFormatted().toString()} ${FormattedNumberUnit.ETH }`
+    }
   }
-
-  /**
-   * Generates tooltip text for an Eth value where applicable
-   * @param value BigNumber - must be original wei value (not already converted to Eth)
-   * @returns string containing value as Eth and unit, or undefined if value should be displayed in Eth
-   */
-  generateTooltipEthValue(value: BigNumber): string | undefined {
-    return value.isLessThanOrEqualTo(9999999999) ? `${ new EthValue(value).toEthFormatted().toString()} ${EthUnit.ETH }` : undefined
-  }
-
 
   /* USD values */
 
   /**
    * Converts a USD value to a formatted string
    * @param value: BigNumber
-   * @returns formatted string
+   * @returns Object FormattedNumber with value as formatted string, unit and tooltipText
    */
-  formatUsdValue(value: BigNumber): string {
+  formatUsdValue(value: BigNumber): FormattedNumber {
 
-    if (value.isGreaterThan(999999999)) {
-      // TODO round to nearest billion or trillion
-      return '1B'
+    const unit = FormattedNumberUnit.USD
+    if (value.isGreaterThanOrEqualTo(OneTrillion)) { // 1 Trillion or greater
+      return { value: `${value.dividedBy(OneTrillion).toFormat(0)}T`, unit, tooltipText: value.toFormat(0) }
     }
-    if (value.isGreaterThanOrEqualTo(0.004)) {
-      return value.toFormat(2)
+    if (value.isGreaterThanOrEqualTo(OneBillion)) { // 1 Billion or greater
+      return { value: `${value.dividedBy(OneBillion).toFormat(0)}B`, unit,  tooltipText: value.toFormat(0) }
     }
-    if (value.isLessThan(0.0000001)) {
-      return '< 0.0000001'
+    if (value.isGreaterThanOrEqualTo(SmallUsdBreakpoint)) { // 0.004 or greater
+      return { value: value.toFormat(2), unit }
     }
-    return value.toFormat(7) // TODO Confirm if this should only show as many decimal points as are present
-  }
-
-  /**
-   * Generates tooltip text where applicable for a USD value
-   * @param value: BigNumber
-   */
-  generateTooltipUsdValue(value: BigNumber): string | undefined {
-    if (value.isLessThan(0.0000001)) {
-      return value.toFormat()
+    if (value.isLessThan(SmallNumberBreakpoint)) { // Less than 0.0000001
+      return { value: '< 0.0000001', unit, tooltipText: value.toFormat() }
     }
-    if (value.isGreaterThan(999999999)) {
-      return value.toFormat(0)
-    }
+    return { value: value.toFormat(7), unit } // TODO Confirm if this should only show as many decimal points as are present
   }
 
   /* Percentage values */
 
   /**
    * Converts a percentage value to a formatted string
-   * @param value: number already converted to a percentage e.g. < 100 (expect in special cases)
-   * @returns formatted string with "%" unit
+   * @param value: BigNumber already converted to a percentage e.g. < 100 (expect in special cases)
+   * @returns Object FormattedNumber with value as formatted string, unit and tooltipText
    */
-  formatPercentageValue(value: number): string {
+  formatPercentageValue(value: BigNumber): FormattedNumber {
 
-    if (value < 0.01) {
-      return '< 0.01%'
+    const unit = FormattedNumberUnit.PERCENT
+    if (value.isLessThan(0.01)) {
+      return { value: '< 0.01', unit }
     }
-    if (value > 100) {
-      return '> 100%'
+    if (value.isGreaterThan(100)) {
+      return { value: '> 100%', unit, tooltipText: `${ new BigNumber(value).toFormat(2)}%` }
     }
-    return `${new BigNumber(value).toFormat(2)}%`
-  }
-
-  /**
-   * Generates tooltip text where applicable for a percentage value
-   * @param value: number already converted to a percentage e.g. < 100 (expect in special cases)
-   * @returns formatted string with "%" unit or undefined
-   */
-  generateTooltipPercentageValue(value: number): string | undefined {
-    return value > 100 ? `${ new BigNumber(value).toFormat(2)}%` : undefined // TODO confirm dps
+    return { value: new BigNumber(value).toFormat(2), unit }
   }
 
 }
