261b3d021382a8958af41169b1cc0b0f320e49d7
Aldo Borrero <aldo@aldoborrero.com>
2019-09-02 11:40:40 +0200

    chore: cleanup of ChainId.kt for those networks that are not being actively used

--- apps/processing/common/src/main/kotlin/com/ethvm/common/config/ChainId.kt
+++ apps/processing/common/src/main/kotlin/com/ethvm/common/config/ChainId.kt
@@ -3,21 +3,8 @@ package com.ethvm.common.config
 enum class ChainId(val number: Int) {
 
   Mainnet(1),
-  Morden(2),
   Ropsten(3),
-  Rinkleby(4),
-  UbiqMainNet(8),
-  UbiqTestNest(9),
-  Dev(17),
-  RootStockMainNet(30),
-  RootStockTestNet(31),
-  Kovan(42),
-  EthClassicMainNet(61),
-  EthClassicTestNet(62),
-  EWasmTestNet(66),
-  GethPrivateChains(1337),
-  Görli(6284),
-  Stureby(314158);
+  Dev(17);
 
   companion object {
 
