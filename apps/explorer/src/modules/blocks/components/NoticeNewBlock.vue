20854a3af0b6716195361493ba734b043f3895c3
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-09-09 17:53:39 -0700

    fix: added new strigns and funct to component

--- apps/explorer/src/modules/blocks/components/NoticeNewBlock.vue
+++ apps/explorer/src/modules/blocks/components/NoticeNewBlock.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-btn flat class="new-block-alert text-capitalize  ma-0" @click="onReload" v-if="display">
-    {{ buttonText }} <v-icon class="ml-1 secondary--text fas fa-sync fa-1x fa-rotate-90"></v-icon>
+  <v-btn flat class="new-block-alert text-capitalize ma-0" @click="onReload" v-if="display">
+    {{ buttonText }}
+    <v-icon class="ml-1 secondary--text fas fa-sync fa-1x fa-rotate-90"></v-icon>
   </v-btn>
 </template>
 
@@ -14,25 +15,67 @@ import { TranslateResult } from 'vue-i18n'
     $subscribe: {
       newBlock: {
         query: newBlock,
-
         result({ data }) {
           ;(this as any).display = true
+        },
+        variables: {
+          block: 0
         }
       }
     }
   }
 })
 export default class NoticeNewBlock extends Vue {
-  @Prop(String) message?: string
-  display: boolean = false
+  /*
+    ===================================================================================
+      Props:
+    ===================================================================================
+  */
+  @Prop({ type: String, default: 'block' }) pageID!: string // Ids: block, uncles, txs, pending
 
+  /*
+    ===================================================================================
+      Data:
+    ===================================================================================
+  */
+  display: boolean = false
+  countTotal: number = 0
+
+  /*
+    ===================================================================================
+      Lifecycle:
+    ===================================================================================
+  */
+  mounted() {
+    //Check for valid ids:
+    const validID = ['block', 'tx', 'pending', 'uncle']
+    if (!validID.includes(this.pageID)) {
+      throw new Error('Invalid pageID for notice new block component: ' + this.pageID)
+    }
+  }
+
+  /*
+    ===================================================================================
+      Methods:
+    ===================================================================================
+  */
   onReload() {
     this.$emit('reload')
     this.display = false
+    this.countTotal = 0
   }
 
-  get buttonText(): TranslateResult {
-    return this.message || this.$tc('message.update.block', 1)
+
+  /*
+    ===================================================================================
+      Computed:
+    ===================================================================================
+  */
+
+  get buttonText(): String {
+    const plural = this.countTotal === 1 ? 1 : 2
+    const message = `message.update.${this.pageID}`
+    return `${this.countTotal} ${this.$tc(message, plural)}`
   }
 }
 </script>
