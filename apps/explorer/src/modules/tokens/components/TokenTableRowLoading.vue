5b67e81336bd4d9e35ebfd11db8d3678c9940f36
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-09-12 15:05:30 -0700

    feat(loading layout): tokens table loading row fixed tally

--- apps/explorer/src/modules/tokens/components/TokenTableRowLoading.vue
+++ apps/explorer/src/modules/tokens/components/TokenTableRowLoading.vue
@@ -1,42 +1,64 @@
 <template>
   <v-card flat white>
-    <v-layout>
+    <v-layout class="pr-1 pl-1">
+      <!--
+        =====================================================================================
+          Mobile (XS)
+        =====================================================================================
+      -->
       <v-flex xs12 hidden-sm-and-up>
-        <div class="token-mobile">
-          <v-layout grid-list-xs row wrap align-center justify-start fill-height>
+        <div class="table-row-loading-mobile">
+          <v-layout grid-list-xs row wrap align-center justify-start fill-height class="pr-2 pl-2">
             <v-flex xs2>
-              <div class="table-row-loading"></div>
+              <div class="token-image-mobile">
+                <v-img :src="require('@/assets/icon-token.png')" contain />
+              </div>
             </v-flex>
             <v-flex xs8>
-              <div class="table-row-loading"></div>
-              <div class="table-row-loading"></div>
-              <div class="table-row-loading"></div>
+              <div class="table-row-loading mb-1"></div>
+              <div class="table-row-loading mb-1"></div>
+              <div class="table-row-loading mb-1"></div>
               <div class="table-row-loading"></div>
             </v-flex>
             <v-flex xs2>
-              <div class="table-row-loading"></div>
+              <div class="table-row-loading loading-token-button"></div>
             </v-flex>
           </v-layout>
         </div>
       </v-flex>
+      <!--
+        =====================================================================================
+          Desktop (SM-LG)
+        =====================================================================================
+      -->
       <v-flex hidden-xs-only sm12>
-        <v-layout grid-list-xs row wrap align-center justify-start fill-height class="pl-2 pr-2 pt-2">
-          <v-flex xs1>
+        <v-layout
+          grid-list-xs
+          row
+          wrap
+          align-center
+          justify-start
+          fill-height
+          class="pr-2 pt-2"
+        >
+          <v-flex sm1>
+            <div class="token-image">
+              <v-img :src="require('@/assets/icon-token.png')" contain />
+            </div>
+          </v-flex>
+          <v-flex sm3>
             <div class="table-row-loading"></div>
           </v-flex>
-          <v-flex xs2>
+          <v-flex sm2>
             <div class="table-row-loading"></div>
           </v-flex>
-          <v-flex xs2>
+          <v-flex sm2>
             <div class="table-row-loading"></div>
           </v-flex>
-          <v-flex xs1>
+          <v-flex sm2>
             <div class="table-row-loading"></div>
           </v-flex>
-          <v-flex xs3>
-            <div class="table-row-loading"></div>
-          </v-flex>
-          <v-flex xs3>
+          <v-flex sm2>
             <div class="table-row-loading"></div>
           </v-flex>
         </v-layout>
@@ -57,9 +79,11 @@ export default class TokenTableRowLoading extends Vue {}
 .table-row-loading {
   margin: 1px;
 }
-.token-mobile {
+.table-row-loading-mobile {
   border: 1px solid #b4bfd2;
   padding: 10px 0px 10px 0px;
-  height: 68px;
+}
+.loading-token-button {
+  height: 2em;
 }
 </style>
