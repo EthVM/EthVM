840293618eed9c47f71678df07e78d95d891200e
Jessica <jessicaspeng@gmail.com>
2020-09-01 18:41:45 -0700

    Fix lint

--- newclient/src/core/helper/eth.ts
+++ newclient/src/core/helper/eth.ts
@@ -3,7 +3,7 @@ import web3 from 'web3'
 const eth = {
     isValidHash: (raw: any): boolean => /^(0x[0-9a-fA-F]{64}|0xGENESIS_{17}[0-9a-fA-F]{40})$/.test(raw),
     isValidBlockNumber: (raw: any = ''): boolean => {
-        const isNum = /^\d+$/.test(raw);
+        const isNum = /^\d+$/.test(raw)
         const n = Number(raw)
         return !isNaN(n) && Number.isInteger(n) && n >= 0 && isNum
     },
