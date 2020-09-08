2e68af734f54ad6e85ca405304867df0371807d0
Jessica <jessicaspeng@gmail.com>
2020-09-08 14:27:09 -0700

    Add pending queries

--- newclient/src/core/components/layout/TheNavigationDrawer.vue
+++ newclient/src/core/components/layout/TheNavigationDrawer.vue
@@ -219,19 +219,19 @@ export default class TheNavigationDrawer extends Vue {
                     text: this.$i18n.tc('tx.name', 2),
                     icon: 'fas fa-exchange-alt',
                     routerLink: '/txs'
-                }
-                // links: [
-                //     {
-                //         text: this.$i18n.t('tx.mined'),
-                //         routerLink: '/txs',
-                //         name: 'transactions'
-                //     }
-                // {
-                //   text: this.$i18n.tc('tx.pending', 2),
-                //   routerLink: '/pending-txs',
-                //   name: 'pending'
-                // }
-                // ]
+                },
+                links: [
+                    {
+                        text: this.$i18n.t('tx.mined'),
+                        routerLink: '/txs',
+                        name: 'transactions'
+                    },
+                    {
+                        text: this.$i18n.tc('tx.pending', 2),
+                        routerLink: '/pending-txs',
+                        name: 'pending'
+                    }
+                ]
             },
             {
                 header: {
