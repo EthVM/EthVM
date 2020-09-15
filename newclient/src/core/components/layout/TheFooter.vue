05bbf144b017d4888bcd7c2e4bcc9fc086d2d44a
olgakup <olga.kupchevskaya.812@my.csun.edu>
2020-09-14 18:21:19 -0700

    chore: remove beta mentions

--- newclient/src/core/components/layout/TheFooter.vue
+++ newclient/src/core/components/layout/TheFooter.vue
@@ -119,7 +119,7 @@
                     </v-flex>
                     <v-flex xs12>
                         <v-layout align-end justify-end fill-height pr-3>
-                            <p class="info--text text-xs-right caption">Beta: 0.0.1</p>
+                            <p class="info--text text-xs-right caption">V1.0.0</p>
                         </v-layout>
                     </v-flex>
                     <v-flex xs12>
