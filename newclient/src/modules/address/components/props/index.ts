e8c613b5dac3f9c36f2598729200582d03de09dd
gamalielhere <gamalielgad1234@gmail.com>
2020-09-21 15:33:29 -0700

    run lint

--- newclient/src/modules/address/components/props/index.ts
+++ newclient/src/modules/address/components/props/index.ts
@@ -1,21 +1,21 @@
 export interface Address {
-  hash: string
-  balance: string
-  isMiner: boolean
-  isContractCreator: boolean
-  isContract: boolean
-  totalERC20: number
+    hash: string
+    balance: string
+    isMiner: boolean
+    isContractCreator: boolean
+    isContract: boolean
+    totalERC20: number
 }
 
 import { FormattedNumber } from '@app/core/helper/number-format-helper'
 
 export interface Tx {
-  hash: string
-  block: string
-  from: string
-  to: string
-  timestamp: Date
-  fee: FormattedNumber
-  value: FormattedNumber
-  status: boolean
+    hash: string
+    block: string
+    from: string
+    to: string
+    timestamp: Date
+    fee: FormattedNumber
+    value: FormattedNumber
+    status: boolean
 }
