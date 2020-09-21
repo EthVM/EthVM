0e2c4fbd9decb4a33666e684876a5bb25faaee0f
gamalielhere <gamalielgad1234@gmail.com>
2020-09-21 16:41:17 -0700

    Remove extra parenthesis

--- newclient/src/modules/address/handlers/AddressOverview/AddressOverview.vue
+++ newclient/src/modules/address/handlers/AddressOverview/AddressOverview.vue
@@ -58,7 +58,7 @@ import { ErrorMessage } from '@app/modules/address/models/ErrorMessagesForAddres
                 }
             },
             error(error) {
-                const newError = JSON.stringify(error.message))
+                const newError = JSON.stringify(error.message)
                 if (newError.includes('No contract found')) {
                     this.hasError = false
                     this.setContract(false)
