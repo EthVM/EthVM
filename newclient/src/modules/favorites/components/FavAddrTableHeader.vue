1c630be4c3609371f1bc0563ecd05ae5a104e11e
olgakup <olga.kupchevskaya.812@my.csun.edu>
2020-10-16 15:06:11 -0700

    devop(fav address): added string to localization

--- newclient/src/modules/favorites/components/FavAddrTableHeader.vue
+++ newclient/src/modules/favorites/components/FavAddrTableHeader.vue
@@ -9,7 +9,7 @@
             <h5 class="pl-4 ml-2">{{ $tc('address.name', 1) }}</h5>
         </v-flex>
         <v-flex md3>
-            <h5>Name</h5>
+            <h5>{{ $t('fav.name') }}</h5>
         </v-flex>
         <v-flex md2>
             <h5>{{ $t('common.eth-balance') }}</h5>
