3c4e4bf4b9f8f0119c97e272f96a9361c8e540ce
olgakup <olga.kupchevskaya.812@my.csun.edu>
2021-12-16 18:32:05 -0800

    devop: add federation version resolution

--- newclient/src/core/components/mixins/Greet/apolloTypes/setNotFirstTime.ts
+++ newclient/src/core/components/mixins/Greet/apolloTypes/setNotFirstTime.ts
@@ -4,9 +4,13 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
-// GraphQL query operation: getConsent
+// GraphQL mutation operation: setNotFirstTime
 // ====================================================
 
-export interface getConsent {
-  getConsentToTrack: boolean;
+export interface setNotFirstTime {
+  setNotFirstTime: boolean;
+}
+
+export interface setNotFirstTimeVariables {
+  _notFirstTimeVisit: boolean;
 }
