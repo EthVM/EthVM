4fabfac3a00317cce579dabf8b356cf833a6a3a8
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-01-06 11:20:35 -0800

    chore: lint code

--- newclient/src/core/components/ui/AppCopyToClip.vue
+++ newclient/src/core/components/ui/AppCopyToClip.vue
@@ -3,7 +3,7 @@
         <v-btn icon small class="ma-0" @click="copy"> <v-img :src="require('@/assets/icon-copy.png')" height="20px" max-width="20x" contain /></v-btn>
         <v-snackbar v-model="showCopyMes" bottom right :color="mesColor" multi-line class="break-string" timeout="3000">
             {{ message }}
-            <v-btn color="white" flat @click="showCopyMes = false" icon my-1>
+            <v-btn color="white" flat icon my-1 @click="showCopyMes = false">
                 <v-icon class="fas fa-times" small />
             </v-btn>
         </v-snackbar>
