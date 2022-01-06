62ae0b0c35ee677af46a2d6f738b0ec8a876aeb9
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-01-05 17:41:47 -0800

    devop: update package version, add snackbar message

--- newclient/src/core/components/ui/AppCopyToClip.vue
+++ newclient/src/core/components/ui/AppCopyToClip.vue
@@ -1,5 +1,13 @@
 <template>
-    <v-btn icon small class="ma-0" @click="copy"> <v-img :src="require('@/assets/icon-copy.png')" height="20px" max-width="20x" contain /></v-btn>
+    <div>
+        <v-btn icon small class="ma-0" @click="copy"> <v-img :src="require('@/assets/icon-copy.png')" height="20px" max-width="20x" contain /></v-btn>
+        <v-snackbar v-model="showCopyMes" bottom right :color="mesColor" multi-line class="break-string" timeout="3000">
+            {{ message }}
+            <v-btn color="white" flat @click="showCopyMes = false" icon my-1>
+                <v-icon class="fas fa-times" small />
+            </v-btn>
+        </v-snackbar>
+    </div>
 </template>
 
 <script lang="ts">
@@ -18,14 +26,38 @@ export default class AppCopyToClip extends Vue {
 
     /*
   ===================================================================================
+    Data
+  ===================================================================================
+  */
+
+    showCopyMes = false
+    message: any = ''
+    mesColor = 'primary'
+
+    /*
+  ===================================================================================
     Methods
   ===================================================================================
   */
     /**
      * Copies string to clipboard
      */
-    copy(): void {
-        const status = clipboardCopy(this.valueToCopy)
+    async copy(): Promise<void> {
+        this.message = ''
+        try {
+            await clipboardCopy(this.valueToCopy)
+            this.message = `${this.$t('copied')}: ${this.valueToCopy}`
+            this.mesColor = 'primary'
+        } catch (err) {
+            this.message = err
+            this.mesColor = 'error'
+        }
+        this.showCopyMes = true
     }
 }
 </script>
+<style>
+.break-string {
+    word-break: break-all;
+}
+</style>
