9d9550c037a311f21f6f10377a7667e5c7a9e11f
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-07-21 15:50:03 -0700

    fix: vuetify-loader

--- v2/vue.config.js
+++ v2/vue.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack')
+const { VuetifyPlugin } = require('webpack-plugin-vuetify')
+
 const path = require('path')
 module.exports = defineConfig({
     transpileDependencies: ['vuetify'],
     pluginOptions: {
-        vuetify: {
+        VuetifyPlugin: {
             styles: 'expose'
             // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
         }
