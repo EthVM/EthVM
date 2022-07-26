302a06b4f3eba34be618c7005ae691d05c93f8fb
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-07-26 16:51:32 -0700

    feat(wip): icon button ui

--- v2/src/core/components/AppBtnIcon.vue
+++ v2/src/core/components/AppBtnIcon.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-btn icon :color="props.color" flat :size="props.size" @click.stop="btnClick()">
-        <v-icon>{{ props.icon }}</v-icon>
+    <v-btn icon variant="plain" :size="props.size" @click.stop="btnClick()" class="button-icon" :ripple="false">
+        <v-icon :color="props.color">{{ props.icon }}</v-icon>
         <v-tooltip v-if="tooltipText !== ''" activator="parent" location="top">
             {{ tooltipText }}
         </v-tooltip>
@@ -19,7 +19,7 @@ const props = defineProps({
     },
     color: {
         type: String,
-        default: 'transparent'
+        default: 'textPrimary'
     },
     size: {
         type: String,
@@ -39,3 +39,21 @@ const btnClick = (): void => {
     emit('click')
 }
 </script>
+
+<style lang="scss" scoped>
+@use '~vuetify/lib/components/VBtn/VBtn.sass';
+
+.button-icon {
+    opacity: 100% !important;
+    .v-icon {
+        font-size: 16px;
+        transition: 0.3s;
+        &:hover {
+            opacity: 70%;
+        }
+        &:active {
+            opacity: 50%;
+        }
+    }
+}
+</style>
--- v2/src/views/ViewTemp.vue
+++ v2/src/views/ViewTemp.vue
@@ -1,7 +1,18 @@
 <template>
     <v-row>
         <v-col cols="12">
-            <p>Hello I am a temp view for: {{ routeName }}</p>
+            <v-card class="pa-6 mt-4" elevation="1" rounded="xl">
+                <v-card-title class="mb-6">Hello I am a temp view for: {{ routeName }}</v-card-title>
+                <v-row>
+                    <v-col cols="12"> <app-btn text="large button"></app-btn> </v-col>
+                    <v-col cols="12">
+                        <app-btn text="Small Button" isSmall icon="east"></app-btn>
+                    </v-col>
+                    <v-col cols="12">
+                        <app-btn-icon icon="content_copy" tooltip-text="Hello" />
+                    </v-col>
+                </v-row>
+            </v-card>
         </v-col>
     </v-row>
 </template>
@@ -9,7 +20,24 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
+import AppBtn from '@core/components/AppBtn.vue'
+import AppBtnIcon from '@/core/components/AppBtnIcon.vue'
 const routeName = computed(() => {
     return useRoute().name
 })
 </script>
+<style lang="scss" scoped>
+// @use '~vuetify/lib/components/VBtn/VBtn.sass';
+// .a {
+//     opacity: 1;
+//     &:hover {
+//         color: green;
+//     }
+// }
+// .b {
+//     opacity: 1;
+//     &:hover {
+//         font-weight: 700;
+//     }
+// }
+</style>
