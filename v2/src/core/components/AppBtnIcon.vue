546b234453994e131d17e50185cf94e379ee7a5c
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-07-28 15:42:09 -0700

    feat: icon button

--- v2/src/core/components/AppBtnIcon.vue
+++ v2/src/core/components/AppBtnIcon.vue
@@ -1,29 +1,22 @@
 <template>
-    <v-btn icon variant="plain" :size="props.size" @click.stop="btnClick()" class="button-icon" :ripple="false">
-        <v-icon :color="props.color">{{ props.icon }}</v-icon>
+    <v-btn icon color="textPrimary" variant="text" size="x-small" @click.stop="btnClick()">
+        <v-icon v-bind="$attrs"></v-icon>
         <v-tooltip v-if="tooltipText !== ''" activator="parent" location="top">
-            {{ tooltipText }}
+            {{ props.tooltipText }}
         </v-tooltip>
     </v-btn>
 </template>
 
 <script setup lang="ts">
+/**
+ * PLESE USE FALLTHROUGH vue attrs from button icon:
+ * pass `icon` prop of material design, ei: icon="content_copy"
+ * you can also pass `color`, to change icon color
+ */
 const props = defineProps({
     tooltipText: {
         type: String,
         default: ''
-    },
-    icon: {
-        type: String,
-        default: 'question_mark'
-    },
-    color: {
-        type: String,
-        default: 'textPrimary'
-    },
-    size: {
-        type: String,
-        default: 'x-small'
     }
 })
 
@@ -40,20 +33,4 @@ const btnClick = (): void => {
 }
 </script>
 
-<style lang="scss" scoped>
-@use '~vuetify/lib/components/VBtn/VBtn.sass';
-
-.button-icon {
-    opacity: 100% !important;
-    .v-icon {
-        font-size: 16px;
-        transition: 0.3s;
-        &:hover {
-            opacity: 70%;
-        }
-        &:active {
-            opacity: 50%;
-        }
-    }
-}
-</style>
+<style lang="scss" scoped></style>
--- v2/src/views/ViewTemp.vue
+++ v2/src/views/ViewTemp.vue
@@ -4,12 +4,17 @@
             <v-card class="pa-6 mt-4" elevation="1" rounded="xl">
                 <v-card-title class="mb-6">Hello I am a temp view for: {{ routeName }}</v-card-title>
                 <v-row>
-                    <v-col cols="12"> <app-btn text="large button"></app-btn> </v-col>
+                    <v-col cols="12"> <app-btn></app-btn> </v-col>
                     <v-col cols="12">
                         <app-btn text="Small Button" isSmall icon="east"></app-btn>
                     </v-col>
-                    <v-col cols="12">
-                        <app-btn-icon icon="content_copy" tooltip-text="Hello" />
+                    <v-col cols="6">
+                        <p class="text-caption">Default</p>
+                        <app-btn-icon icon="content_copy" />
+                    </v-col>
+                    <v-col cols="6">
+                        <p class="text-caption">Color + Tooltip Prop</p>
+                        <app-btn-icon icon="content_copy" color="warning" tooltip-text="Hello" />
                     </v-col>
                 </v-row>
             </v-card>
@@ -26,18 +31,4 @@ const routeName = computed(() => {
     return useRoute().name
 })
 </script>
-<style lang="scss" scoped>
-// @use '~vuetify/lib/components/VBtn/VBtn.sass';
-// .a {
-//     opacity: 1;
-//     &:hover {
-//         color: green;
-//     }
-// }
-// .b {
-//     opacity: 1;
-//     &:hover {
-//         font-weight: 700;
-//     }
-// }
-</style>
+<style lang="scss" scoped></style>
