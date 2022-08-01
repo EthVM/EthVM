ba7ca3230dc7fcb0ad90888bb2ced148c8e01c1a
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-08-01 14:57:44 -0700

    feat: expansion panel

--- v2/src/core/components/AppExpansionPanel.vue
+++ v2/src/core/components/AppExpansionPanel.vue
@@ -1,56 +1,52 @@
 <template>
-    <v-row class="ml-6 mb-3" align="center">
-        <p class="text-h6 mr-4">Eth Transfers</p>
+    <v-row v-bind="$attrs" class="ml-6 mb-3 align-center justify-start">
+        <!-- 
+            Use title-content slot to insert image before title 
+        -->
+        <slot name="title-content"></slot>
+        <p class="text-h6 mr-4">{{ props.title }}</p>
         <app-btn-icon :icon="icon" size="default" @click="expand = !expand"></app-btn-icon>
     </v-row>
     <v-divider></v-divider>
-    <v-card fluid width="auto" flat class="mx-6 mt-5">
-        <p class="mb-5">Hello1</p>
-        <p class="mb-5">Hello2</p>
-        <p class="mb-5">Hello3</p></v-card
-    >
+    <v-card fluid width="auto" flat class="mx-6">
+        <!-- 
+            Use visible-content slot to show items that a visible when expansion panel is closed
+        -->
+        <slot name="visible-content"></slot>
+    </v-card>
     <v-expand-transition>
-        <v-card fluid v-show="expand" width="auto" flat class="mx-6">
-            <p class="mb-5">Hello4</p>
-            <p class="mb-5">Hello5</p>
-            <p class="mb-5">Hello6</p>
-            <p class="mb-5">Hello7</p></v-card
-        >
+        <v-card fluid v-if="expand" width="auto" flat class="mx-6">
+            <!-- 
+                Use expand-content slot to show items that a visible when expansion panel is opened
+            -->
+            <slot name="expand-content"></slot>
+        </v-card>
     </v-expand-transition>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import AppBtnIcon from './AppBtnIcon.vue'
-
+/**
+ * title @string - title of the expansion panel
+ */
 const props = defineProps({
-    tooltipText: {
-        type: String,
-        default: ''
+    title: {
+        type: String
     }
 })
 
+/**
+ * Controls visibility fo the expnaded content
+ */
 const expand = ref(false)
+
+/**
+ * Returns icon based on the expand ref
+ */
 const icon = computed<string>(() => {
     return expand.value ? 'expand_less' : 'expand_more'
 })
-// /**
-//  * PLESE USE FALLTHROUGH vue attrs from button icon:
-//  * pass `icon` prop of material design, ei: icon="content_copy"
-//  * you can also pass `color`, to change icon color
-//  */
-
-// const emit = defineEmits(['click'])
-
-// /*
-// ===================================================================================
-//     Computed
-// ===================================================================================
-// */
-
-// const btnClick = (): void => {
-//     emit('click')
-// }
 </script>
 
 <style lang="scss" scoped></style>
