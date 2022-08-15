46626c6700c3040a38717e07cffc0fe4ad79fa94
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-08-15 15:34:06 +0100

    feat: 🎸 add composable for token balance

--- v2/src/modules/address/ModuleAddressTokenBalance.vue
+++ v2/src/modules/address/ModuleAddressTokenBalance.vue
@@ -1,12 +1,12 @@
 <template>
     <v-card fluid class="py-4 px-8 pa-md-6 h-100" elevation="1" rounded="xl">
         <p class="text-info text-h6 font-weight-regular">Token Balance</p>
-        <v-col v-if="store.loadingErc20Tokens || store.loadingCoinData" cols="6" sm="4" md="6" class="pa-0">
+        <v-col v-if="loadingTokens || loadingMarketInfo" cols="6" sm="4" md="6" class="pa-0">
             <v-progress-linear height="28px" rounded="xl" indeterminate color="#ECF2F7" bg-color="info" width="100px"></v-progress-linear>
         </v-col>
         <template v-else>
-            <p class="text-h4">${{ store.addressTokenBalance.value }}</p>
-            <p class="text-caption font-weight-regular text-info">{{ store.numOfErc20Tokens }} total tokens</p>
+            <p class="text-h4">${{ tokenBalance.value }}</p>
+            <p class="text-caption font-weight-regular text-info">{{ erc20Tokens.length || 0 }} total tokens</p>
             <v-divider class="my-6" />
             <p class="text-info text-h6 font-weight-regular">Total NFT's</p>
             <p class="text-h4">200</p>
@@ -15,9 +15,18 @@
 </template>
 
 <script setup lang="ts">
-import { useStore } from '@/store'
+import { useAddressToken } from '@core/composables/AddressTokenBalance/addressTokenBalance.composable'
+import { useCoinData } from '@core/composables/CoinData/coinData.composable'
 
-const store = useStore()
+const props = defineProps({
+    addressRef: {
+        type: String,
+        required: true
+    }
+})
+
+const { loadingTokens, tokenBalance, erc20Tokens } = useAddressToken(props.addressRef)
+const { loading: loadingMarketInfo } = useCoinData()
 </script>
 
 <style scoped></style>
--- v2/src/modules/address/ModuleAddressTokens.vue
+++ v2/src/modules/address/ModuleAddressTokens.vue
@@ -141,14 +141,13 @@
 import { computed, reactive } from 'vue'
 import AppPaginate from '@core/components/AppPaginate.vue'
 import AppNewUpdate from '@core/components/AppNewUpdate.vue'
-import { TokenOwnersFragment, useGetOwnersErc20TokensQuery } from '@module/address/apollo/tokens.generated'
 import { MarketDataFragment as TokenMarketData } from '@core/composables/CoinData/getLatestPrices.generated'
 import { useCoinData } from '@core/composables/CoinData/coinData.composable'
 import { TOKEN_FILTER_VALUES, TokenSort, Token } from '@module/address/models/TokenSort'
 import { formatFloatingPointValue, formatPercentageValue, FormattedNumber } from '@core/helper/number-format-helper'
 import BN from 'bignumber.js'
-
-const { getEthereumTokensMap, loading: loadingEthTokens, getEthereumTokenByContract } = useCoinData()
+import { useAddressToken } from '@core/composables/AddressTokenBalance/addressTokenBalance.composable.ts'
+const { getEthereumTokenByContract } = useCoinData()
 const MAX_ITEMS = 10
 
 const props = defineProps({
@@ -179,59 +178,10 @@ const state: ComponentState = reactive({
     index: 0
 })
 
-const {
-    result: erc20TokensResult,
-    loading: loadingTokens,
-    refetch: refetchTokens
-} = useGetOwnersErc20TokensQuery(
-    {
-        hash: props.addressHash
-    },
-    { notifyOnNetworkStatusChange: true }
-)
-
-const erc20Tokens = computed<Array<TokenOwnersFragment | null> | undefined>(() => {
-    return erc20TokensResult.value?.getOwnersERC20Tokens.owners
-})
-
-/**
- * Gets an object with all sorted arrays
- *
- * @returns false OR Map<string, TokenMarketData>  if values have been loaded
- * @returns  null  otherwise
- */
-const tokenPrices = computed<Map<string, TokenMarketData> | false | null>(() => {
-    if (!loadingTokens.value && erc20Tokens.value && !loadingEthTokens.value) {
-        const contracts: string[] = []
-        erc20Tokens.value.forEach(token => {
-            if (token) {
-                contracts.push(token.tokenInfo.contract)
-            }
-        })
-        if (contracts.length > 0) {
-            return getEthereumTokensMap(contracts)
-        }
-    }
-    return null
-})
+const { erc20Tokens, tokenPrices, loadingTokens, refetchTokens, tokenSort, tokenBalance } = useAddressToken(props.addressHash)
 
 const hasTokens = computed<boolean>(() => {
     return !!erc20Tokens.value
-})
-
-/**
- * Gets an object with all sorted arrays
- * @param {String} contract
- * @returns
- * - TokenSort if all has been loaded
- * - false if still loading
- */
-
-const tokenSort = computed<TokenSort | false>(() => {
-    if (!loadingTokens.value && erc20Tokens.value && tokenPrices.value !== null) {
-        return new TokenSort(erc20Tokens.value, tokenPrices.value, true)
-    }
-    return false
 })
 
 const sortTable = (key: string): void => {
