114e2454ce2b8fedfdbe7a24f4f2479d6cd4a5d9
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-08-17 18:40:10 -0700

    feat: address blockie, mobile ui fot address view menu

--- v2/src/core/components/AppMenu.vue
+++ v2/src/core/components/AppMenu.vue
@@ -1,12 +1,23 @@
 <template>
-    <v-menu close-delay="100" max-height="500" open-delay="60" :open-on-hover="true" transition="slide-y-transition" location="bottom">
+    <v-menu close-delay="100" max-height="500" open-delay="60" :open-on-hover="openOnHover" transition="slide-y-transition" location="bottom">
         <template #activator="{ props }">
-            <slot name="activator" v-bind="{ props }" />
+            <slot name="activator" v-bind="props" />
         </template>
-        <v-list bg-color="surface" class="py-2 rounded-b-xl rounded-t-0">
+        <v-list :bg-color="bgColor" class="py-2 rounded-xl" max-width="1000px">
             <slot v-if="$slots.default" />
         </v-list>
     </v-menu>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+    openOnHover: {
+        type: Boolean,
+        defualt: true
+    },
+    bgColor: {
+        type: String,
+        default: 'surface'
+    }
+})
+</script>
