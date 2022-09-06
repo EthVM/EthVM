9e5b24a0cf098eb95103871145eb35a798057f00
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-09-06 12:12:47 -0700

    devop: add newclient builds with api test

--- newclient/src/configs.ts
+++ newclient/src/configs.ts
@@ -1,6 +1,7 @@
+
 const configs = {
-    APOLLO_HTTP: process.env.VUE_APP_HTTP_LINK || '',
-    APOLLO_WS: process.env.VUE_APP_WS_CLIENT || '',
+    APOLLO_HTTP: 'https://baghkyu21b.execute-api.us-west-2.amazonaws.com/dev/',
+    APOLLO_WS: 'wss://baghkyu21b.execute-api.us-west-2.amazonaws.com/dev',
     OPENSEA: process.env.VUE_APP_OPENSEA_API || '',
     NODE_ENV: process.env.NODE_ENV,
     VERSION: process.env.VERSION,
