075ccd76f8a39773cc2b1dee4b6a3c872483f6df
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-09-07 14:29:31 +0100

    fix: 🐛 clean up typescript errors

--- v2/src/modules/tokens/ModuleTokensInfo.vue
+++ v2/src/modules/tokens/ModuleTokensInfo.vue
@@ -1,152 +1,154 @@
 <template>
-    <v-card variant="flat">
-        <app-table-title title="Tokens" title-caption="Top 200 tokens" :has-pagination="!smAndDown" page-type="">
-            <template #pagination>
-                <app-paginate v-if="totalPages > 1" :total="totalPages" :current-page="state.index" class="pb-2" @newPage="setPage" />
-            </template>
-        </app-table-title>
-        <v-container fluid>
-            <v-card color="info" variant="flat" min-height="40">
-                <v-row align="center" justify="start" class="ma-0">
-                    <v-col cols="4">
-                        <v-row align="center" class="ma-0">
-                            <h5 class="pl-5 pr-2">Token</h5>
-                            <app-btn-icon
-                                v-if="state.sortDirection === 'asc'"
-                                class="sort"
-                                :class="{ 'sort--active': state.sortKey === KEY_SYMBOL }"
-                                icon="arrow_upward"
-                                @click="sortTokens(KEY_SYMBOL, 'desc')"
-                            />
-                            <app-btn-icon
-                                v-if="state.sortDirection === 'desc'"
-                                class="sort"
-                                :class="{ 'sort--active': state.sortKey === KEY_SYMBOL }"
-                                icon="arrow_downward"
-                                @click="sortTokens(KEY_SYMBOL, 'asc')"
-                            />
-                        </v-row>
-                    </v-col>
-                    <v-col cols="2">
-                        <v-row align="center" class="ma-0">
-                            <h5 class="pl-5 pr-2">Price</h5>
-                            <app-btn-icon
-                                v-if="state.sortDirection === 'asc'"
-                                class="sort"
-                                :class="{ 'sort--active': state.sortKey === KEY_PRICE }"
-                                icon="arrow_upward"
-                                @click="sortTokens(KEY_PRICE, 'desc')"
-                            />
-                            <app-btn-icon
-                                v-if="state.sortDirection === 'desc'"
-                                class="sort"
-                                :class="{ 'sort--active': state.sortKey === KEY_PRICE }"
-                                icon="arrow_downward"
-                                @click="sortTokens(KEY_PRICE, 'asc')"
-                            />
-                        </v-row>
-                    </v-col>
-                    <v-col cols="2">
-                        <v-row align="center" class="ma-0">
-                            <h5 class="pl-5 pr-2">%Change (24H)</h5>
-                        </v-row>
-                    </v-col>
-                    <v-col cols="2">
-                        <v-row align="center" class="ma-0">
-                            <h5 class="pl-5 pr-2">Volume</h5>
-                            <app-btn-icon
-                                v-if="state.sortDirection === 'asc'"
-                                class="sort"
-                                :class="{ 'sort--active': state.sortKey === KEY_VOLUME }"
-                                icon="arrow_upward"
-                                @click="sortTokens(KEY_VOLUME, 'desc')"
-                            />
-                            <app-btn-icon
-                                v-if="state.sortDirection === 'desc'"
-                                class="sort"
-                                :class="{ 'sort--active': state.sortKey === KEY_VOLUME }"
-                                icon="arrow_downward"
-                                @click="sortTokens(KEY_VOLUME, 'asc')"
-                            />
-                        </v-row>
-                    </v-col>
-                    <v-col cols="2">
-                        <v-row align="center" class="ma-0">
-                            <h5 class="pl-5 pr-2">Market Cap</h5>
-                            <app-btn-icon
-                                v-if="state.sortDirection === 'asc'"
-                                class="sort"
-                                :class="{ 'sort--active': state.sortKey === KEY_MARKET_CAP }"
-                                icon="arrow_upward"
-                                @click="sortTokens(KEY_MARKET_CAP, 'desc')"
-                            />
-                            <app-btn-icon
-                                v-if="state.sortDirection === 'desc'"
-                                class="sort"
-                                :class="{ 'sort--active': state.sortKey === KEY_MARKET_CAP }"
-                                icon="arrow_downward"
-                                @click="sortTokens(KEY_MARKET_CAP, 'asc')"
-                            />
-                        </v-row>
-                    </v-col>
-                </v-row>
-            </v-card>
-            <div v-if="tokensInPage.length > 0" class="my-2 text-body-1">
-                <div v-for="token in tokensInPage" :key="token.symbol">
+    <div style="width: 1390px; max-width: 100%">
+        <v-card variant="elevated" elevation="1" rounded="xl" class="pa-4 pa-sm-6">
+            <app-table-title title="Tokens" title-caption="Top 200 tokens" :has-pagination="!smAndDown" page-type="">
+                <template #pagination>
+                    <app-paginate v-if="totalPages > 1" :total="totalPages" :current-page="state.index" class="pb-2" @newPage="setPage" />
+                </template>
+            </app-table-title>
+            <v-container fluid>
+                <v-card color="info" variant="flat" min-height="40">
                     <v-row align="center" justify="start" class="ma-0">
                         <v-col cols="4">
                             <v-row align="center" class="ma-0">
-                                <div class="mr-2">
-                                    <v-img :src="getTokenImage(token)" width="25px" height="25px" />
-                                </div>
-                                <router-link v-if="token.symbol || token.name" :to="`/token/${token.contract}`" class="text-body-1">
-                                    <span v-if="token.symbol" class="font-weight-medium text-uppercase">{{ token.symbol }} - </span>
-                                    <span class="font-weight-regular">{{ token.name }}</span>
-                                </router-link>
-                                <div v-else>
-                                    <p>Contract:</p>
-                                    <app-transform-hash :hash="eth.toCheckSum(token.contract)" :link="`/token/${token.contract}`" />
-                                </div>
+                                <h5 class="pl-5 pr-2">Token</h5>
+                                <app-btn-icon
+                                    v-if="state.sortDirection === 'asc'"
+                                    class="sort"
+                                    :class="{ 'sort--active': state.sortKey === KEY_SYMBOL }"
+                                    icon="arrow_upward"
+                                    @click="sortTokens(KEY_SYMBOL, 'desc')"
+                                />
+                                <app-btn-icon
+                                    v-if="state.sortDirection === 'desc'"
+                                    class="sort"
+                                    :class="{ 'sort--active': state.sortKey === KEY_SYMBOL }"
+                                    icon="arrow_downward"
+                                    @click="sortTokens(KEY_SYMBOL, 'asc')"
+                                />
                             </v-row>
                         </v-col>
                         <v-col cols="2">
-                            <v-row align="center" class="ma-0 pl-5">
-                                <p class="mb-0">
-                                    {{ getTokenPrice(token).value }}
-                                    <app-tooltip v-if="getTokenPrice(token).tooltipText" :text="getTokenPrice(token).tooltipText" />
-                                </p>
+                            <v-row align="center" class="ma-0">
+                                <h5 class="pl-5 pr-2">Price</h5>
+                                <app-btn-icon
+                                    v-if="state.sortDirection === 'asc'"
+                                    class="sort"
+                                    :class="{ 'sort--active': state.sortKey === KEY_PRICE }"
+                                    icon="arrow_upward"
+                                    @click="sortTokens(KEY_PRICE, 'desc')"
+                                />
+                                <app-btn-icon
+                                    v-if="state.sortDirection === 'desc'"
+                                    class="sort"
+                                    :class="{ 'sort--active': state.sortKey === KEY_PRICE }"
+                                    icon="arrow_downward"
+                                    @click="sortTokens(KEY_PRICE, 'asc')"
+                                />
                             </v-row>
                         </v-col>
                         <v-col cols="2">
-                            <v-row align="center" class="ma-0 pl-5">
-                                <p :class="priceChangeClass(token)">{{ percentageChange(token).value }}%</p>
-                                <v-icon v-if="!isPositivePriceChange(token)" color="error" size="x-small">south_east</v-icon>
-                                <v-icon v-else color="success" size="x-small">north_east</v-icon>
-                                <app-tooltip v-if="percentageChange(token).tooltip" :text="percentageChange(token).tooltip" />
+                            <v-row align="center" class="ma-0">
+                                <h5 class="pl-5 pr-2">%Change (24H)</h5>
                             </v-row>
                         </v-col>
                         <v-col cols="2">
-                            <v-row align="center" class="ma-0 pl-5">
-                                <p class="mb-0">
-                                    {{ getTokenVolume(token).value }}
-                                    <app-tooltip v-if="getTokenVolume(token).tooltipText" :text="getTokenVolume(token).tooltipText" />
-                                </p>
+                            <v-row align="center" class="ma-0">
+                                <h5 class="pl-5 pr-2">Volume</h5>
+                                <app-btn-icon
+                                    v-if="state.sortDirection === 'asc'"
+                                    class="sort"
+                                    :class="{ 'sort--active': state.sortKey === KEY_VOLUME }"
+                                    icon="arrow_upward"
+                                    @click="sortTokens(KEY_VOLUME, 'desc')"
+                                />
+                                <app-btn-icon
+                                    v-if="state.sortDirection === 'desc'"
+                                    class="sort"
+                                    :class="{ 'sort--active': state.sortKey === KEY_VOLUME }"
+                                    icon="arrow_downward"
+                                    @click="sortTokens(KEY_VOLUME, 'asc')"
+                                />
                             </v-row>
                         </v-col>
                         <v-col cols="2">
-                            <v-row align="center" class="ma-0 pl-5">
-                                <p class="mb-0">
-                                    {{ getTokenMarketCap(token).value }}
-                                    <app-tooltip v-if="getTokenMarketCap(token).tooltipText" :text="getTokenMarketCap(token).tooltipText" />
-                                </p>
+                            <v-row align="center" class="ma-0">
+                                <h5 class="pl-5 pr-2">Market Cap</h5>
+                                <app-btn-icon
+                                    v-if="state.sortDirection === 'asc'"
+                                    class="sort"
+                                    :class="{ 'sort--active': state.sortKey === KEY_MARKET_CAP }"
+                                    icon="arrow_upward"
+                                    @click="sortTokens(KEY_MARKET_CAP, 'desc')"
+                                />
+                                <app-btn-icon
+                                    v-if="state.sortDirection === 'desc'"
+                                    class="sort"
+                                    :class="{ 'sort--active': state.sortKey === KEY_MARKET_CAP }"
+                                    icon="arrow_downward"
+                                    @click="sortTokens(KEY_MARKET_CAP, 'asc')"
+                                />
                             </v-row>
                         </v-col>
                     </v-row>
-                    <v-divider />
+                </v-card>
+                <div v-if="tokensInPage.length > 0" class="my-2 text-body-1">
+                    <div v-for="token in tokensInPage" :key="token.symbol">
+                        <v-row align="center" justify="start" class="ma-0">
+                            <v-col cols="4">
+                                <v-row align="center" class="ma-0">
+                                    <div class="mr-2">
+                                        <v-img :src="getTokenImage(token)" width="25px" height="25px" />
+                                    </div>
+                                    <router-link v-if="token.symbol || token.name" :to="`/token/${token.contract}`" class="text-body-1">
+                                        <span v-if="token.symbol" class="font-weight-medium text-uppercase">{{ token.symbol }} - </span>
+                                        <span class="font-weight-regular">{{ token.name }}</span>
+                                    </router-link>
+                                    <div v-else>
+                                        <p>Contract:</p>
+                                        <app-transform-hash :hash="eth.toCheckSum(token.contract)" :link="`/token/${token.contract}`" />
+                                    </div>
+                                </v-row>
+                            </v-col>
+                            <v-col cols="2">
+                                <v-row align="center" class="ma-0 pl-5">
+                                    <p class="mb-0">
+                                        {{ getTokenPrice(token).value }}
+                                        <app-tooltip v-if="getTokenPrice(token).tooltipText" :text="getTokenPrice(token).tooltipText" />
+                                    </p>
+                                </v-row>
+                            </v-col>
+                            <v-col cols="2">
+                                <v-row align="center" class="ma-0 pl-5">
+                                    <p :class="priceChangeClass(token)">{{ percentageChange(token).value }}%</p>
+                                    <v-icon v-if="!isPositivePriceChange(token)" color="error" size="x-small">south_east</v-icon>
+                                    <v-icon v-else color="success" size="x-small">north_east</v-icon>
+                                    <app-tooltip v-if="percentageChange(token).tooltip" :text="percentageChange(token).tooltip" />
+                                </v-row>
+                            </v-col>
+                            <v-col cols="2">
+                                <v-row align="center" class="ma-0 pl-5">
+                                    <p class="mb-0">
+                                        {{ getTokenVolume(token).value }}
+                                        <app-tooltip v-if="getTokenVolume(token).tooltipText" :text="getTokenVolume(token).tooltipText" />
+                                    </p>
+                                </v-row>
+                            </v-col>
+                            <v-col cols="2">
+                                <v-row align="center" class="ma-0 pl-5">
+                                    <p class="mb-0">
+                                        {{ getTokenMarketCap(token).value }}
+                                        <app-tooltip v-if="getTokenMarketCap(token).tooltipText" :text="getTokenMarketCap(token).tooltipText" />
+                                    </p>
+                                </v-row>
+                            </v-col>
+                        </v-row>
+                        <v-divider />
+                    </div>
                 </div>
-            </div>
-        </v-container>
-    </v-card>
+            </v-container>
+        </v-card>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -181,7 +183,7 @@ interface ComponentState {
     tokensByVolume: TokensSorted
     tokensBySymbol: TokensSorted
     tokensByPrice: TokensSorted
-    tokens: TokensSorted
+    tokens: TokenMarketData[]
 }
 
 const state: ComponentState = reactive({
@@ -225,7 +227,7 @@ class TokensSorted {
     }
 }
 
-const getCurrentCoinData = (): Array<TokenMarketData | null> => {
+const getCurrentCoinData = (): TokensSorted => {
     switch (state.sortKey) {
         case KEY_PRICE:
             return state.tokensByPrice
@@ -314,7 +316,9 @@ const sortTokens = (key: string, direction: string): void => {
 }
 
 const setTokensData = (): void => {
-    state.tokensByMarketCap = new TokensSorted(ethereumTokens.value, KEY_MARKET_CAP)
+    if (ethereumTokens.value) {
+        state.tokensByMarketCap = new TokensSorted(ethereumTokens.value, KEY_MARKET_CAP)
+    }
     state.tokens = state.tokensByMarketCap.descend
     state.tokensByPrice = new TokensSorted(state.tokensByMarketCap.ascend, KEY_PRICE)
     state.tokensBySymbol = new TokensSorted(state.tokensByMarketCap.ascend, KEY_SYMBOL)
