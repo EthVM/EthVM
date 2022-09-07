cd6c5b68cb5e21f83dbee2a69d6ad78ca456926d
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-09-07 12:47:21 -0700

    devop: update url

--- newclient/apollo.config.js
+++ newclient/apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
     client: {
         service: {
             name: 'api',
-            url: 'https://baghkyu21b.execute-api.us-west-2.amazonaws.com/dev/',
+            url: 'https://api-v2.ethvm.dev',
             includes: ['**/*.graphql'],
             excludes: ['node_modules/**/*']
         }
