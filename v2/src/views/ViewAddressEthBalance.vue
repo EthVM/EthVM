7552a40f297d369b028b833320a8243c6aabf61d
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-10-11 17:22:24 +0100

    feat: 🎸 transfer history initial setup

--- v2/src/views/ViewAddressEthBalance.vue
+++ v2/src/views/ViewAddressEthBalance.vue
@@ -1,20 +1,38 @@
 <template>
     <v-row :class="rowMargin">
         <v-col cols="12" :class="columnPadding">
-            <module-address-balance :address-ref="props.addressRef" />
+            <v-card class="pa-4 pa-sm-6" elevation="1" rounded="xl" height="100%">
+                <module-address-balance :address-ref="props.addressRef" />
+                <module-address-transfer-history :address-ref="props.addressRef" :tab="props.tab" @tabChange="setLastViewedTab" />
+            </v-card>
         </v-col>
     </v-row>
 </template>
 
 <script setup lang="ts">
 import ModuleAddressBalance from '@module/address/ModuleAddressBalance.vue'
+import ModuleAddressTransferHistory from '@module/address/ModuleAddressTransferHistory.vue'
 import { useAppViewGrid } from '@core/composables/AppViewGrid/AppViewGrid.composable'
 
 const props = defineProps({
     addressRef: {
         type: String,
         required: true
+    },
+    tab: {
+        type: String,
+        required: true
     }
 })
 const { columnPadding, rowMargin } = useAppViewGrid()
+
+const emit = defineEmits<{
+    (e: 'tabChange', newTab: string): void
+}>()
+/**
+ * Emits to parent to remember last visted tab
+ */
+const setLastViewedTab = (tab: string): void => {
+    emit('tabChange', tab)
+}
 </script>
