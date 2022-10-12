60554ab9c379162994dbd76233cdf7aee3ba04fd
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-10-12 18:20:54 +0100

    feat: 🎸 add table row

---
+++ v2/src/modules/address/components/EthBalanceTabs/TableAllAndInternalRow.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-row class="my-5" align="center">
+        <v-col sm="2">
+            <span style="width: 30px" class="d-inline-block">
+                <v-icon color="success">check_circle</v-icon>
+            </span>
+            <span class="ml-6">0.0259500 ETH</span>
+        </v-col>
+        <v-col :sm="props.tab === routes[0] ? 2 : 1"> 10 seconds ago </v-col>
+        <template v-if="props.tab === routes[1]">
+            <v-col cols="1" class="text-center">
+                <app-chip bg="warning" text="to" />
+            </v-col>
+            <v-col sm="2">
+                <div class="d-flex align-center">
+                    <app-address-blockie :address="'0x09d9591442b213fe4ac9d0634023b37cc818e51c' || ''" :size="8" class="mr-2 mr-sm-2" />
+                    <app-transform-hash
+                        is-short
+                        is-blue
+                        :hash="eth.toCheckSum('0x09d9591442b213fe4ac9d0634023b37cc818e51c')"
+                        :link="`/address/${'0x09d9591442b213fe4ac9d0634023b37cc818e51c'}`"
+                    />
+                </div>
+            </v-col>
+            <v-col sm="2">
+                <app-transform-hash
+                    is-short
+                    is-blue
+                    :hash="eth.toCheckSum('0x09d9591442b213fe4ac9d0634023b37cc818e51c')"
+                    :link="`/address/${'0x09d9591442b213fe4ac9d0634023b37cc818e51c'}`"
+                />
+            </v-col>
+        </template>
+        <v-col sm="2"> 0.0259500 ETH </v-col>
+        <v-col sm="2"> 0.0259500 ETH </v-col>
+        <template v-if="props.tab === routes[0]">
+            <v-col sm="2">
+                <app-chip bg="purple" text="transaction" />
+            </v-col>
+            <v-col sm="2">
+                <div class="d-flex align-center">
+                    <span class="text-info mr-2">Hash:</span>
+                    <app-transform-hash
+                        is-short
+                        is-blue
+                        :hash="eth.toCheckSum('0x09d9591442b213fe4ac9d0634023b37cc818e51c')"
+                        :link="`/address/${'0x09d9591442b213fe4ac9d0634023b37cc818e51c'}`"
+                    />
+                </div>
+            </v-col>
+        </template>
+    </v-row>
+</template>
+
+<script setup lang="ts">
+import AppBtnIcon from '@core/components/AppBtnIcon.vue'
+import AppAddressBlockie from '@/core/components/AppAddressBlockie.vue'
+import AppTransformHash from '@/core/components/AppTransformHash.vue'
+import AppChip from '@core/components/AppChip.vue'
+import { Q_ADDRESS_TRANSFERS } from '@core/router/routesNames'
+import { eth } from '@core/helper'
+
+const routes = Q_ADDRESS_TRANSFERS
+
+const props = defineProps({
+    tab: {
+        type: String,
+        required: true
+    }
+})
+</script>
---
+++ v2/src/modules/address/components/EthBalanceTabs/TableMinedAndPendingRow.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-row class="my-5">
+        <v-col sm="2">
+            <span class="mr-6">
+                <v-icon v-if="props.tab === routes[2]" color="success">check_circle</v-icon>
+                <v-icon v-else>update</v-icon>
+            </span>
+            <span>0.0259500 ETH</span>
+        </v-col>
+        <v-col sm="2"> 10 seconds ago </v-col>
+        <v-col sm="2">
+            <app-chip bg="success" text="from" />
+        </v-col>
+        <v-col sm="2">
+            <div class="d-flex align-center">
+                <app-address-blockie :address="'0x09d9591442b213fe4ac9d0634023b37cc818e51c' || ''" :size="8" class="mr-2 mr-sm-2" />
+                <app-transform-hash
+                    is-short
+                    is-blue
+                    :hash="eth.toCheckSum('0x09d9591442b213fe4ac9d0634023b37cc818e51c')"
+                    :link="`/address/${'0x09d9591442b213fe4ac9d0634023b37cc818e51c'}`"
+                />
+            </div>
+        </v-col>
+        <v-col sm="2">
+            <app-transform-hash
+                is-short
+                is-blue
+                :hash="eth.toCheckSum('0x09d9591442b213fe4ac9d0634023b37cc818e51c')"
+                :link="`/address/${'0x09d9591442b213fe4ac9d0634023b37cc818e51c'}`"
+            />
+        </v-col>
+        <template v-if="props.tab === routes[2]">
+            <v-col sm="2">
+                <div class="d-flex justify-space-between">
+                    <span>0.0259500 ETH</span>
+                    <span>
+                        <app-btn-icon icon="expand_more" />
+                    </span>
+                </div>
+            </v-col>
+        </template>
+        <template v-else>
+            <v-col sm="2">0.0259500 ETH</v-col>
+        </template>
+    </v-row>
+</template>
+
+<script setup lang="ts">
+import AppBtnIcon from '@core/components/AppBtnIcon.vue'
+import AppAddressBlockie from '@/core/components/AppAddressBlockie.vue'
+import AppTransformHash from '@/core/components/AppTransformHash.vue'
+import AppChip from '@core/components/AppChip.vue'
+import { Q_ADDRESS_TRANSFERS } from '@core/router/routesNames'
+import { eth } from '@core/helper'
+
+const routes = Q_ADDRESS_TRANSFERS
+
+const props = defineProps({
+    tab: {
+        type: String,
+        required: true
+    }
+})
+</script>
