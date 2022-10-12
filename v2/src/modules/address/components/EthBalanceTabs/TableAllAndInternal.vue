d1a424800c761a9ca59ed593194c075c3cc9c923
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-10-12 18:20:54 +0100

    feat: 🎸 add table row

---
+++ v2/src/modules/address/components/EthBalanceTabs/TableAllAndInternal.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-row align="center" justify="start" class="text-info mt-2 mt-sm-6">
+        <v-col sm="2">
+            <span style="width: 30px; height: 1px" class="d-inline-block"></span>
+            <span class="ml-6">Tx Value</span>
+        </v-col>
+        <v-col :sm="props.tab === routes[0] ? 2 : 1"> Timestamp </v-col>
+        <v-spacer />
+        <template v-if="props.tab === routes[1]">
+            <v-col sm="2"> Address </v-col>
+            <v-col sm="2"> Hash </v-col>
+        </template>
+        <v-col sm="2"> Balance Before </v-col>
+        <v-col sm="2"> Balance After </v-col>
+        <template v-if="props.tab === routes[0]">
+            <v-col sm="4">Type</v-col>
+        </template>
+    </v-row>
+    <v-divider class="my-0 mt-md-4 mx-n4 mx-sm-n6" />
+    <div v-for="i in 5" :key="i">
+        <table-all-and-internal-row :tab="props.tab" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import TableAllAndInternalRow from '@module/address/components/EthBalanceTabs/TableAllAndInternalRow.vue'
+import { Q_ADDRESS_TRANSFERS } from '@/core/router/routesNames'
+
+const routes = Q_ADDRESS_TRANSFERS
+
+const props = defineProps({
+    tab: {
+        type: String,
+        required: true
+    }
+})
+</script>
--- v2/src/modules/address/components/EthBalanceTabs/TableMinedAndPending.vue
+++ v2/src/modules/address/components/EthBalanceTabs/TableMinedAndPending.vue
@@ -1,25 +1,33 @@
 <template>
-    <v-row align="center" justify="start" class="text-body-1 text-info mt-2 mt-sm-5">
+    <v-row align="center" justify="start" class="text-info mt-2 mt-sm-6">
         <v-col sm="2">
             <span class="mr-6">Status</span>
             <span>Tx Value</span>
         </v-col>
         <v-col sm="2"> Timestamp </v-col>
-        <v-col :sm="props.tab === routes[0] ? 1 : 2"> To/From </v-col>
+        <v-col sm="2"> To/From </v-col>
         <v-col sm="2"> Address </v-col>
         <v-col sm="2"> Hash </v-col>
-        <template v-if="props.tab === routes[0]">
-            <v-col sm="2"> Balance Change </v-col>
-            <v-col sm="1"> Details </v-col>
+        <template v-if="props.tab === routes[2]">
+            <v-col sm="2">
+                <div class="d-flex justify-space-between">
+                    <span>Balance Change</span>
+                    <span>Details</span>
+                </div>
+            </v-col>
         </template>
         <template v-else>
             <v-col sm="2">Estimated Fee</v-col>
         </template>
     </v-row>
     <v-divider class="my-0 mt-md-4 mx-n4 mx-sm-n6" />
+    <div v-for="i in 5" :key="i">
+        <table-mined-and-pending-row :tab="props.tab" />
+    </div>
 </template>
 
 <script setup lang="ts">
+import TableMinedAndPendingRow from '@module/address/components/EthBalanceTabs/TableMinedAndPendingRow.vue'
 import { Q_ADDRESS_TRANSFERS } from '@/core/router/routesNames'
 
 const routes = Q_ADDRESS_TRANSFERS
