383ec95afd50825acecfa9b53979fa0351cb4e63
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-10-14 21:21:27 +0100

    feat: 🎸 add infinite scroll

--- v2/src/modules/address/ModuleAddressTransferHistory.vue
+++ v2/src/modules/address/ModuleAddressTransferHistory.vue
@@ -2,7 +2,7 @@
     <div class="pt-4 pt-sm-6">
         <app-tabs v-model="state.tab" :routes="routes" :tabs="tabs" @update:modelValue="setLastViewedTab()" class="mb-4 mb-sm-0"></app-tabs>
         <table-mined-and-pending v-if="state.tab === routes[2] || state.tab === routes[3]" :tab="state.tab" />
-        <table-all-and-internal v-if="state.tab === routes[0] || state.tab === routes[1]" :tab="state.tab" />
+        <table-all-and-internal v-if="state.tab === routes[0] || state.tab === routes[1]" :tab="state.tab" :address-ref="props.addressRef" />
     </div>
 </template>
 
