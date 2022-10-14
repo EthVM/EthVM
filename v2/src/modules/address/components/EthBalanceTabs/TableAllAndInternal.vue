7d832d7c0d1dd935b15379552646ee1a15398a68
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-10-14 21:21:27 +0100

    feat: 🎸 add infinite scroll

--- v2/src/modules/address/components/EthBalanceTabs/TableAllAndInternal.vue
+++ v2/src/modules/address/components/EthBalanceTabs/TableAllAndInternal.vue
@@ -17,21 +17,108 @@
         </template>
     </v-row>
     <v-divider class="my-0 mt-md-4 mx-n4 mx-sm-n6" />
-    <div v-for="i in 5" :key="i">
-        <table-all-and-internal-row :tab="props.tab" />
-    </div>
+    <template v-if="!initialLoad">
+        <template v-if="transfers && transfers.length > 0">
+            <div v-for="transfer in transfers" :key="transfer.transfer.transactionHash">
+                <table-all-and-internal-row :tab="props.tab" :loading="initialLoad" :transfer="transfer" :address-ref="props.addressRef" />
+            </div>
+        </template>
+        <app-no-result v-else text="This address does not have any internal transfers" class="mt-4 mt-sm-6 mb-5"></app-no-result>
+        <app-intersect v-if="hasMore" @intersect="loadMoreData">
+            <div class="skeleton-box rounded-xl mt-1 my-4" style="height: 24px"></div>
+            <v-divider />
+        </app-intersect>
+    </template>
+    <template v-else>
+        <div v-for="item in 10" :key="item" class="my-2">
+            <div class="skeleton-box rounded-xl my-5" style="height: 24px"></div>
+        </div>
+    </template>
 </template>
 
 <script setup lang="ts">
+import AppIntersect from '@/core/components/AppIntersect.vue'
+import AppNoResult from '@/core/components/AppNoResult.vue'
 import TableAllAndInternalRow from '@module/address/components/EthBalanceTabs/TableAllAndInternalRow.vue'
+import { computed } from 'vue'
 import { Q_ADDRESS_TRANSFERS } from '@/core/router/routesNames'
+import { EthItxTransfersFragment, useGetEthItxTransfersQuery } from '@module/address/apollo/EthTransfers/internalTransfers.generated'
 
 const routes = Q_ADDRESS_TRANSFERS
+const MAX_ITEMS = 10
 
 const props = defineProps({
     tab: {
         type: String,
         required: true
+    },
+    addressRef: {
+        type: String,
+        required: true
     }
 })
+
+const isItxEnabled = computed<boolean>(() => {
+    return props.tab === routes[1]
+})
+
+const {
+    result: internalTransfersData,
+    loading: loadingInternalTransfersData,
+    fetchMore: fetchMoreInternalTransfersData
+} = useGetEthItxTransfersQuery(
+    () => ({
+        hash: props.addressRef
+    }),
+    () => ({
+        fetchPolicy: 'network-only',
+        enabled: isItxEnabled
+    })
+)
+
+const transfers = computed<EthItxTransfersFragment | undefined | null>(() => {
+    if (props.tab === routes[1]) {
+        return internalTransfersData.value?.getEthItxTransfers.transfers
+    }
+    return null
+})
+
+const initialLoad = computed<boolean>(() => {
+    if (props.tab === routes[1]) {
+        return !internalTransfersData.value
+    }
+    return false
+})
+
+const hasMore = computed<boolean>(() => {
+    if (props.tab === routes[1]) {
+        return !!internalTransfersData.value?.getEthItxTransfers.nextKey
+    }
+    return false
+})
+
+const loadMoreItxData = (): void => {
+    fetchMoreInternalTransfersData({
+        variables: {
+            hash: props.addressRef,
+            _limit: MAX_ITEMS,
+            _nextKey: internalTransfersData.value?.getEthItxTransfers.nextKey
+        },
+        updateQuery: (prev, { fetchMoreResult }) => {
+            return {
+                getEthItxTransfers: {
+                    nextKey: fetchMoreResult?.getEthItxTransfers.nextKey,
+                    transfers: [...prev.getEthItxTransfers.transfers, ...(fetchMoreResult?.getEthItxTransfers.transfers || [])],
+                    __typename: fetchMoreResult?.getEthItxTransfers.__typename
+                }
+            }
+        }
+    })
+}
+
+const loadMoreData = (e: boolean): void => {
+    if (hasMore.value && e) {
+        loadMoreItxData()
+    }
+}
 </script>
--- v2/src/modules/address/components/EthBalanceTabs/TableAllAndInternalRow.vue
+++ v2/src/modules/address/components/EthBalanceTabs/TableAllAndInternalRow.vue
@@ -2,37 +2,32 @@
     <v-row class="my-5" align="center">
         <v-col sm="2">
             <span style="width: 30px" class="d-inline-block">
-                <v-icon color="success">check_circle</v-icon>
+                <v-icon :color="statusIcon.color">{{ statusIcon.icon }}</v-icon>
             </span>
-            <span class="ml-6">0.0259500 ETH</span>
+            <span class="ml-6">{{ txFee.value }} ETH</span>
         </v-col>
-        <v-col :sm="props.tab === routes[0] ? 2 : 1"> 10 seconds ago </v-col>
+        <v-col :sm="props.tab === routes[0] ? 2 : 1"> {{ timestamp }} </v-col>
         <template v-if="props.tab === routes[1]">
             <v-col cols="1" class="text-center">
-                <app-chip bg="warning" text="to" />
+                <app-chip :bg="transferDirection.color" :text="transferDirection.text" />
             </v-col>
             <v-col sm="2">
                 <div class="d-flex align-center">
-                    <app-address-blockie :address="'0x09d9591442b213fe4ac9d0634023b37cc818e51c' || ''" :size="8" class="mr-2 mr-sm-2" />
-                    <app-transform-hash
-                        is-short
-                        is-blue
-                        :hash="eth.toCheckSum('0x09d9591442b213fe4ac9d0634023b37cc818e51c')"
-                        :link="`/address/${'0x09d9591442b213fe4ac9d0634023b37cc818e51c'}`"
-                    />
+                    <app-address-blockie :address="txAddress || ''" :size="8" class="mr-2 mr-sm-2" />
+                    <app-transform-hash is-short is-blue :hash="eth.toCheckSum(txAddress)" :link="`/address/${txAddress}`" />
                 </div>
             </v-col>
             <v-col sm="2">
                 <app-transform-hash
                     is-short
                     is-blue
-                    :hash="eth.toCheckSum('0x09d9591442b213fe4ac9d0634023b37cc818e51c')"
-                    :link="`/address/${'0x09d9591442b213fe4ac9d0634023b37cc818e51c'}`"
+                    :hash="eth.toCheckSum(transfer.transfer.transactionHash)"
+                    :link="`/address/${transfer.transfer.transactionHash}`"
                 />
             </v-col>
         </template>
-        <v-col sm="2"> 0.0259500 ETH </v-col>
-        <v-col sm="2"> 0.0259500 ETH </v-col>
+        <v-col sm="2"> {{ balanceBefore.value }} ETH </v-col>
+        <v-col sm="2"> {{ balanceAfter.value }} ETH </v-col>
         <template v-if="props.tab === routes[0]">
             <v-col sm="2">
                 <app-chip bg="purple" text="transaction" />
@@ -58,14 +53,86 @@ import AppAddressBlockie from '@/core/components/AppAddressBlockie.vue'
 import AppTransformHash from '@/core/components/AppTransformHash.vue'
 import AppChip from '@core/components/AppChip.vue'
 import { Q_ADDRESS_TRANSFERS } from '@core/router/routesNames'
-import { eth } from '@core/helper'
+import { eth, timeAgo } from '@core/helper'
+import { EthItxTransfersFragment } from '@module/address/apollo/EthTransfers/internalTransfers.generated'
+import { computed } from 'vue'
+import { formatNonVariableEthValue, FormattedNumber } from '@core/helper/number-format-helper'
+import BN from 'bignumber.js'
 
 const routes = Q_ADDRESS_TRANSFERS
 
-const props = defineProps({
-    tab: {
-        type: String,
-        required: true
+interface ComponentProps {
+    transfer: EthItxTransfersFragment
+    tab: string
+    addressRef: string
+}
+
+const props = defineProps<ComponentProps>()
+
+const statusIcon = computed<{ [key: string]: string }>(() => {
+    if (props.transfer.transfer.status) {
+        return {
+            icon: 'check_circle',
+            color: 'success'
+        }
     }
+    return {
+        icon: 'highlight_off',
+        color: 'danger'
+    }
+})
+
+const transferDirection = computed<{ [key: string]: string }>(() => {
+    if (props.transfer.transfer.to === props.addressRef) {
+        return {
+            text: 'From',
+            color: 'success'
+        }
+    } else if (props.transfer.transfer.from === props.addressRef) {
+        return {
+            text: 'To',
+            color: 'orange'
+        }
+    }
+    return {
+        text: 'Self',
+        color: 'orange'
+    }
+})
+
+const balanceBefore = computed<FormattedNumber>(() => {
+    if (props.transfer.transfer.to === props.addressRef) {
+        // Use the stateDiff of the destination address
+        return formatNonVariableEthValue(new BN(props.transfer.stateDiff.to.before))
+    }
+    // use from address
+    return formatNonVariableEthValue(new BN(props.transfer.stateDiff.from.before))
+})
+
+const balanceAfter = computed<FormattedNumber>(() => {
+    if (props.transfer.transfer.to === props.addressRef) {
+        // use to address
+        return formatNonVariableEthValue(new BN(props.transfer.stateDiff.to.after))
+    }
+    // use from address
+    return formatNonVariableEthValue(new BN(props.transfer.stateDiff.from.after))
+})
+
+const txAddress = computed<string>(() => {
+    if (props.transfer.transfer.to === props.addressRef) {
+        // Use the stateDiff of the destination address
+        return props.transfer.transfer.from
+    }
+    // use from address
+    return props.transfer.transfer.to
+})
+
+const timestamp = computed<string>(() => {
+    const date = new Date(props.transfer.transfer.timestamp * 1e3)
+    return timeAgo(date, false)
+})
+
+const txFee = computed<FormattedNumber>(() => {
+    return formatNonVariableEthValue(new BN(props.transfer.transfer.txFee))
 })
 </script>
