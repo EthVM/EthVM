68ed74f1fbbb94d14531e25b77d2c78c0af4f330
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-11-07 16:15:29 -0800

    feat(portfolio): list row ui

--- v2/src/modules/address/ModulePortfolioList.vue
+++ v2/src/modules/address/ModulePortfolioList.vue
@@ -5,129 +5,72 @@
         {{ store.portfolioLength }}
         <!--Table Header-->
         <v-row :dense="xs" :class="'mt-sm-4 d-flex text-body-1 text-info mb-sm-3'" :justify="xs ? 'end' : 'start'">
-            <v-col sm="5" md="4" class="py-0 d-none d-sm-block">
+            <v-col sm="6" lg="3" class="py-0 d-none d-sm-block">
                 <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.HASH)">
                     Address <v-icon v-if="isActiveSort(KEY.HASH)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
                 >
             </v-col>
-            <v-col sm="7" md="8" class="d-none d-sm-flex">
-                <v-row>
-                    <!--
+            <!--
                         NAME:
                         hidden on xs-md
                     -->
-                    <v-col lg="3" class="py-0 d-none d-lg-block">
-                        <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.NAME)">
-                            Name <v-icon v-if="isActiveSort(KEY.NAME)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
-                        >
-                    </v-col>
-                    <!--
+            <v-col lg="3" class="py-0 d-none d-lg-block">
+                <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.NAME)">
+                    Name <v-icon v-if="isActiveSort(KEY.NAME)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
+                >
+            </v-col>
+            <!--
                         ETH Balance:
                         hidden on xs
                     -->
-                    <v-col sm="6" md="4" lg="3" class="py-0 d-none d-sm-block">
-                        <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.ETH)">
-                            ETH Balance <v-icon v-if="isActiveSort(KEY.ETH)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
-                        >
-                    </v-col>
-                    <!--
+            <v-col sm="3" lg="2" class="py-0 d-none d-sm-block">
+                <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.ETH)">
+                    ETH Balance <v-icon v-if="isActiveSort(KEY.ETH)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
+                >
+            </v-col>
+            <!--
                         ETH Value:
                         hidden on xs-sm
                     -->
-                    <v-col md="4" lg="3" class="py-0 d-none d-md-block">
-                        <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.ETH_USD)">
-                            ETH Value <v-icon v-if="isActiveSort(KEY.ETH_USD)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
-                        >
-                    </v-col>
-                    <!--
+            <v-col lg="2" class="py-0 d-none d-lg-block">
+                <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.ETH_USD)">
+                    ETH Value <v-icon v-if="isActiveSort(KEY.ETH_USD)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
+                >
+            </v-col>
+            <!--
                         total:
                         hidden on xs
                     -->
-                    <v-col sm="6" md="4" lg="3" class="py-0 d-none d-sm-block">
-                        <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.TOTAL)">
-                            Total <v-icon v-if="isActiveSort(KEY.TOTAL)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
-                        >
-                    </v-col>
-                </v-row>
+            <v-col sm="3" lg="2" class="py-0 d-none d-sm-block">
+                <v-btn variant="text" color="info" class="font-weight-regular ml-n3" rounded="pill" size="small" @click="sortTable(KEY.TOTAL)">
+                    Total <v-icon v-if="isActiveSort(KEY.TOTAL)" class="ml-1" size="x-small">{{ sortIcon }}</v-icon></v-btn
+                >
             </v-col>
         </v-row>
 
         <v-divider class="mx-n4 mx-sm-n6 mb-5" />
         <template v-if="addressList.length > 0">
-            <div v-for="adr in sortList" :key="adr.hash">
-                <v-row :dense="xs" class="d-flex align-start align-lg-center text-body-1 mt-0 mb-5" :justify="xs ? 'end' : 'start'">
-                    <v-col xs="6" sm="5" md="4" class="py-0 d-flex align-center">
-                        <module-add-adress-to-porfolio :address="adr.hash" />
-                        <app-address-blockie :address="adr.hash" class="ml-5 mr-4" />
-                        <div v-if="mdAndDown">
-                            <p>{{ adr.name }}</p>
-                            <app-transform-hash :hash="eth.toCheckSum(adr.hash)" :link="`address/${adr.hash}`" is-blue is-short />
-                        </div>
-                        <app-transform-hash v-else :hash="eth.toCheckSum(adr.hash)" :link="`address/${adr.hash}`" is-blue class="mr-16" />
-                    </v-col>
-                    <v-col xs="6" sm="7" md="8">
-                        <v-row>
-                            <!--
-                        NAME:
-                        hidden on xs-md
-                    -->
-                            <v-col lg="3" class="py-0 d-none d-lg-flex align-center">
-                                <p>{{ adr.name }}</p>
-                                <module-add-adress-to-porfolio :address="adr.hash" :name="adr.name" is-edit-mode />
-                            </v-col>
-                            <!--
-                        ETH Balance:
-                        hidden on xs
-                    -->
-                            <v-col sm="6" md="4" lg="3" class="py-0 d-none d-sm-block d-lg-flex align-lg-center">
-                                <div v-if="adr.eth">
-                                    <p>{{ adr.eth }}</p>
-                                    <p v-if="smAndDown" class="text-info">{{ adr.ethUSD }}</p>
-                                </div>
-                                <div v-else class="skeleton-box rounded-xl" style="height: 20px; width: 60%"></div>
-                            </v-col>
-                            <!--
-                        ETH Value:
-                        hidden on xs-sm
-                    -->
-                            <v-col md="4" lg="3" class="py-0 d-none d-md-block d-lg-flex align-lg-center">
-                                <p v-if="adr.ethUSD">{{ adr.ethUSD }}</p>
-                                <div v-else class="skeleton-box rounded-xl" style="height: 20px; width: 60%"></div>
-                            </v-col>
-                            <!--
-                        total:
-                        hidden on xs
-                    -->
-                            <v-col xs="12" sm="6" md="4" lg="3" class="py-0 d-lg-flex align-lg-center">
-                                <p v-if="adr.total" class="text-right text-sm-left">{{ adr.total }}</p>
-                                <div v-else class="skeleton-box rounded-xl" style="height: 20px; width: 60%"></div>
-                            </v-col>
-                        </v-row>
-                    </v-col>
-                </v-row>
+            <div v-for="adr in sortList" flat :key="adr.hash">
+                <table-row-portfolio-item :adr="adr"></table-row-portfolio-item>
             </div>
         </template>
-
         <template v-else>
             <app-no-result :text="`Your portfolio is empty`"></app-no-result>
         </template>
-        <!--  -->
     </div>
 </template>
 
 <script setup lang="ts">
 import ModuleAddAdressToPorfolio from './ModuleAddAdressToPorfolio.vue'
-import AppAddressBlockie from '@core/components/AppAddressBlockie.vue'
-import AppTransformHash from '@/core/components/AppTransformHash.vue'
 import AppNoResult from '@core/components/AppNoResult.vue'
+import TableRowPortfolioItem from './components/TableRowPortfolioItem.vue'
 import { computed, reactive } from 'vue'
 import { useStore } from '@/store'
-import { eth } from '@core/helper/eth'
 import { useDisplay } from 'vuetify/lib/framework.mjs'
 import { formatUsdValue } from '@core/helper/number-format-helper'
 import BN from 'bignumber.js'
 
-const { xs, smAndDown, mdAndDown } = useDisplay()
+const { xs } = useDisplay()
 const store = useStore()
 
 enum KEY {
@@ -159,7 +102,7 @@ interface DisplayItem {
     total?: string
     weiBN?: BN
     ethUSDBN?: BN
-    totalBN?: BN
+    // totalBN?: BN
 }
 
 const addressList = computed<DisplayItem[]>(() => {
@@ -173,7 +116,7 @@ const addressList = computed<DisplayItem[]>(() => {
             eth: isLoaded ? store.portfolioEthBalanceMap[cleanHash].balanceFormatted : undefined,
             ethUSD: isLoaded ? store.portfolioEthBalanceMap[cleanHash].balanceFiatFormatted : undefined,
             total: total ? formatUsdValue(total).value : undefined,
-            weiBN: isLoaded ? store.portfolioEthBalanceMap[cleanHash].weiBalance : undefined,
+            weiBN: isLoaded ? new BN(store.portfolioEthBalanceMap[cleanHash].weiBalance) : undefined,
             ethUSDBN: isLoaded ? store.portfolioEthBalanceMap[cleanHash].balanceFiatBN : undefined,
             totalBN: total
         }
---
+++ v2/src/modules/address/components/TableRowPortfolioItem.vue
@@ -0,0 +1,103 @@
+<template>
+    <v-card :color="state.showMore && xs ? 'pillGrey' : 'transparent'" rounded="0" flat class="mx-n5 px-5 py-2" :link="xs" @click="openMoreInfo">
+        <v-row :dense="xs" class="d-flex align-center jusify-start text-body-1 mt-0 mb-1" :justify="xs ? 'end' : 'start'">
+            <v-col cols="12" sm="6" lg="3" :class="['py-0 d-flex align-center justify-start', { 'mb-5': state.showMore }]">
+                <app-address-blockie :address="props.adr.hash" class="mr-4" />
+                <div>
+                    <p v-if="mdAndDown" :style="!xs ? 'width: 120px;' : ''">{{ props.adr.name }}</p>
+                    <app-transform-hash v-if="!xs" :hash="eth.toCheckSum(props.adr.hash)" :link="`address/${props.adr.hash}`" is-blue is-short class="" />
+                    <div v-else>
+                        <p v-if="props.adr.total" class="">{{ props.adr.total }}</p>
+                        <div v-else class="skeleton-box rounded-xl" style="height: 20px; width: 60%"></div>
+                    </div>
+                </div>
+                <v-spacer v-if="xs" />
+                <app-copy-to-clip :value-to-copy="eth.toCheckSum(props.adr.hash)" class="ml-2" />
+                <app-btn-icon icon="more_vert" />
+            </v-col>
+            <!--
+                NAME:
+                hidden on xs-md
+            -->
+            <v-col lg="3" class="py-0 d-none d-lg-flex align-center">
+                <p>{{ props.adr.name }}</p>
+            </v-col>
+            <!--
+                ETH Balance:
+                hidden on xs & !state.showMore
+            -->
+            <v-col v-if="(xs && state.showMore) || smAndUp" cols="6" sm="3" lg="2" class="py-0 d-block d-lg-flex align-lg-center">
+                <div v-if="props.adr.eth">
+                    <p v-if="xs" class="text-info mb-2">ETH Balance</p>
+                    <p>{{ props.adr.eth }}</p>
+                    <p v-if="!xs && mdAndDown" class="text-info">{{ props.adr.ethUSD }}</p>
+                </div>
+                <div v-else class="skeleton-box rounded-xl" style="height: 20px; width: 60%"></div>
+            </v-col>
+            <!--
+                ETH Value:
+                hidden on (xs & !state.showMore) and SM, MD
+             -->
+            <v-col v-if="(xs && state.showMore) || lgAndUp" cols="6" lg="2" class="py-0 d-block d-lg-flex align-lg-center">
+                <p v-if="xs" class="text-info mb-2">ETH Value</p>
+                <p v-if="props.adr.ethUSD">{{ props.adr.ethUSD }}</p>
+                <div v-else class="skeleton-box rounded-xl" style="height: 20px; width: 60%"></div>
+            </v-col>
+            <!--
+                Total:
+                hidden on xs
+            -->
+            <v-col sm="3" lg="2" class="py-0 d-none d-sm-flex align-lg-center">
+                <p v-if="props.adr.total" class="mb-5 mb-lg-0">{{ props.adr.total }}</p>
+                <div v-else class="skeleton-box rounded-xl" style="height: 20px; width: 60%"></div>
+            </v-col>
+            <!--
+                Hash Value:
+                hidden on (xs & ! state.showMore) and smAndUP
+            -->
+            <v-col v-if="xs && state.showMore" cols="12" class="pt-5 d-block">
+                <p class="text-info mb-2">Hash</p>
+                <app-transform-hash :hash="eth.toCheckSum(props.adr.hash)" :link="`address/${props.adr.hash}`" is-blue is-short />
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import AppAddressBlockie from '@core/components/AppAddressBlockie.vue'
+import AppTransformHash from '@/core/components/AppTransformHash.vue'
+import AppCopyToClip from '@/core/components/AppCopyToClip.vue'
+import AppBtnIcon from '@core/components/AppBtnIcon.vue'
+import { reactive } from 'vue'
+import { useDisplay } from 'vuetify/lib/framework.mjs'
+import BN from 'bignumber.js'
+import { eth } from '@core/helper/eth'
+
+const { xs, mdAndDown, smAndUp, lgAndUp } = useDisplay()
+
+interface DisplayItem {
+    name: string
+    hash: string
+    eth?: string
+    ethUSD?: string
+    total?: string
+    weiBN?: BN
+    ethUSDBN?: BN
+    totalBN?: BN
+}
+
+interface PropsType {
+    adr: DisplayItem
+}
+const props = defineProps<PropsType>()
+
+const state = reactive({
+    showMore: false
+})
+
+const openMoreInfo = () => {
+    if (xs.value) {
+        state.showMore = !state.showMore
+    }
+}
+</script>
