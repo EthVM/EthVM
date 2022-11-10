4c96860617f78c9507a05bded268d11bf102f735
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-11-09 17:49:18 -0800

    feat(portfolio): add eth and tokens total balances

--- v2/src/views/ViewPortfolio.vue
+++ v2/src/views/ViewPortfolio.vue
@@ -3,9 +3,10 @@
         <v-col cols="12" md="6" lg="4" :class="columnPadding">
             <module-portfolio-total />
         </v-col>
-        <!-- <v-col cols="12" md="6" lg="4" :class="columnPadding">
-            <module-address-balance :address-ref="props.addressRef" is-overview />
+        <v-col cols="12" md="6" lg="4" :class="columnPadding">
+            <module-portfolio-balance />
         </v-col>
+        <!-- 
         <v-col cols="12" md="6" lg="4" :class="[columnPadding, 'd-none d-sm-block']">
             <module-address-token-balance :address-ref="props.addressRef" />
         </v-col> -->
@@ -13,33 +14,14 @@
             <v-card elevation="1" rounded="xl" class="pt-4 pt-sm-6">
                 <app-tabs v-model="state.tab" :routes="routes" :tabs="tabs" class="mb-4 mb-sm-0"></app-tabs>
                 <module-portfolio-list v-if="state.tab === routes[0]" />
-                <module-portfolio-token-balance v-if="state.tab === routes[1]" />
-                <!--
-                    <module-address-tokens
-                    v-if="state.tab === routes[0]"
-                    class="mb-4"
-                    :address-hash="props.addressRef"
-                    :new-erc20-transfer="newErc20Transfer"
-                    :scroll-id="scrollId"
-                    @resetCount="resetCount"
-                    :key="routes[0]"
-                />
-                <module-address-token-transfers
-                    v-if="state.tab === routes[1]"
-                    :address-hash="props.addressRef"
-                    :new-erc20-transfer="newErc20Transfer"
-                    @resetCount="resetCount"
-                    :key="routes[1]"
-                />
-                -->
-            </v-card></v-col
-        >
+                <module-portfolio-token-balance v-if="state.tab === routes[1]" /> </v-card
+        ></v-col>
     </v-row>
 </template>
 
 <script setup lang="ts">
 // import ModuleAddressBalance from '@module/address/ModuleAddressBalance.vue'
-// import ModuleAddressTokenBalance from '@module/address/ModuleAddressTokenBalance.vue'
+import ModulePortfolioBalance from '@/modules/address/ModulePortfolioBalances.vue'
 import ModulePortfolioTotal from '@/modules/address/ModulePortfolioTotal.vue'
 import ModulePortfolioTokenBalance from '@/modules/address/ModulePortfolioTokenBalance.vue'
 import ModulePortfolioList from '@module/address/ModulePortfolioList.vue'
