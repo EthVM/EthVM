31e9b608b8a8f5b1f99757c6d594ddc4b446f719
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-11-25 16:28:32 +0100

    feat: 🎸 add balance breakdown

--- v2/src/modules/address/components/EthBalanceTabs/TableTxsHistoryRow.vue
+++ v2/src/modules/address/components/EthBalanceTabs/TableTxsHistoryRow.vue
@@ -17,28 +17,6 @@
                     <app-transform-hash is-short is-blue :hash="eth.toCheckSum(txAddress)" :link="`/address/${txAddress}`" />
                 </div>
             </v-col>
-            <v-col cols="12" v-if="showMoreDetails">
-                <v-divider class="mx-n6 mb-3" />
-                <div class="mb-4 d-flex justify-space-between">
-                    <span class="text-info mr-2">Balance Before:</span>
-                    <span>{{ balanceBefore.value }} ETH</span>
-                </div>
-                <div class="mb-4 d-flex justify-space-between">
-                    <span class="text-info mr-2">Balance After:</span>
-                    <span>{{ balanceAfter.value }} ETH</span>
-                </div>
-                <div class="d-flex justify-space-between">
-                    <span class="text-info mr-2">Hash:</span>
-                    <span>
-                        <app-transform-hash
-                            is-short
-                            is-blue
-                            :hash="eth.toCheckSum(transfer.transfer.transactionHash)"
-                            :link="`/tx/${transfer.transfer.transactionHash}`"
-                        />
-                    </span>
-                </div>
-            </v-col>
         </v-row>
         <v-row v-else class="my-5" align="center">
             <v-col :sm="mdAndDown ? 3 : 2">
@@ -71,12 +49,51 @@
                     :link="`/tx/${transfer.transfer.transactionHash}`"
                 />
             </v-col>
-            <v-col sm="2"> {{ balanceBefore.value }} ETH </v-col>
+            <v-col sm="2"> {{ totalBalanceChange }} ETH </v-col>
             <v-col sm="1">
-                <app-btn-icon icon="expand_more" tooltip-text="Show transfer details" />
+                <app-btn-icon icon="expand_more" tooltip-text="Show transfer details" @click="showMoreDetails = !showMoreDetails" />
             </v-col>
         </v-row>
-        <div v-if="showMoreDetails && smAndDown" class="row-bg bg-tableGrey"></div>
+        <v-row v-if="showMoreDetails" justify="space-between" class="my-4 flex-column-reverse flex-sm-row">
+            <v-col cols="2" class="d-none d-md-block"></v-col>
+            <v-col cols="12" sm="5" md="4">
+                <p class="font-weight-bold">Events</p>
+                <p class="my-5 text-info">
+                    {{ txEvents }}
+                </p>
+            </v-col>
+            <v-spacer />
+            <v-col cols="12" sm="7" md="4">
+                <p class="font-weight-bold">Balance Change Breakdown</p>
+                <v-row class="my-5" align="center">
+                    <v-col cols="6" class="text-info">Transaction Value</v-col>
+                    <span class="text-error">{{ txValue.value }} ETH</span>
+                </v-row>
+                <v-row v-if="!isIncoming" class="my-5" align="center">
+                    <v-col cols="6" class="text-info">TX Fee Paid</v-col>
+                    <span class="text-error">{{ txFee.value }} ETH</span>
+                </v-row>
+                <v-row v-if="internalTransferValue" class="my-5" align="center">
+                    <v-col cols="6" class="text-info">Internal Transfer Value</v-col>
+                    <span class="text-success">{{ internalTransferValue.value }} ETH</span>
+                </v-row>
+                <v-divider class="mx-n5 mx-sm-0" />
+                <v-row class="my-5" align="center">
+                    <v-col cols="6" class="text-info">Total Balance Change</v-col>
+                    <span class="text-error">{{ totalBalanceChange }} ETH</span>
+                </v-row>
+                <v-row class="my-5" align="center">
+                    <v-col cols="6" class="text-info">Balance Before</v-col>
+                    <span>{{ balanceBeforeFormatted.value }} ETH</span>
+                </v-row>
+                <v-row class="my-5" align="center">
+                    <v-col cols="6" class="text-info">Balance After</v-col>
+                    <span>{{ balanceAfterFormatted.value }} ETH</span>
+                </v-row>
+            </v-col>
+            <v-col cols="1" class="d-none d-md-block"></v-col>
+        </v-row>
+        <div v-if="showMoreDetails" class="row-bg bg-tableGrey"></div>
     </div>
 </template>
 
@@ -86,12 +103,13 @@ import AppAddressBlockie from '@/core/components/AppAddressBlockie.vue'
 import AppTransformHash from '@/core/components/AppTransformHash.vue'
 import AppChip from '@core/components/AppChip.vue'
 import { Q_ADDRESS_TRANSFERS } from '@core/router/routesNames'
-import { eth, timeAgo } from '@core/helper'
-import { TxsTransfersFragment } from '@module/address/apollo/EthTransfers/transfersHistory.generated'
+import { timeAgo, eth } from '@core/helper'
+import { TxsTransfersFragment, useGetEthTransfersByHashQuery } from '@module/address/apollo/EthTransfers/transfersHistory.generated'
 import { computed, ref } from 'vue'
-import { formatNonVariableEthValue, FormattedNumber, formatNumber } from '@core/helper/number-format-helper'
+import { formatNonVariableEthValue, FormattedNumber } from '@core/helper/number-format-helper'
 import BN from 'bignumber.js'
 import { useDisplay } from 'vuetify'
+import { TransferSubtype } from '@/apollo/types'
 
 const routes = Q_ADDRESS_TRANSFERS
 const BLOCK_REWARD_HASH = '0xBLOCK_REWARD'
@@ -115,7 +133,7 @@ const statusIcon = computed<{ [key: string]: string }>(() => {
     }
     return {
         icon: 'highlight_off',
-        color: 'danger'
+        color: 'error'
     }
 })
 
@@ -179,22 +197,45 @@ const transferType = computed<{ [key: string]: string }>(() => {
     }
 })
 
-const balanceBefore = computed<FormattedNumber>(() => {
-    if (props.transfer.transfer.to === props.addressRef) {
+const balanceBefore = computed<BN>(() => {
+    if (props.transfer.transfer.to === props.addressRef.toLowerCase()) {
         // Use the stateDiff of the destination address
-        return formatNonVariableEthValue(new BN(props.transfer?.stateDiff?.to.before || 0))
+        return new BN(props.transfer?.transactionStateDiff?.to.before || 0)
     }
     // use from address
-    return formatNonVariableEthValue(new BN(props.transfer?.stateDiff?.from.before || 0))
+    return new BN(props.transfer?.transactionStateDiff?.from.before || 0)
 })
 
-const balanceAfter = computed<FormattedNumber>(() => {
-    if (props.transfer.transfer.to === props.addressRef) {
-        // use to address
-        return formatNonVariableEthValue(new BN(props.transfer.stateDiff?.to.after || 0))
+const balanceBeforeFormatted = computed<FormattedNumber>(() => {
+    if (props.transfer.transfer.to === props.addressRef.toLowerCase()) {
+        // Use the stateDiff of the destination address
+        return formatNonVariableEthValue(balanceBefore.value)
     }
     // use from address
-    return formatNonVariableEthValue(new BN(props.transfer.stateDiff?.from.after || 0))
+    return formatNonVariableEthValue(balanceBefore.value)
+})
+
+const balanceAfter = computed<BN>(() => {
+    if (props.transfer.transfer.to === props.addressRef.toLowerCase()) {
+        // use to address
+        return new BN(props.transfer?.transactionStateDiff?.to.after || 0)
+    }
+    // use from address
+    return new BN(props.transfer?.transactionStateDiff?.from.after || 0)
+})
+
+const balanceAfterFormatted = computed<FormattedNumber>(() => {
+    if (props.transfer.transfer.to === props.addressRef.toLowerCase()) {
+        // use to address
+        return formatNonVariableEthValue(balanceAfter.value)
+    }
+    // use from address
+    return formatNonVariableEthValue(balanceAfter.value)
+})
+
+const totalBalanceChange = computed<string | number>(() => {
+    const balanceChange = formatNonVariableEthValue(balanceAfter.value.minus(balanceBefore.value))
+    return balanceChange.value
 })
 
 const txAddress = computed<string>(() => {
@@ -217,6 +258,43 @@ const txValue = computed<FormattedNumber>(() => {
 
 const txFee = computed<FormattedNumber>(() => {
     return formatNonVariableEthValue(new BN(props.transfer.transfer.txFee))
+})
+
+const internalTransferValue = computed<FormattedNumber | false>(() => {
+    const txValue = new BN(props.transfer.value)
+    const txFee = new BN(props.transfer.transfer.txFee)
+    const totalTxsAmount = txValue.plus(txFee)
+    const balanceChange = balanceAfter.value.minus(balanceBefore.value)
+    if (totalTxsAmount.isLessThan(balanceChange)) {
+        const internalTransfer = balanceChange.minus(totalTxsAmount)
+        return formatNonVariableEthValue(internalTransfer)
+    }
+    return false
+})
+
+// Load ethTransfers by hash to see if an internal transaction exists
+const { result: ethTransfers, loading: loadingEthTransfers } = useGetEthTransfersByHashQuery(
+    () => ({
+        owner: props.addressRef,
+        _limit: 1,
+        hash: props.transfer.transfer.transactionHash
+    }),
+    () => ({
+        enabled: showMoreDetails
+    })
+)
+
+const hasInternalTransaction = computed<boolean>(() => {
+    return ethTransfers.value?.getEthTransfersByHash.transfers[0]?.transfer.subtype === TransferSubtype.InternalTransaction
+})
+
+const txEvents = computed<string>(() => {
+    const events = []
+    events.push('ETH Transfers')
+    if (hasInternalTransaction.value) {
+        events.push('Internal Transaction')
+    }
+    return events.join(', ')
 })
 </script>
 
