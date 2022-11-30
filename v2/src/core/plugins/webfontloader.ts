28ef9dc9c18861a1c12ec5f8dfd2790e1b18c973
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-11-30 14:16:12 -0800

    devop: add mono space fonts

--- v2/src/core/plugins/webfontloader.ts
+++ v2/src/core/plugins/webfontloader.ts
@@ -9,7 +9,7 @@ export async function loadFonts() {
 
     webFontLoader.load({
         google: {
-            families: ['Roboto:100,300,400,500,700,900&display=swap']
+            families: ['Roboto:100,300,400,500,700,900&display=swap', 'Roboto+Mono:100,300,400,500,700,900&display=swap']
         }
     })
 }
