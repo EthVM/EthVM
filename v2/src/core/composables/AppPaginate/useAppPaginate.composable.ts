823c8784a78e4d87b0cdcca5a804c5fdb3bb4f3c
Abdulqudus <abdulqudusabubakre@gmail.com>
2023-01-19 16:45:19 +0100

    feat: 🎸 split all eth transfers into different module

--- v2/src/core/composables/AppPaginate/useAppPaginate.composable.ts
+++ v2/src/core/composables/AppPaginate/useAppPaginate.composable.ts
@@ -1,12 +1,29 @@
 import { ITEMS_PER_PAGE } from '@core/constants'
-import { computed, Ref, ref } from 'vue'
+import { computed, Ref, ref, toRefs, unref, watch } from 'vue'
+import { useStore } from '@/store'
 
-export function useAppPaginate(dataToPaginate: Ref<Array<unknown>>) {
+export function useAppPaginate(dataToPaginate: Ref<Array<unknown>>, id?: Ref<string>) {
+    const store = useStore()
     const pageNum = ref(1)
 
+    if (id) {
+        if (!store.paginationStateMap.has(id.value)) {
+            store.paginationStateMap.set(id.value, 1)
+        }
+        watch(id, () => {
+            if (id && !store.paginationStateMap.has(id.value)) {
+                store.paginationStateMap.set(id.value, 1)
+            }
+        })
+    }
+
+    const computedPageNum = computed<number>(() => {
+        return id ? store.paginationStateMap.get(id.value) || 1 : pageNum.value
+    })
+
     const pageData = computed(() => {
-        const start = (pageNum.value - 1) * ITEMS_PER_PAGE
-        const end = pageNum.value * ITEMS_PER_PAGE
+        const start = (computedPageNum.value - 1) * ITEMS_PER_PAGE
+        const end = computedPageNum.value * ITEMS_PER_PAGE
         return dataToPaginate.value.slice(start, end)
     })
 
@@ -16,12 +33,15 @@ export function useAppPaginate(dataToPaginate: Ref<Array<unknown>>) {
 
     const setPageNum = (page: number) => {
         pageNum.value = page
+        if (id) {
+            store.paginationStateMap.set(id.value, page)
+        }
     }
 
     return {
         numberOfPages,
         pageData,
-        pageNum,
+        pageNum: computedPageNum,
         setPageNum
     }
 }
