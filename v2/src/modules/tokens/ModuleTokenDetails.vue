00a8ac74fd85f4248dca037d861761ca0159f9fc
olgakup <olga.kupchevskaya.812@my.csun.edu>
2023-01-31 16:32:22 -0800

    feat: new erc20 contract layout

--- v2/src/modules/tokens/ModuleTokenDetails.vue
+++ v2/src/modules/tokens/ModuleTokenDetails.vue
@@ -1,5 +1,10 @@
 <template>
     <v-row :class="rowMargin">
+        <!--
+        =====================================================================================
+        Contract OverView: Hash, Standard Type, Decimals/Collections
+        =====================================================================================
+        -->
         <v-col cols="12" :class="columnPadding">
             <v-card elevation="1" rounded="xl" class="pa-4 pa-sm-6">
                 <p class="mb-4 text-h4 font-weight-bold">Contract</p>
@@ -26,16 +31,30 @@
                 </v-row>
             </v-card>
         </v-col>
+        <!--
+        =====================================================================================
+        Contract Details
+        =====================================================================================
+        -->
         <v-col cols="12" :class="columnPadding">
-            <!-- <token-details-list
-                :holder-details="null"
-                :address-ref="props.addressRef"
-                :token-details="tokenDetails"
-                :is-nft="state.isNft"
-                :is-loading="loadingTokenDetails || state.hasError"
-                @errorDetails="emitErrorState"
-            /> -->
+            <v-card elevation="1" rounded="xl" class="pa-4 pa-sm-6 h-100">
+                <div v-if="loadingTokenDetails || state.hasError || loadingCoinData" class="skeleton-box rounded-xl my-2" style="height: 280px"></div>
+                <div v-else>
+                    <token-details-erc20
+                        v-if="tokenDetails"
+                        :address-ref="props.addressRef"
+                        :token-details="tokenDetails"
+                        :is-loading="loadingTokenDetails || state.hasError"
+                        @errorDetails="emitErrorState"
+                    />
+                </div>
+            </v-card>
         </v-col>
+        <!--
+        =====================================================================================
+        Contract Holders and Transfers
+        =====================================================================================
+        -->
         <v-col cols="12" :class="columnPadding">
             <v-card elevation="1" rounded="xl" class="pt-4 pt-sm-6">
                 <app-tabs v-model="state.tab" :routes="routes" :tabs="tabs"></app-tabs>
@@ -59,61 +78,18 @@
             </v-card>
         </v-col>
     </v-row>
-
-    <!-- <div> -->
-    <!--
-    =====================================================================================
-      BASIC VIEW
-
-      Will show details if URL does NOT include query param "holder"
-      Shows details pertinent to the token as a whole, with no holder-specific information
-    =====================================================================================
-    -->
-    <!-- <div v-if="!props.isHolder">
-
-            <token-details-list
-                :holder-details="null"
-                :address-ref="props.addressRef"
-                :token-details="tokenDetails"
-                :is-nft="state.isNft"
-                :is-loading="loadingTokenDetails || state.hasError"
-                @errorDetails="emitErrorState"
-            />
-           
-        </div> -->
-    <!--
-    =====================================================================================
-      HOLDER VIEW
-
-      Will show details if the URL DOES include query param "holder"
-      Shows holder details pertaining to particular token contract
-    =====================================================================================
-    -->
-    <!-- <div v-if="props.isHolder">
-            <token-details-list
-                :address-ref="props.addressRef"
-                :holder-details="tokenDetails"
-                :token-details="tokenDetails ? tokenDetails['tokenInfo'] : {}"
-                :is-loading="loadingTokenDetails || state.hasError"
-                @errorDetails="emitErrorState"
-            />
-        </div>
-    </div> -->
 </template>
 
 <script setup lang="ts">
 import { reactive, computed, onMounted, watch } from 'vue'
-import TokenDetailsList from '@module/tokens/components/TokenDetailsList.vue'
+import TokenDetailsErc20 from '@module/tokens/components/TokenDetails/TokenDetailsERC20.vue'
 import TokenTransfers from '@module/tokens/components/TokenTransfers.vue'
 import TokenHolders from '@module/tokens/components/TokenHolders.vue'
 import AppTabs from '@/core/components/AppTabs.vue'
 import { Tab } from '@core/components/props'
 import {
-    Erc20TokenOwnerDetailsFragment as TokenOwnerInfo,
     TokenDetailsFragment as TokenInfo,
-    useGetErc20TokenBalanceQuery,
     useGetTokenInfoByContractQuery,
-    GetErc20TokenBalanceQuery,
     GetTokenInfoByContractQuery
 } from '@module/tokens/apollo/TokenDetails/tokenDetails.generated'
 import { eth } from '@core/helper'
@@ -123,7 +99,11 @@ import { useAppViewGrid } from '@core/composables/AppViewGrid/AppViewGrid.compos
 import AppAddressBlockie from '@core/components/AppAddressBlockie.vue'
 import AppTransformHash from '@/core/components/AppTransformHash.vue'
 import { TransferType } from '@/apollo/types'
+import { useCoinData } from '@core/composables/CoinData/coinData.composable'
+
 const { columnPadding, rowMargin } = useAppViewGrid()
+const { loading: loadingCoinData } = useCoinData()
+
 const routes = Q_TOKEN_DETAILS
 const tabs: Tab[] = [
     {
@@ -138,14 +118,6 @@ const tabs: Tab[] = [
 
 const props = defineProps({
     addressRef: {
-        type: String,
-        required: true
-    },
-    isHolder: {
-        type: Boolean,
-        required: true
-    },
-    holderAddress: {
         type: String,
         required: true
     },
@@ -190,14 +162,9 @@ const {
     loading: loadingTokenDetails,
     onError: onTokenDetailsError,
     result
-} = props.isHolder
-    ? useGetErc20TokenBalanceQuery(() => ({
-          contract: props.addressRef,
-          owner: props.holderAddress
-      }))
-    : useGetTokenInfoByContractQuery(() => ({
-          contract: props.addressRef
-      }))
+} = useGetTokenInfoByContractQuery(() => ({
+    contract: props.addressRef
+}))
 
 onTokenDetailsError(error => {
     const newError = JSON.stringify(error.message)
@@ -208,15 +175,9 @@ onTokenDetailsError(error => {
     }
 })
 
-const tokenDetails = computed<TokenInfo | TokenOwnerInfo | null>(() => {
+const tokenDetails = computed<TokenInfo | null>(() => {
     if (result && result.value) {
-        if (props.isHolder) {
-            const res = result.value as GetErc20TokenBalanceQuery
-            return res.getERC20TokenBalance as TokenOwnerInfo
-        }
         const res = result.value as GetTokenInfoByContractQuery
-        console.log(res.getTokenInfoByContract)
-
         return res.getTokenInfoByContract
     }
     return null
@@ -232,28 +193,18 @@ const isValid = computed<boolean>(() => {
     return eth.isValidAddress(props.addressRef)
 })
 
-const symbol = computed<string | null>(() => {
+const symbol = computed<string | undefined>(() => {
     if (tokenDetails.value) {
-        if (tokenDetails.value.__typename === 'ERC20TokenBalance' && tokenDetails.value.tokenInfo.symbol) {
-            return tokenDetails.value.tokenInfo.symbol
-        }
-        if (tokenDetails.value.__typename === 'EthTokenInfo' && tokenDetails.value.symbol) {
-            return tokenDetails.value.symbol
-        }
+        return tokenDetails.value.symbol || undefined
     }
-    return null
+    return undefined
 })
 
-const decimals = computed<number | null>(() => {
+const decimals = computed<number | undefined>(() => {
     if (tokenDetails.value) {
-        if (tokenDetails.value.__typename === 'ERC20TokenBalance' && tokenDetails.value.tokenInfo.decimals) {
-            return tokenDetails.value.tokenInfo.decimals
-        }
-        if (tokenDetails.value.__typename === 'EthTokenInfo' && tokenDetails.value.decimals) {
-            return tokenDetails.value.decimals
-        }
+        return tokenDetails.value.decimals || undefined
     }
-    return null
+    return undefined
 })
 
 const leftTitle = computed<string>(() => {
