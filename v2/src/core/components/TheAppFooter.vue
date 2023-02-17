8d8d65f38d0578c7d1260812e3521f2bd05f2daa
olgakup <olga.kupchevskaya.812@my.csun.edu>
2023-02-16 16:05:56 -0800

    devop: refactor footer

--- v2/src/core/components/TheAppFooter.vue
+++ v2/src/core/components/TheAppFooter.vue
@@ -1,111 +1,112 @@
 <template>
     <v-footer flat color="primary" class="pa-0 flex-column">
-        <v-container class="footer px-0 mx-2 mx-sm-6">
-            <div class="px-4 px-sm-6">
-                <v-row justify="space-between" justify-lg="center" class="flex-column flex-sm-row mx-0">
-                    <v-col cols="12" class="d-block d-lg-none mb-7">
-                        <router-link to="/">
-                            <v-img :src="require('@/assets/logo.svg')" height="25" width="80" contain />
-                        </router-link>
-                    </v-col>
-                    <v-col cols="12" sm="7" md="6" lg="9">
-                        <v-row class="flex-column flex-lg-row">
-                            <v-col cols="12" lg="3" class="d-none d-lg-block">
-                                <router-link to="/">
-                                    <v-img :src="require('@/assets/logo.svg')" height="25" width="80" contain />
-                                </router-link>
+        <v-container class="mx-2 mx-sm-6 mx-md-auto mx-lg-6 mx-xl-auto px-4 px-sm-6">
+            <v-row justify="space-between" class="flex-wrap">
+                <v-col cols="12" lg="3" class="mb-10 mb-lg-0" order="1">
+                    <router-link to="/">
+                        <v-img :src="require('@/assets/logo.svg')" height="25" width="80" contain />
+                    </router-link>
+                    <v-img
+                        v-if="lgAndUp"
+                        :src="require('@/assets/hero/hero-blocks.png')"
+                        width="225"
+                        height="225"
+                        max-width="250"
+                        max-height="250"
+                        aspect-ratio="1"
+                        contain
+                        class="footer-hero mt-4"
+                    />
+                </v-col>
+                <v-col cols="6" sm="6" lg="2" class="mb-11 mb-lg-0" order="2">
+                    <p class="footer-item-title text-h5 font-weight-bold mb-2">Discover</p>
+                    <ul class="footer-item-lists pa-0 surf">
+                        <li class="mb-2">
+                            <a href="#" class="text-link">About us</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-link">Privacy Policy</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-link">Knowledge Base</a>
+                        </li>
+                    </ul>
+                </v-col>
+                <v-col cols="12" sm="6" lg="3" class="mb-11 mb-lg-0" order="4" order-sm="4" order-lg="3">
+                    <p class="footer-item-title text-h5 font-weight-bold mb-2">Love ethVM?</p>
+                    <p class="footer-item-text">Help us keep ethVM free and open-source, your donations go a long way towards making that possible.</p>
+                    <div class="mt-5">
+                        <div class="mb-2">
+                            <a href="#" class="d-inline-flex align-center primary--text">
                                 <v-img
-                                    :src="require('@/assets/hero/hero-blocks.png')"
-                                    width="225"
-                                    height="225"
-                                    aspect-ratio="1"
-                                    contain
-                                    class="footer-hero mt-4"
+                                    :src="require('@/assets/icon-ethereum.svg')"
+                                    alt="Ethereum symbol in circle with purple background"
+                                    width="32"
+                                    height="32"
+                                    class="mr-3"
                                 />
-                            </v-col>
-                            <v-spacer />
-                            <v-col cols="12" lg="3" class="mb-11 mb-lg-0">
-                                <p class="footer-item-title text-h5 font-weight-bold mb-2">Discover</p>
-                                <ul class="footer-item-lists pa-0 surf">
-                                    <li class="mb-2">
-                                        <a href="#" class="text-link">About us</a>
-                                    </li>
-                                    <li class="mb-2">
-                                        <a href="#" class="text-link">Privacy Policy</a>
-                                    </li>
-                                    <li class="mb-2">
-                                        <a href="#" class="text-link">Knowledge Base</a>
-                                    </li>
-                                    <!--                                    <li class="mb-2">-->
-                                    <!--                                        <a href="#" class="text-link">Tools</a>-->
-                                    <!--                                    </li>-->
-                                </ul>
-                            </v-col>
-                            <v-col cols="12" lg="5" class="mb-11 mb-lg-0">
-                                <p class="footer-item-title text-h5 font-weight-bold mb-2">Love ethVM?</p>
-                                <p class="footer-item-text">
-                                    Help us keep ethVM free and open-source, your donations go a long way towards making that possible.
-                                </p>
-                                <div class="mt-5">
-                                    <div class="mb-2">
-                                        <a href="#" class="d-inline-flex align-center primary--text">
-                                            <v-img
-                                                :src="require('@/assets/icon-ethereum.svg')"
-                                                alt="Ethereum symbol in circle with purple background"
-                                                width="32"
-                                                height="32"
-                                                class="mr-3"
-                                            />
-                                            <p>Ethereum Donation</p>
-                                        </a>
-                                    </div>
-                                    <div>
-                                        <a href="#" class="d-inline-flex align-center primary--text">
-                                            <v-img
-                                                :src="require('@/assets/icon-bitcoin.svg')"
-                                                alt="Bitcoin symbol in circle with yellow background"
-                                                width="32"
-                                                height="32"
-                                                class="mr-3"
-                                            />
-                                            <p>Bitcoin Donation</p>
-                                        </a>
-                                    </div>
-                                </div>
-                            </v-col>
-                        </v-row>
-                    </v-col>
-                    <v-spacer></v-spacer>
-                    <v-col cols="12" sm="4" md="3" lg="2" class="text-lg-right mb-11 mb-lg-0">
-                        <a href="#" class="footer-item-title d-block text-h5 font-weight-bold mb-2">
+                                <p>Ethereum Donation</p>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#" class="d-inline-flex align-center primary--text">
+                                <v-img
+                                    :src="require('@/assets/icon-bitcoin.svg')"
+                                    alt="Bitcoin symbol in circle with yellow background"
+                                    width="32"
+                                    height="32"
+                                    class="mr-3"
+                                />
+                                <p>Bitcoin Donation</p>
+                            </a>
+                        </div>
+                    </div>
+                </v-col>
+
+                <!-- <v-spacer></v-spacer> -->
+                <v-col cols="12" sm="4" class="text-lg-right mb-11 mb-sm-0" order="5" order-sm="3" order-lg="4">
+                    <!-- <a href="#" class="footer-item-title d-block text-h5 font-weight-bold mb-2 text-sm-right">
                             Settings
                             <v-icon class="ml-2">settings</v-icon>
-                        </a>
-                        <p class="mb-3">Pricing from Coingecko</p>
-                        <v-switch
-                            @update:modelValue="toggleTheme"
-                            v-model="isDarkMode"
-                            label="Dark mode"
-                            hide-details
-                            color="textPrimary"
-                            class="theme-toggle"
-                        />
-                        <div class="d-flex font-weight-light align-center justify-lg-end">
-                            <p class="text-uppercase mb-0 mr-2 text-caption font-weight-light">Powered by</p>
-                            <div class="d-flex align-center">
-                                <v-img :src="require('@/assets/logo/mew.svg')" width="24" height="24" contain class="mr-1" />
-                                <p class="text-uppercase text-h3">MEW</p>
-                            </div>
+                        </a> -->
+                    <v-switch
+                        @update:modelValue="toggleTheme"
+                        v-model="isDarkMode"
+                        :label="themeSwitchLabel"
+                        hide-details
+                        color="textPrimary"
+                        class="theme-toggle mt-2"
+                    ></v-switch>
+                    <p class="mb-3 text-sm-right">Pricing from Coingecko</p>
+                    <div class="d-flex font-weight-light align-center justify-sm-end">
+                        <p class="text-uppercase mb-0 mr-2 text-caption font-weight-light">Powered by</p>
+                        <div class="d-flex align-center">
+                            <v-img :src="require('@/assets/logo/mew.svg')" width="24" height="24" contain class="mr-1" />
+                            <p class="text-uppercase text-h3">MEW</p>
                         </div>
-                    </v-col>
-                </v-row>
-                <div class="d-flex justify-lg-end flex-wrap pa-2">
-                    <a v-for="(link, index) in socialIcons" :key="index" :href="link.link" target="_blank" class="text-link">
-                        <v-img :src="link.img" :alt="link.altText" width="22" height="22" class="mr-4 mb-4 mb-lg-0 mr-lg-3 text-red" />
-                        <p class="d-sr-only">{{ link.srText }}</p>
-                    </a>
-                </div>
-            </div>
+                    </div>
+                </v-col>
+                <v-col v-if="mdAndDown" cols="5" sm="6" order="3" order-sm="5">
+                    <v-img
+                        :src="require('@/assets/hero/hero-blocks.png')"
+                        min-width="80"
+                        min-height="80"
+                        max-width="200"
+                        max-height="200"
+                        aspect-ratio="1"
+                        contain
+                        class="ml-sm-auto mr-sm-n6"
+                    />
+                </v-col>
+                <v-col cols="12" order="6">
+                    <div class="d-flex justify-lg-end flex-wrap py-2 px-n4">
+                        <a v-for="(link, index) in socialIcons" :key="index" :href="link.link" target="_blank" class="text-link">
+                            <v-img :src="link.img" :alt="link.altText" width="22" height="22" class="mr-4 mb-4 mb-lg-0 mr-lg-3 text-red" />
+                            <p class="d-sr-only">{{ link.srText }}</p>
+                        </a>
+                    </div>
+                </v-col>
+            </v-row>
         </v-container>
         <div class="copyright bg-tabActive w-100">
             <v-container class="py-2 px-0">
@@ -119,10 +120,12 @@
 
 <script setup lang="ts">
 import { useTheme } from 'vuetify'
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, ref, watch, computed } from 'vue'
 import { useStore } from '@/store'
 import { usePreferredColorScheme } from '@vueuse/core'
+import { useDisplay } from 'vuetify/lib/framework.mjs'
 
+const { sm, md, lgAndUp, mdAndDown } = useDisplay()
 const socialIcons = [
     {
         link: 'https://www.facebook.com/MyEtherWallet',
@@ -190,6 +193,10 @@ const toggleTheme = () => {
     store.setDarkMode(theme.global.name.value)
 }
 
+const themeSwitchLabel = computed<string>(() => {
+    return isDarkMode.value ? 'Dark Node On' : 'Dark Mode Off'
+})
+
 onMounted(() => {
     const preferredColor = usePreferredColorScheme()
     if (store.appTheme) {
@@ -232,7 +239,7 @@ a {
 
 .theme-toggle {
     :deep(.v-input__control) {
-        @media (min-width: 1240px) {
+        @media (min-width: 600px) {
             justify-self: flex-end;
         }
     }
