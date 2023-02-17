8ed19b81b409829d17e18a707a9262d6940df3db
olgakup <olga.kupchevskaya.812@my.csun.edu>
2023-02-16 18:19:29 -0800

    devop: add portfolio and tokens to home page

--- v2/src/views/ViewHome.vue
+++ v2/src/views/ViewHome.vue
@@ -4,7 +4,6 @@
             <v-container :class="['px-7 px-lg-12 pt-5 pt-lg-0 pb-16']">
                 <v-row align="center" justify="space-between" class="flex-column flex-lg-row">
                     <v-img v-if="mdAndDown" :src="require('@/assets/hero/hero.png')" alt="" height="300" width="300" max-height="300" max-width="300" contain />
-
                     <div class="pl-lg-14 pb-10 pb-sm-16 pb-lg-0">
                         <p class="mt-lg-16 text-white font-weight-light text-h3 text-sm-h2 text-lg-h1 text-center text-lg-left">Track, analyze and explore</p>
                         <p class="text-white font-weight-light text-h3 text-sm-h2 text-lg-h1 text-center text-lg-left">on the Ethereum blockchain.</p>
@@ -32,6 +31,20 @@
                     <v-col cols="12" :class="columnPadding">
                         <block-stats-module />
                     </v-col>
+                    <v-col v-if="showPortfolio" cols="12" :class="columnPadding">
+                        <v-card elevation="1" rounded="xl" class="pt-4 pt-sm-6">
+                            <v-card-title class="px-0 d-flex align-center justify-space-between px-4 px-sm-6 py-0 mb-2">
+                                <p class="text-h6 font-weight-bold">Your portfolio</p>
+                                <app-btn text="More" isSmall icon="east" @click="goToPortfolio"></app-btn>
+                            </v-card-title>
+                            <module-portfolio-list /> </v-card
+                    ></v-col>
+                    <v-col cols="12" lg="6" :class="columnPadding">
+                        <module-tokens-info :home-page="TOKENS_VIEW.ALL" />
+                    </v-col>
+                    <v-col cols="12" lg="6" :class="columnPadding">
+                        <module-tokens-info v-if="showFavToknes" :home-page="TOKENS_VIEW.FAV" />
+                    </v-col>
                     <v-col cols="12" :class="columnPadding">
                         <recent-blocks :max-items="10" page-type="home" />
                     </v-col>
@@ -50,11 +63,32 @@ import BlockStatsModule from '@module/block/ModuleBlockStats.vue'
 import RecentBlocks from '@module/block/ModuleRecentBlocks.vue'
 import ModuleTxs from '@module/txs/ModuleTxs.vue'
 import ModuleSearch from '@/modules/search/ModuleAppSearch.vue'
-
+import ModulePortfolioList from '@module/address/ModulePortfolioList.vue'
+import ModuleTokensInfo from '@module/tokens/ModuleTokensInfo.vue'
+import { TOKENS_VIEW } from '@module/tokens/models/tokensView'
+import AppBtn from '@core/components/AppBtn.vue'
 import { useAppViewGrid } from '@core/composables/AppViewGrid/AppViewGrid.composable'
 import { useDisplay } from 'vuetify/lib/framework.mjs'
-const { smAndDown, lgAndUp, md, mdAndDown } = useDisplay()
+import { ROUTE_NAME } from '@core/router/routesNames'
+import { useRouter } from 'vue-router'
+import { computed } from 'vue'
+import { useStore } from '@/store'
+const { lgAndUp, mdAndDown } = useDisplay()
 const { columnPadding, rowMargin } = useAppViewGrid()
+
+const router = useRouter()
+const store = useStore()
+const showPortfolio = computed<boolean>(() => {
+    return store.portfolioLength > 0
+})
+const goToPortfolio = async (): Promise<void> => {
+    await router.push({
+        name: ROUTE_NAME.PORTFOLIO.NAME
+    })
+}
+const showFavToknes = computed<boolean>(() => {
+    return store.favTokens.length > 0
+})
 </script>
 
 <style lang="scss" scoped>
