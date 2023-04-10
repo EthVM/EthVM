31282052e75a6e1636c66e1ba82f9f95dfe43ca1
olgakup <olga.kupchevskaya.812@my.csun.edu>
2023-04-10 12:46:34 -0700

    fix: open dropdown

--- v2/src/core/components/AppChangeNetwork.vue
+++ v2/src/core/components/AppChangeNetwork.vue
@@ -3,22 +3,22 @@
         <v-btn class="text-subtitle-1 font-weight-light text-body-1" rounded="pill" variant="outlined" height="24" id="network-dropdown">
             {{ networkName }}
             <v-icon class="ml-1" size="16">expand_more</v-icon>
+            <app-menu min-width="180" activator="parent">
+                <v-list-item
+                    title="Ethereum Mainnet"
+                    class="py-2 network-item"
+                    :href="getIsCurrNetwork(ETH_NAME) ? undefined : 'https://www.ethvm.com/'"
+                    :target="getIsCurrNetwork(ETH_NAME) ? '_self' : '_blank'"
+                >
+                </v-list-item>
+                <v-list-item
+                    title="Sepolia Testnet"
+                    class="py-2 network-item"
+                    :href="getIsCurrNetwork(SEP_NAME) ? undefined : 'https://sepolia.ethvm.com/'"
+                    :target="getIsCurrNetwork(SEP_NAME) ? '_self' : '_blank'"
+                ></v-list-item>
+            </app-menu>
         </v-btn>
-        <app-menu min-width="180" activator="#network-dropdown">
-            <v-list-item
-                title="Ethereum Mainnet"
-                class="py-2 network-item"
-                :href="getIsCurrNetwork(ETH_NAME) ? undefined : 'https://www.ethvm.com/'"
-                :target="getIsCurrNetwork(ETH_NAME) ? '_self' : '_blank'"
-            >
-            </v-list-item>
-            <v-list-item
-                title="Sepolia Testnet"
-                class="py-2 network-item"
-                :href="getIsCurrNetwork(SEP_NAME) ? undefined : 'https://sepolia.ethvm.com/'"
-                :target="getIsCurrNetwork(SEP_NAME) ? '_self' : '_blank'"
-            ></v-list-item>
-        </app-menu>
     </div>
 </template>
 
