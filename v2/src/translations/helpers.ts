3cda0da4672e1601b3dddbe9f319564a9f01d855
olgakup <olga.kupchevskaya.812@my.csun.edu>
2024-05-23 12:36:05 -0700

    feat: translate option

--- v2/src/translations/helpers.ts
+++ v2/src/translations/helpers.ts
@@ -1,11 +1,11 @@
 import en_US from './en_US.json'
 
-export type Mess = 'en_US' | 'ru_RU'
+export type Mess = 'en_US' | 'ru_RU' | 'vi_VN'
 
 export type MassagesShema = {
     [i in Mess]: typeof en_US
 }
 
 export const isOfTypeMes = (keyInput: string): keyInput is Mess => {
-    return ['en_US', 'ru_RU'].includes(keyInput)
+    return ['en_US', 'ru_RU', 'vi_VN'].includes(keyInput)
 }
--- v2/src/translations/index.ts
+++ v2/src/translations/index.ts
@@ -2,11 +2,13 @@ import { createI18n } from 'vue-i18n'
 
 import en_US from './en_US.json'
 import ru_RU from './ru_RU.json'
+import vi_VN from './vi_VN.json'
 import { MassagesShema } from './helpers'
 
 const messages: MassagesShema = {
     en_US,
-    ru_RU
+    ru_RU,
+    vi_VN
 }
 
 const instance = createI18n({
